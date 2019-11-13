27f579d2737aa197602eec531d2d5a4326e2eab7
Joao Rebelo <2730147+jrebelo@users.noreply.github.com>
2019-11-13 20:41:35 +0100

    Implement parse info source

--- src/parser/mod.rs
+++ src/parser/mod.rs
@@ -271,7 +271,7 @@ pub struct InfoReply {
 pub struct InfoSrc {
     protocol_version: ProtocolVersion,
     vendor_id: [u8;2],
-    guid_prefix: [u8;12],
+    guid_prefix: GuidPrefix,
 }
 
 #[derive(Serialize, Deserialize, PartialEq, Debug)]
@@ -675,8 +675,13 @@ fn parse_info_reply_submessage(submessage: &[u8], submessage_flags: &u8) -> Resu
     })
 }
 
-fn parse_info_source_submessage(_submessage: &[u8], _submessage_flags: &u8) -> Result<InfoSrc> {
-    unimplemented!()
+fn parse_info_source_submessage(submessage: &[u8], submessage_flags: &u8) -> Result<InfoSrc> {
+    const INFO_SRC_FIRST_INDEX: usize = 4;
+    let info_src_last_index = submessage.len()-1;
+
+    let submessage_endianess = endianess(submessage_flags)?;
+
+    deserialize::<InfoSrc>(submessage, &INFO_SRC_FIRST_INDEX, &info_src_last_index, &submessage_endianess)
 }
 
 fn parse_info_timestamp_submessage(submessage: &[u8], submessage_flags: &u8) -> Result<InfoTs> {
@@ -1704,7 +1709,30 @@ mod tests{
 
     #[test]
     fn test_parse_info_source_submessage() {
-        parse_info_source_submessage(&[0,0], &0);
+        {
+            let submessage = [
+                0x00, 0x00, 0x00, 0x00,
+                0x02, 0x04, 0x10, 0x20,
+                0x01, 0x02, 0x03, 0x04,
+                0x05, 0x06, 0x07, 0x08,
+                0x09, 0x0A, 0x0B, 0x0C,
+            ];
+
+            let info_src_big_endian = parse_info_source_submessage(&submessage, &0).unwrap();
+
+            assert_eq!(info_src_big_endian.protocol_version.major, 2);
+            assert_eq!(info_src_big_endian.protocol_version.minor, 4);
+            assert_eq!(info_src_big_endian.vendor_id, [0x10, 0x20]);
+            assert_eq!(info_src_big_endian.guid_prefix, [0x01, 0x02, 0x03, 0x04, 0x05, 0x06, 0x07, 0x08, 0x09, 0x0A, 0x0B, 0x0C,]);
+
+            let info_src_little_endian = parse_info_source_submessage(&submessage, &1).unwrap();
+
+            assert_eq!(info_src_little_endian.protocol_version.major, 2);
+            assert_eq!(info_src_little_endian.protocol_version.minor, 4);
+            assert_eq!(info_src_little_endian.vendor_id, [0x10, 0x20]);
+            assert_eq!(info_src_little_endian.guid_prefix, [0x01, 0x02, 0x03, 0x04, 0x05, 0x06, 0x07, 0x08, 0x09, 0x0A, 0x0B, 0x0C,]);
+        }
+        
     }
 
     #[test]
