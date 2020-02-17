c98c7ea93e502dd2dad6c7a77702c9840e1d22a2
Joao Rebelo <2730147+jrebelo@users.noreply.github.com>
2020-02-17 11:47:55 +0100

    Add writer struct implementation

--- src/writer.rs
+++ src/writer.rs
@@ -1,1 +1,14 @@
+use crate::endpoint::{Endpoint};
 use crate::cache::{WriterHistoryCache};
+use crate::types::{Duration, SequenceNumber};
+
+pub struct Writer {
+    endpoint : Endpoint,
+    pub push_mode: bool,
+    pub heartbeat_period: Duration,
+    pub nack_response_delay: Duration,
+    pub nack_suppression_duration : Duration,
+    pub last_change_sequence_number: SequenceNumber,
+    pub writer_cache: WriterHistoryCache,
+    pub data_max_sized_serialized : Option<i32>,
+}
