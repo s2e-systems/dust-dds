e8aea4e01f3714987636df589b14a5a3afcbc91d
Joao Rebelo <2730147+jrebelo@users.noreply.github.com>
2020-06-12 00:33:04 +0200

    Add stateful reader behavior waiting state test

--- src/behavior/stateful_reader.rs
+++ src/behavior/stateful_reader.rs
@@ -29,7 +29,44 @@ impl StatefulReaderBehaviour {
                 }
             }
         }
+    }
+}
 
-        todo!()
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use crate::types::{SequenceNumber, ChangeKind, GuidPrefix, TopicKind, ReliabilityKind, Locator};
+    use crate::behavior::types::constants::DURATION_ZERO;
+    use crate::messages::types::Count;
+    use crate::types::constants::{
+        ENTITYID_SEDP_BUILTIN_SUBSCRIPTIONS_ANNOUNCER, ENTITYID_SEDP_BUILTIN_SUBSCRIPTIONS_DETECTOR, ENTITYID_SEDP_BUILTIN_TOPICS_ANNOUNCER, 
+        ENTITYID_BUILTIN_PARTICIPANT_MESSAGE_WRITER, ENTITYID_SEDP_BUILTIN_PUBLICATIONS_DETECTOR, };
+    use crate::cache::CacheChange;
+    use crate::messages::{Data, Payload};
+    use crate::messages::submessage_elements::SequenceNumberSet;
+    use crate::serdes::Endianness;
+    use crate::stateful_writer::StatefulWriter;
+    use crate::serialized_payload::SerializedPayload;
+    use std::thread::sleep;
+
+    #[test]
+    fn run_waiting_state() {
+        let mut history_cache = HistoryCache::new();
+        let remote_writer_guid = GUID::new(GuidPrefix([1;12]), ENTITYID_SEDP_BUILTIN_SUBSCRIPTIONS_ANNOUNCER);
+        let mut writer_proxy = WriterProxy::new(remote_writer_guid, vec![], vec![]);
+
+        let mut submessages = Vec::new();
+        let data1 = Data::new(
+            Endianness::LittleEndian, 
+            ENTITYID_SEDP_BUILTIN_SUBSCRIPTIONS_DETECTOR, 
+            ENTITYID_SEDP_BUILTIN_SUBSCRIPTIONS_ANNOUNCER, 
+            SequenceNumber(1),
+            None,
+            Payload::Data(SerializedPayload(vec![1,2,3])));
+        submessages.push(RtpsSubmessage::Data(data1));
+
+        let received_message = RtpsMessage::new(*remote_writer_guid.prefix(), submessages);
+
+        StatefulReaderBehaviour::run_waiting_state(&mut writer_proxy, &mut history_cache, Some(&received_message));
     }
 }
