9317fc402609e1d86586f0651f372b65ce7e3281
Joao Rebelo <2730147+jrebelo@users.noreply.github.com>
2020-07-31 17:53:48 +0200

    Fix compilation warnings

--- src/serialized_payload.rs
+++ src/serialized_payload.rs
@@ -1,5 +1,3 @@
-use std::rc::Rc;
-
 use crate::messages::{ParameterList, SubmessageElement, Endianness, Pid};
 use cdr::{LittleEndian, BigEndian};
 
--- src/spdp.rs
+++ src/spdp.rs
@@ -1,4 +1,3 @@
-use std::io::Write;
 use std::convert::TryInto;
 use crate::messages::Endianness;
 
