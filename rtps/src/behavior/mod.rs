fb3b3cd8fa0953552acf1ced8700b91f5d2f520e
Stefan Kimmer <skimmer@s2e-systems.com>
2020-10-09 15:51:56 +0200

    move reader_proxy from stateful_writer to own file

--- rtps/src/behavior/mod.rs
+++ rtps/src/behavior/mod.rs
@@ -1,6 +1,7 @@
 pub mod types;
 pub mod stateful_writer;
 pub mod writer_proxy;
+pub mod reader_proxy;
 pub mod best_effort_writer_proxy;
 pub mod reliable_writer_proxy;
 pub mod stateful_reader;
@@ -11,8 +12,9 @@ pub mod stateless_writer_behavior;
 pub mod stateless_reader_behavior;
 
 pub use writer_proxy::WriterProxy;
+pub use reader_proxy::ReaderProxy;
 pub use stateful_reader::StatefulReader;
-pub use stateful_writer::{StatefulWriter, ReaderProxy};
+pub use stateful_writer::StatefulWriter;
 pub use stateless_reader::StatelessReader;
 pub use stateless_writer::{StatelessWriter, ReaderLocator};
 
--- rtps/src/behavior/stateful_writer.rs
+++ rtps/src/behavior/stateful_writer.rs
@@ -1,176 +1,20 @@
-use std::collections::{BTreeSet, HashMap, VecDeque};
-use std::sync::{RwLock, RwLockReadGuard, Mutex, MutexGuard};
+use std::collections::{HashMap, VecDeque};
+use std::sync::{RwLock, RwLockReadGuard, Mutex};
 
 use crate::types::{ChangeKind, InstanceHandle, Locator, ReliabilityKind, SequenceNumber, TopicKind, GUID, GuidPrefix};
 use crate::behavior::types::Duration;
-use crate::behavior::stateful_writer_behavior::{StatefulWriterBehavior, BestEffortStatefulWriterBehavior, ReliableStatefulWriterBehavior};
+use crate::behavior::stateful_writer_behavior::{BestEffortStatefulWriterBehavior, ReliableStatefulWriterBehavior};
 use crate::messages::RtpsSubmessage;
 use crate::messages::message_receiver::Receiver;
 use crate::messages::message_sender::Sender;
 use crate::structure::HistoryCache;
 use crate::structure::CacheChange;
 use crate::serialized_payload::ParameterList;
+use super::reader_proxy::ReaderProxy;
 use rust_dds_interface::protocol::{ProtocolEntity, ProtocolWriter, ProtocolEndpoint};
 use rust_dds_interface::qos::DataWriterQos;
 use rust_dds_interface::types::{Data, Time, ReturnCode};
 
-struct ChangeForReader {
-    highest_sequence_number_sent: SequenceNumber,
-    highest_sequence_number_acknowledged: SequenceNumber,
-    sequence_numbers_requested: BTreeSet<SequenceNumber>,
-}
-
-impl ChangeForReader {
-    fn new() -> Self {
-        Self {
-            highest_sequence_number_sent: 0,
-            highest_sequence_number_acknowledged: 0,
-            sequence_numbers_requested: BTreeSet::new(),
-        }
-    }
-
-    fn next_unsent_change(&mut self, last_change_sequence_number: SequenceNumber) -> Option<SequenceNumber> {
-        let next_unsent_sequence_number = self.highest_sequence_number_sent + 1;
-        if next_unsent_sequence_number > last_change_sequence_number {
-            None
-        } else {
-            self.highest_sequence_number_sent = next_unsent_sequence_number;
-            Some(next_unsent_sequence_number)
-        }
-    }
-
-    fn unsent_changes(&self, last_change_sequence_number: SequenceNumber) -> BTreeSet<SequenceNumber> {
-        let mut unsent_changes_set = BTreeSet::new();
-
-        // The for loop is made with the underlying sequence number type because it is not possible to implement the Step trait on Stable yet
-        for unsent_sequence_number in
-            self.highest_sequence_number_sent + 1..=last_change_sequence_number
-        {
-            unsent_changes_set.insert(unsent_sequence_number);
-        }
-
-        unsent_changes_set
-    }
-
-    fn acked_changes(&self) -> SequenceNumber {
-        self.highest_sequence_number_acknowledged
-    }
-
-    fn acked_changes_set(&mut self, committed_seq_num: SequenceNumber) {
-        self.highest_sequence_number_acknowledged = committed_seq_num;
-    }
-
-    fn unacked_changes(&self, last_change_sequence_number: SequenceNumber) -> BTreeSet<SequenceNumber> {
-        let mut unacked_changes_set = BTreeSet::new();
-
-        // The for loop is made with the underlying sequence number type because it is not possible to implement the Step trait on Stable yet
-        for unsent_sequence_number in
-            self.highest_sequence_number_acknowledged + 1..=last_change_sequence_number
-        {
-            unacked_changes_set.insert(unsent_sequence_number);
-        }
-
-        unacked_changes_set
-    }
-
-    fn requested_changes_set(&mut self, req_seq_num_set: BTreeSet<SequenceNumber>) {
-        let mut new_set = req_seq_num_set;
-        self.sequence_numbers_requested.append(&mut new_set);
-    }
-
-    fn requested_changes(&self) -> BTreeSet<SequenceNumber> {
-        self.sequence_numbers_requested.clone()
-    }
-
-    fn next_requested_change(&mut self) -> Option<SequenceNumber> {
-        let next_requested_change = *self.sequence_numbers_requested.iter().next()?;
-
-        self.sequence_numbers_requested.remove(&next_requested_change);
-
-        Some(next_requested_change)
-    }
-}
-
-
-pub struct ReaderProxy {
-    remote_reader_guid: GUID,
-    // remoteGroupEntityId: EntityId_t,
-    unicast_locator_list: Vec<Locator>,
-    multicast_locator_list: Vec<Locator>,
-    changes_for_reader: Mutex<ChangeForReader>,
-    expects_inline_qos: bool,
-    is_active: bool,
-    stateful_writer_behavior: Mutex<StatefulWriterBehavior>,
-
-    send_messages: Mutex<VecDeque<RtpsSubmessage>>,
-    received_messages: Mutex<VecDeque<(GuidPrefix, RtpsSubmessage)>>,
-}
-
-
-impl ReaderProxy {
-    pub fn new(
-        remote_reader_guid: GUID,
-        unicast_locator_list: Vec<Locator>,
-        multicast_locator_list: Vec<Locator>,
-        expects_inline_qos: bool,
-        is_active: bool) -> Self {
-            Self {
-                remote_reader_guid,
-                unicast_locator_list,
-                multicast_locator_list,
-                expects_inline_qos,
-                is_active,
-                changes_for_reader: Mutex::new(ChangeForReader::new()),
-                stateful_writer_behavior: Mutex::new(StatefulWriterBehavior::new()),
-                send_messages: Mutex::new(VecDeque::new()),
-                received_messages: Mutex::new(VecDeque::new()),
-        }
-    }
-
-    pub fn acked_changes_set(&self, committed_seq_num: SequenceNumber) {
-        self.changes_for_reader.lock().unwrap().acked_changes_set(committed_seq_num);
-    }
-
-    pub fn next_requested_change(&self) -> Option<SequenceNumber> {
-        self.changes_for_reader.lock().unwrap().next_requested_change()
-    }
-
-    pub fn next_unsent_change(&self, last_change_sequence_number: SequenceNumber) -> Option<SequenceNumber> {
-        self.changes_for_reader.lock().unwrap().next_unsent_change(last_change_sequence_number)
-    }
-
-    pub fn unsent_changes(&self, last_change_sequence_number: SequenceNumber) -> BTreeSet<SequenceNumber> {
-        self.changes_for_reader.lock().unwrap().unsent_changes(last_change_sequence_number)
-    }
-
-    pub fn requested_changes(&self) -> BTreeSet<SequenceNumber> {
-        self.changes_for_reader.lock().unwrap().requested_changes()
-    }
-
-    pub fn requested_changes_set(&self, req_seq_num_set: BTreeSet<SequenceNumber>) {
-        self.changes_for_reader.lock().unwrap().requested_changes_set(req_seq_num_set);
-    }
-
-    pub fn unacked_changes(&self, last_change_sequence_number: SequenceNumber) -> BTreeSet<SequenceNumber> {
-        self.changes_for_reader.lock().unwrap().unacked_changes(last_change_sequence_number)
-    }
-
-    pub fn unicast_locator_list(&self) -> &Vec<Locator> {
-        &self.unicast_locator_list
-    }
-
-    pub fn multicast_locator_list(&self) -> &Vec<Locator> {
-        &self.multicast_locator_list
-    }
-
-    pub fn remote_reader_guid(&self) -> &GUID {
-        &self.remote_reader_guid
-    }
-
-    pub fn behavior(&self) -> MutexGuard<StatefulWriterBehavior> {
-        self.stateful_writer_behavior.lock().unwrap()
-    }
-}
 
 pub struct StatefulWriter {
     /// Entity base class (contains the GUID)
@@ -388,7 +232,8 @@ mod tests {
     use crate::messages::Endianness;
     
     use rust_dds_interface::qos_policy::ReliabilityQosPolicyKind;
-
+    
+    use std::collections::BTreeSet;
     use std::thread::sleep;
 
     #[test]
@@ -426,100 +271,6 @@ mod tests {
         );
         assert_eq!(cache_change_seq2.inline_qos().len(), 0);
         assert_eq!(cache_change_seq2.instance_handle(), &[1; 16]);
-    }
-
-    #[test]
-    fn reader_proxy_unsent_changes_operations() {
-        let remote_reader_guid = GUID::new([1,2,3,4,5,6,7,8,9,10,11,12], ENTITYID_SEDP_BUILTIN_SUBSCRIPTIONS_DETECTOR);
-        let reader_proxy = ReaderProxy::new(remote_reader_guid, vec![], vec![], false, true);
-
-        // Check that a reader proxy that has no changes marked as sent doesn't reports no changes
-        let no_change_in_writer_sequence_number = 0;
-        assert_eq!(reader_proxy.next_unsent_change(no_change_in_writer_sequence_number), None);
-        assert!(reader_proxy.unsent_changes(no_change_in_writer_sequence_number).is_empty());
-
-        // Check the behaviour for a reader proxy starting with no changes sent and two changes in writer
-        let two_changes_in_writer_sequence_number = 2;
-        assert_eq!(reader_proxy.unsent_changes(two_changes_in_writer_sequence_number).len(), 2);
-        assert!(reader_proxy.unsent_changes(two_changes_in_writer_sequence_number).contains(&1));
-        assert!(reader_proxy.unsent_changes(two_changes_in_writer_sequence_number).contains(&2));
-
-        assert_eq!(reader_proxy.next_unsent_change(two_changes_in_writer_sequence_number), Some(1));
-        assert_eq!(reader_proxy.unsent_changes(two_changes_in_writer_sequence_number).len(), 1);
-        assert!(reader_proxy.unsent_changes(two_changes_in_writer_sequence_number).contains(&2));
-
-        assert_eq!(reader_proxy.next_unsent_change(two_changes_in_writer_sequence_number), Some(2));
-        assert!(reader_proxy.unsent_changes(two_changes_in_writer_sequence_number).is_empty());
-
-        assert_eq!(reader_proxy.next_unsent_change(two_changes_in_writer_sequence_number), None);
-    }
-
-    #[test]
-    fn reader_proxy_requested_changes_operations() {
-        let remote_reader_guid = GUID::new([1,2,3,4,5,6,7,8,9,10,11,12], ENTITYID_SEDP_BUILTIN_SUBSCRIPTIONS_DETECTOR);
-        let reader_proxy = ReaderProxy::new(remote_reader_guid, vec![], vec![], false, true);
-
-        // Check that a reader proxy that has no changes marked as sent doesn't reports no changes
-        assert!(reader_proxy.requested_changes().is_empty());
-        assert_eq!(reader_proxy.next_requested_change(), None);
-
-        // Insert some requested changes
-        let mut requested_changes = BTreeSet::new();
-        requested_changes.insert(2);
-        requested_changes.insert(3);
-        requested_changes.insert(6);
-        reader_proxy.requested_changes_set(requested_changes);
-
-        // Verify that the changes were correctly inserted and are removed in the correct order
-        assert_eq!(reader_proxy.requested_changes().len(), 3);
-        assert!(reader_proxy.requested_changes().contains(&2));
-        assert!(reader_proxy.requested_changes().contains(&3));
-        assert!(reader_proxy.requested_changes().contains(&6));
-
-        assert_eq!(reader_proxy.next_requested_change(), Some(2));
-        assert_eq!(reader_proxy.next_requested_change(), Some(3));
-        assert_eq!(reader_proxy.requested_changes().len(), 1);
-        assert!(reader_proxy.requested_changes().contains(&6));
-        assert_eq!(reader_proxy.next_requested_change(), Some(6));
-        assert_eq!(reader_proxy.next_requested_change(), None);
-
-
-        // Verify that if requested changes are inserted when there are already requested changes
-        // that the sets are not replaced
-        let mut requested_changes_1 = BTreeSet::new();
-        requested_changes_1.insert(2);
-        requested_changes_1.insert(3);
-        reader_proxy.requested_changes_set(requested_changes_1);
-
-        let mut requested_changes_2 = BTreeSet::new();
-        requested_changes_2.insert(2); // Repeated number
-        requested_changes_2.insert(7);
-        requested_changes_2.insert(9);
-        reader_proxy.requested_changes_set(requested_changes_2);
-        
-        assert_eq!(reader_proxy.requested_changes().len(), 4);
-        assert!(reader_proxy.requested_changes().contains(&2));
-        assert!(reader_proxy.requested_changes().contains(&3));
-        assert!(reader_proxy.requested_changes().contains(&7));
-        assert!(reader_proxy.requested_changes().contains(&9));
-    }
-
-    #[test]
-    fn reader_proxy_unacked_changes_operations() {
-        let remote_reader_guid = GUID::new([1,2,3,4,5,6,7,8,9,10,11,12], ENTITYID_SEDP_BUILTIN_SUBSCRIPTIONS_DETECTOR);
-        let reader_proxy = ReaderProxy::new(remote_reader_guid, vec![], vec![], false, true);
-
-        let no_change_in_writer = 0;
-        assert!(reader_proxy.unacked_changes(no_change_in_writer).is_empty());
-
-        let two_changes_in_writer = 2;
-        assert_eq!(reader_proxy.unacked_changes(two_changes_in_writer).len(), 2);
-        assert!(reader_proxy.unacked_changes(two_changes_in_writer).contains(&1));
-        assert!(reader_proxy.unacked_changes(two_changes_in_writer).contains(&2));
-
-        reader_proxy.acked_changes_set(1);
-        assert_eq!(reader_proxy.unacked_changes(two_changes_in_writer).len(), 1);
-        assert!(reader_proxy.unacked_changes(two_changes_in_writer).contains(&2));
     }
 
     #[test]
