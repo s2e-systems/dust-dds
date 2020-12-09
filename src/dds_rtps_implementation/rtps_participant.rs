e441470656cec011ee50395e30fd7792875ad900
Joao Rebelo <2730147+jrebelo@users.noreply.github.com>
2020-12-09 18:52:40 +0100

    Add methods and call for rtps participant

--- src/dds_rtps_implementation/rtps_participant.rs
+++ src/dds_rtps_implementation/rtps_participant.rs
@@ -1,42 +1,44 @@
-use std::sync::{Arc};
+use std::sync::Arc;
 
-use crate::types::{DomainId, ReturnCode, DDSType};
+use crate::dds_infrastructure::qos::{PublisherQos, SubscriberQos, TopicQos};
+use crate::types::{DDSType, DomainId, Duration, InstanceHandle, ReturnCode, Time};
+use crate::builtin_topics::{ParticipantBuiltinTopicData, TopicBuiltinTopicData};
 
-use crate::rtps::transport::Transport;
 use crate::rtps::transport::udp::UdpTransport;
+use crate::rtps::transport::Transport;
 
-use crate::dds_rtps_implementation::rtps_subscriber::RtpsSubscriber;
-use crate::dds_rtps_implementation::rtps_publisher::{RtpsPublisher, RtpsPublisherInner};
-use crate::dds_rtps_implementation::rtps_topic::RtpsTopic;
 use crate::dds_rtps_implementation::rtps_object::RtpsObject;
+use crate::dds_rtps_implementation::rtps_publisher::{RtpsPublisher, RtpsPublisherInner};
+use crate::dds_rtps_implementation::rtps_subscriber::RtpsSubscriber;
+use crate::dds_rtps_implementation::rtps_topic::RtpsTopic;
 
 pub struct RtpsParticipant {
     userdata_transport: Box<dyn Transport>,
     metatraffic_transport: Box<dyn Transport>,
-    publisher_list: Arc<[RtpsObject<RtpsPublisherInner>;32]>,
+    publisher_list: Arc<[RtpsObject<RtpsPublisherInner>; 32]>,
 }
 
 impl RtpsParticipant {
-    pub fn new (
+    pub fn new(
         domain_id: DomainId,
-    //     qos: DomainParticipantQos,
-    //     a_listener: impl DomainParticipantListener,
-    //     mask: StatusMask,
-    //     enabled: bool,
-    ) ->  Option<Self> {
+        //     qos: DomainParticipantQos,
+        //     a_listener: impl DomainParticipantListener,
+        //     mask: StatusMask,
+        //     enabled: bool,
+    ) -> Option<Self> {
         let interface = "Ethernet";
-        let userdata_transport = Box::new(
-            UdpTransport::default_userdata_transport(domain_id, interface).unwrap());
-        let metatraffic_transport = Box::new(
-            UdpTransport::default_metatraffic_transport(domain_id, interface).unwrap());
+        let userdata_transport =
+            Box::new(UdpTransport::default_userdata_transport(domain_id, interface).unwrap());
+        let metatraffic_transport =
+            Box::new(UdpTransport::default_metatraffic_transport(domain_id, interface).unwrap());
         // let domain_tag = "".to_string();
         // let lease_duration = Duration {
         //     sec: 30,
         //     nanosec: 0,
         // };
-        
+
         // let participant = RtpsParticipant::new(domain_id);
-        
+
         // // if enabled {
         // //     new_participant.enable().ok()?;
         // // }
@@ -44,11 +46,11 @@ impl RtpsParticipant {
         Some(Self {
             userdata_transport,
             metatraffic_transport,
-            publisher_list: Arc::new(Default::default())
+            publisher_list: Arc::new(Default::default()),
         })
     }
 
-    pub fn create_publisher<'a>(&'a self) -> Option<RtpsPublisher<'a>> {
+    pub fn create_publisher<'a>(&'a self, _qos: Option<&PublisherQos>) -> Option<RtpsPublisher<'a>> {
         todo!()
     }
 
@@ -56,15 +58,15 @@ impl RtpsParticipant {
         todo!()
     }
 
-    pub fn create_topic<T:DDSType>(&self) -> Option<RtpsTopic<T>> {
+    pub fn create_topic<T: DDSType>(&self, _topic_name: String, _qos: Option<&TopicQos>,) -> Option<RtpsTopic<T>> {
         todo!()
     }
 
-    pub fn delete_topic<T:DDSType>(&self, _a_topic: &RtpsTopic<T>) -> ReturnCode<()> {
+    pub fn delete_topic<T: DDSType>(&self, _a_topic: &RtpsTopic<T>) -> ReturnCode<()> {
         todo!()
     }
 
-    pub fn create_subscriber(&self) -> Option<RtpsSubscriber> {
+    pub fn create_subscriber(&self, _qos: Option<&SubscriberQos>) -> Option<RtpsSubscriber> {
         todo!()
     }
 
@@ -72,5 +74,112 @@ impl RtpsParticipant {
         todo!()
     }
 
-    
+    pub fn find_topic<T: DDSType>(
+        &self,
+        _topic_name: String,
+        _timeout: Duration,
+    ) -> Option<RtpsTopic<T>> {
+        todo!()
+    }
+
+    pub fn lookup_topicdescription<T: DDSType>(&self, _name: &String) -> Option<RtpsTopic<T>> {
+        todo!()
+    }
+
+    pub fn get_builtin_subscriber(&self) -> RtpsSubscriber {
+        todo!()
+    }
+
+    pub fn ignore_participant(&self, _handle: InstanceHandle) -> ReturnCode<()> {
+        todo!()
+    }
+
+    pub fn ignore_topic(&self, _handle: InstanceHandle) -> ReturnCode<()> {
+        todo!()
+    }
+
+    pub fn ignore_publication(&self, handle: InstanceHandle) -> ReturnCode<()> {
+        todo!()
+    }
+
+    pub fn ignore_subscription(&self, handle: InstanceHandle) -> ReturnCode<()> {
+        todo!()
+    }
+
+    pub fn get_domain_id(&self) -> DomainId {
+        todo!()
+    }
+
+    pub fn delete_contained_entities(&self) -> ReturnCode<()> {
+        todo!()
+    }
+
+    pub fn assert_liveliness(&self) -> ReturnCode<()> {
+        todo!()
+    }
+
+    pub fn set_default_publisher_qos(&self, qos: Option<PublisherQos>) -> ReturnCode<()> {
+        todo!()
+    }
+
+    pub fn get_default_publisher_qos(&self) -> PublisherQos {
+        todo!()
+    }
+
+    pub fn set_default_subscriber_qos(&self, qos: Option<SubscriberQos>) -> ReturnCode<()> {
+        todo!()
+    }
+
+    pub fn get_default_subscriber_qos(&self) -> SubscriberQos {
+        todo!()
+    }
+
+    pub fn set_default_topic_qos(&self, qos: Option<TopicQos>) -> ReturnCode<()> {
+        todo!()
+    }
+
+    pub fn get_default_topic_qos(&self) -> TopicQos {
+        todo!()
+    }
+
+    pub fn get_discovered_participants(
+        &self,
+        participant_handles: &mut [InstanceHandle],
+    ) -> ReturnCode<()> {
+        todo!()
+    }
+
+    pub fn get_discovered_participant_data(
+        &self,
+        participant_data: ParticipantBuiltinTopicData,
+        participant_handle: InstanceHandle,
+    ) -> ReturnCode<()> {
+        todo!()
+    }
+
+    pub fn get_discovered_topics(
+        &self,
+        topic_handles: &mut [InstanceHandle]
+    ) -> ReturnCode<()> {
+        todo!()
+    }
+
+    pub fn get_discovered_topic_data(
+        &self,
+        topic_data: TopicBuiltinTopicData,
+        topic_handle: InstanceHandle
+    ) -> ReturnCode<()> {
+        todo!()
+    }
+
+    pub fn contains_entity(
+        &self,
+        a_handle: InstanceHandle
+    ) -> bool {
+        todo!()
+    }
+
+    pub fn get_current_time(&self) -> ReturnCode<Time> {
+        todo!()
+    }
 }
--- src/lib.rs
+++ src/lib.rs
@@ -5,3 +5,4 @@ pub mod rtps;
 pub mod dds_infrastructure;
 pub mod dds_rtps_implementation;
 pub mod dds;
+pub mod builtin_topics;
