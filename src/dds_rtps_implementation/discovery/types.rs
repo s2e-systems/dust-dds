8f0fadb7081ef65e83fa3f8685f68e8a4bc1d295
Joao Rebelo <2730147+jrebelo@users.noreply.github.com>
2020-12-18 10:01:57 +0100

    Add discovery types

---
+++ src/dds_rtps_implementation/discovery/types.rs
@@ -0,0 +1,43 @@
+use crate::builtin_topics::{ParticipantBuiltinTopicData, TopicBuiltinTopicData, PublicationBuiltinTopicData, SubscriptionBuiltinTopicData};
+use crate::rtps::behavior::types::Duration;
+use crate::rtps::behavior::{ReaderProxy, WriterProxy};
+use crate::rtps::messages::types::Count;
+use crate::rtps::types::{ProtocolVersion, GuidPrefix, Locator, VendorId};
+use crate::rtps::endpoint_types::BuiltInEndpointSet;
+use crate::types::DomainId;
+
+pub struct ParticipantProxy {
+    domain_id: DomainId,
+    domain_tag: String,
+    protocol_version: ProtocolVersion,
+    guid_prefix: GuidPrefix,
+    vendor_id: VendorId,
+    expects_inline_qos: bool,
+    available_built_in_endpoints: BuiltInEndpointSet,
+    // built_in_endpoint_qos: 
+    metatraffic_unicast_locator_list: Vec<Locator>,
+    metatraffic_multicast_locator_list: Vec<Locator>,
+    default_unicast_locator_list: Vec<Locator>,
+    default_multicast_locator_list: Vec<Locator>,
+    manual_liveliness_count: Count,
+}
+
+pub struct SpdpDiscoveredParticipantData{
+    pub dds_participant_data: ParticipantBuiltinTopicData,
+    pub participant_proxy: ParticipantProxy,
+    pub lease_duration: Duration,
+}
+
+pub struct DiscoveredTopicData {
+    pub dds_topic_data: TopicBuiltinTopicData,
+}
+
+pub struct DiscoveredWriterData {
+    pub dds_writer_data: PublicationBuiltinTopicData,
+    pub writer_proxy: WriterProxy,
+}
+
+pub struct DiscoveredReaderData {
+    pub dds_reader_data: SubscriptionBuiltinTopicData,
+    pub reader_proxy: ReaderProxy,
+}
