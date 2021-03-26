f0e0d6014e39d4b2f9aaa093e72c02e42371a985
Joao Rebelo <2730147+jrebelo@users.noreply.github.com>
2021-03-26 22:46:34 +0100

    Remove submessage sufix from filenames

--- rtps_pim/src/structure/cache_change.rs
+++ rtps_pim/src/structure/cache_change.rs
@@ -1,10 +1,10 @@
-use crate::{structure, RtpsPim};
+use crate::RtpsPim;
 
 pub struct RTPSCacheChange<PIM: RtpsPim, Data> {
-    pub kind: <PIM as structure::Types>::ChangeKind,
-    pub writer_guid: <PIM as structure::Types>::Guid,
-    pub instance_handle: <PIM as structure::Types>::InstanceHandle,
-    pub sequence_number: <PIM as structure::Types>::SequenceNumber,
+    pub kind: PIM::ChangeKind,
+    pub writer_guid: PIM::Guid,
+    pub instance_handle: PIM::InstanceHandle,
+    pub sequence_number: PIM::SequenceNumber,
     pub data_value: Data,
-    // pub inline_qos:  <PIM as messages::Types>:: submessage_elements::ParameterList<ParameterId, ParameterValue, ParameterList>,
+    pub inline_qos: PIM::ParameterList,
 }
