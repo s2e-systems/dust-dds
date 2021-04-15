b087e884af8c636fe4dac9893c4fd48d485bec2e
Joao Rebelo <2730147+jrebelo@users.noreply.github.com>
2021-04-15 13:08:19 +0200

    remove obsolete data reader. add history cache impl

--- dds_rtps_implementation/src/rtps_impl/mod.rs
+++ dds_rtps_implementation/src/rtps_impl/mod.rs
@@ -2,8 +2,7 @@ pub mod participant_impl;
 pub mod publisher_group_impl;
 pub mod subscriber_group_impl;
 // pub mod topic_impl;
-// pub mod data_reader_impl;
 pub mod stateless_writer_impl;
 pub mod stateful_writer_impl;
 
-// pub mod history_cache_impl;
+pub mod history_cache_impl;
