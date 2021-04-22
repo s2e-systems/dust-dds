28a56b42d7bf1c506116d736fdc8568f6c9ca103
Joao Rebelo <2730147+jrebelo@users.noreply.github.com>
2021-04-22 09:24:22 +0200

    Implement RTPSStatelessWriter traits

--- dds_rtps_implementation/src/rtps_impl/rtps_stateless_writer_impl.rs
+++ dds_rtps_implementation/src/rtps_impl/rtps_stateless_writer_impl.rs
@@ -1,6 +1,8 @@
 use rust_dds_api::infrastructure::qos::DataWriterQos;
-use rust_rtps_pim::behavior::stateless_writer::RTPSReaderLocator;
+use rust_rtps_pim::{behavior::{RTPSWriter, stateless_writer::{RTPSReaderLocator, RTPSStatelessWriter}}, structure::{RTPSEndpoint, RTPSEntity, types::{ChangeKind, GUID, ReliabilityKind, TopicKind}}};
 use rust_rtps_udp_psm::RtpsUdpPsm;
+
+use super::rtps_history_cache_impl::RTPSHistoryCacheImpl;
 
 pub struct RTPSStatelessWriterImpl {
     pub reader_locators: Vec<RTPSReaderLocator<RtpsUdpPsm>>,
@@ -77,100 +79,100 @@ impl RTPSStatelessWriterImpl {
 //     }
 }
 
-// impl RTPSEntity<RtpsUdpPsm> for StatelessDataWriterImpl {
-//     fn guid(&self) -> GUID<RtpsUdpPsm> {
-//         todo!()
-//     }
-// }
+impl RTPSEntity<RtpsUdpPsm> for RTPSStatelessWriterImpl {
+    fn guid(&self) -> GUID<RtpsUdpPsm> {
+        todo!()
+    }
+}
 
-// impl RTPSWriter<RtpsUdpPsm, HistoryCacheImpl> for StatelessDataWriterImpl {
-//     fn push_mode(&self) -> bool {
-//         todo!()
-//     }
+impl RTPSWriter<RtpsUdpPsm, RTPSHistoryCacheImpl> for RTPSStatelessWriterImpl {
+    fn push_mode(&self) -> bool {
+        todo!()
+    }
 
-//     fn heartbeat_period(&self) -> <RtpsUdpPsm as rust_rtps_pim::behavior::Types>::Duration {
-//         todo!()
-//     }
+    fn heartbeat_period(&self) -> <RtpsUdpPsm as rust_rtps_pim::behavior::Types>::Duration {
+        todo!()
+    }
 
-//     fn nack_response_delay(&self) -> <RtpsUdpPsm as rust_rtps_pim::behavior::Types>::Duration {
-//         todo!()
-//     }
+    fn nack_response_delay(&self) -> <RtpsUdpPsm as rust_rtps_pim::behavior::Types>::Duration {
+        todo!()
+    }
 
-//     fn nack_suppression_duration(
-//         &self,
-//     ) -> <RtpsUdpPsm as rust_rtps_pim::behavior::Types>::Duration {
-//         todo!()
-//     }
+    fn nack_suppression_duration(
+        &self,
+    ) -> <RtpsUdpPsm as rust_rtps_pim::behavior::Types>::Duration {
+        todo!()
+    }
 
-//     fn last_change_sequence_number(
-//         &self,
-//     ) -> <RtpsUdpPsm as rust_rtps_pim::structure::Types>::SequenceNumber {
-//         todo!()
-//     }
+    fn last_change_sequence_number(
+        &self,
+    ) -> <RtpsUdpPsm as rust_rtps_pim::structure::Types>::SequenceNumber {
+        todo!()
+    }
 
-//     fn data_max_size_serialized(&self) -> i32 {
-//         todo!()
-//     }
+    fn data_max_size_serialized(&self) -> i32 {
+        todo!()
+    }
 
-//     fn writer_cache(&self) -> &HistoryCacheImpl {
-//         todo!()
-//     }
+    fn writer_cache(&self) -> &RTPSHistoryCacheImpl {
+        todo!()
+    }
 
-//     fn writer_cache_mut(&mut self) -> &mut HistoryCacheImpl {
-//         todo!()
-//     }
+    fn writer_cache_mut(&mut self) -> &mut RTPSHistoryCacheImpl {
+        todo!()
+    }
 
-//     fn new_change(
-//         &mut self,
-//         _kind: ChangeKind,
-//         _data: <RtpsUdpPsm as rust_rtps_pim::structure::Types>::Data,
-//         _inline_qos: <RtpsUdpPsm as rust_rtps_pim::structure::Types>::ParameterVector,
-//         _handle: <RtpsUdpPsm as rust_rtps_pim::structure::Types>::InstanceHandle,
-//     ) -> structure::RTPSCacheChange<RtpsUdpPsm> {
-//         todo!()
-//     }
-// }
+    fn new_change(
+        &mut self,
+        _kind: ChangeKind,
+        _data: <RtpsUdpPsm as rust_rtps_pim::structure::Types>::Data,
+        _inline_qos: <RtpsUdpPsm as rust_rtps_pim::structure::Types>::ParameterVector,
+        _handle: <RtpsUdpPsm as rust_rtps_pim::structure::Types>::InstanceHandle,
+    ) -> rust_rtps_pim::structure::RTPSCacheChange<RtpsUdpPsm> {
+        todo!()
+    }
+}
 
-// impl RTPSEndpoint<RtpsUdpPsm> for StatelessDataWriterImpl {
-//     fn topic_kind(&self) -> TopicKind {
-//         todo!()
-//     }
+impl RTPSEndpoint<RtpsUdpPsm> for RTPSStatelessWriterImpl {
+    fn topic_kind(&self) -> TopicKind {
+        todo!()
+    }
 
-//     fn reliability_level(&self) -> ReliabilityKind {
-//         todo!()
-//     }
+    fn reliability_level(&self) -> ReliabilityKind {
+        todo!()
+    }
 
-//     fn unicast_locator_list(&self) -> &[<RtpsUdpPsm as rust_rtps_pim::structure::Types>::Locator] {
-//         todo!()
-//     }
+    fn unicast_locator_list(&self) -> &[<RtpsUdpPsm as rust_rtps_pim::structure::Types>::Locator] {
+        todo!()
+    }
 
-//     fn multicast_locator_list(
-//         &self,
-//     ) -> &[<RtpsUdpPsm as rust_rtps_pim::structure::Types>::Locator] {
-//         todo!()
-//     }
-// }
+    fn multicast_locator_list(
+        &self,
+    ) -> &[<RtpsUdpPsm as rust_rtps_pim::structure::Types>::Locator] {
+        todo!()
+    }
+}
 
-// impl RTPSStatelessWriter<RtpsUdpPsm, HistoryCacheImpl> for StatelessDataWriterImpl {
-//     fn reader_locator_add(
-//         &mut self,
-//         a_locator: <RtpsUdpPsm as rust_rtps_pim::structure::Types>::Locator,
-//     ) {
-//         let expects_inline_qos = false;
-//         self.reader_locators
-//             .push(RTPSReaderLocator::new(a_locator, expects_inline_qos));
-//     }
+impl RTPSStatelessWriter<RtpsUdpPsm, RTPSHistoryCacheImpl> for RTPSStatelessWriterImpl {
+    fn reader_locator_add(
+        &mut self,
+        a_locator: <RtpsUdpPsm as rust_rtps_pim::structure::Types>::Locator,
+    ) {
+        let expects_inline_qos = false;
+        self.reader_locators
+            .push(RTPSReaderLocator::new(a_locator, expects_inline_qos));
+    }
 
-//     fn reader_locator_remove(
-//         &mut self,
-//         a_locator: &<RtpsUdpPsm as rust_rtps_pim::structure::Types>::Locator,
-//     ) {
-//         self.reader_locators.retain(|x| x.locator() != a_locator)
-//     }
+    fn reader_locator_remove(
+        &mut self,
+        a_locator: &<RtpsUdpPsm as rust_rtps_pim::structure::Types>::Locator,
+    ) {
+        self.reader_locators.retain(|x| x.locator() != a_locator)
+    }
 
-//     fn reader_locators(
-//         &mut self,
-//     ) -> &mut [rust_rtps_pim::behavior::stateless_writer::RTPSReaderLocator<RtpsUdpPsm>] {
-//         &mut self.reader_locators
-//     }
-// }
+    fn reader_locators(
+        &mut self,
+    ) -> &mut [rust_rtps_pim::behavior::stateless_writer::RTPSReaderLocator<RtpsUdpPsm>] {
+        &mut self.reader_locators
+    }
+}
