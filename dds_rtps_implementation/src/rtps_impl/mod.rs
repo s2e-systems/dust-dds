235af854fb11b94664e301581024a68b3f2070d5
Joao Rebelo <2730147+jrebelo@users.noreply.github.com>
2021-04-22 11:33:39 +0200

    Delete unused subscriber group

--- dds_rtps_implementation/src/rtps_impl/mod.rs
+++ dds_rtps_implementation/src/rtps_impl/mod.rs
@@ -1,6 +1,5 @@
 pub mod rtps_participant_impl;
 pub mod rtps_writer_group_impl;
-pub mod rtps_subscriber_group_impl;
 // pub mod topic_impl;
 pub mod rtps_stateless_writer_impl;
 pub mod rtps_stateful_writer_impl;
