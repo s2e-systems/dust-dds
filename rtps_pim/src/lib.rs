7cb005fb9cc63480b954b2641336f807df3d57cc
Joao Rebelo <2730147+jrebelo@users.noreply.github.com>
2021-04-30 09:25:28 +0200

    Add a combined PIM trait

--- rtps_pim/src/lib.rs
+++ rtps_pim/src/lib.rs
@@ -5,6 +5,8 @@ pub mod messages;
 pub mod structure;
 // pub mod discovery;
 
+pub trait PIM: structure::Types + behavior::Types + messages::Types {}
+
 #[cfg(test)]
 #[macro_use]
 extern crate std;
