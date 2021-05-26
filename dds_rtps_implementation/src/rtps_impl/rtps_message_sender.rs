ef59363f39ec7773d3048669a5f990955ae4d40b
Joao Rebelo <2730147+jrebelo@users.noreply.github.com>
2021-05-26 14:00:20 +0200

    Add test functionality to send data

--- dds_rtps_implementation/src/rtps_impl/rtps_message_sender.rs
+++ dds_rtps_implementation/src/rtps_impl/rtps_message_sender.rs
@@ -3,19 +3,22 @@ use super::rtps_participant_impl::RTPSParticipantImpl;
 struct UdpRtpsMessageSender;
 
 impl UdpRtpsMessageSender {
-    pub fn send_data<PSM:super::PIM>(participant: &RTPSParticipantImpl<PSM>) {
+    pub fn send_data<PSM: super::PIM>(participant: &RTPSParticipantImpl<PSM>) {
         for writer_group in participant.writer_groups() {
             if let Some(writer_group_lock) = writer_group.try_lock() {
                 for writer in writer_group_lock.writer_list() {
                     if let Some(mut writer_lock) = writer.try_lock() {
-                        let (reader_locators, _writer_cache) =
+                        let (reader_locators, writer_cache) =
                             writer_lock.locators_and_writer_cache();
-                        for _reader_locator in reader_locators {
-                            // reader_locator.produce_messages::<RtpsDataSubmessageImpl<PSM>,RtpsGapSubmessageImpl<PSM>>(
-                            //     writer_cache,
-                            //     &mut |_, _| {},
-                            //     &mut |_, _| {},
-                            // );
+                        for reader_locator in reader_locators {
+                            let mut data_vec: Vec<PSM::DataSubmesage> = Vec::new();
+                            let mut gap_vec: Vec<PSM::GapSubmessage> = Vec::new();
+                            rust_rtps_pim::behavior::stateless_writer::produce_messages(
+                                reader_locator,
+                                writer_cache,
+                                &mut |_locator, data| data_vec.push(data),
+                                &mut |_locator, gap| gap_vec.push(gap),
+                            );
                         }
                     }
                 }
