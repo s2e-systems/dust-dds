600f9a1efb7d087562e68844834bcef11fd89afe
Joao Rebelo <2730147+jrebelo@users.noreply.github.com>
2021-07-08 16:52:28 +0200

    Add sample test for message_create

--- dds_rtps_implementation/src/utils/message_sender.rs
+++ dds_rtps_implementation/src/utils/message_sender.rs
@@ -1,5 +1,5 @@
 use rust_rtps_pim::{
-    behavior::stateless_writer::{RTPSReaderLocator, BestEffortBehavior},
+    behavior::stateless_writer::{BestEffortBehavior, RTPSReaderLocator},
     messages::{
         submessages::{
             AckNackSubmessagePIM, DataFragSubmessagePIM, DataSubmessagePIM, GapSubmessagePIM,
@@ -36,9 +36,10 @@ where
         + NackFragSubmessagePIM
         + PadSubmessagePIM,
 
-        HistoryCache: RTPSHistoryCache,
-        <HistoryCache as rust_rtps_pim::structure::RTPSHistoryCache>::CacheChange: RTPSCacheChange,
-    ReaderLocator: BestEffortBehavior<'a, HistoryCache, PSM::DataSubmessageType, PSM::GapSubmessageType>,
+    HistoryCache: RTPSHistoryCache,
+    <HistoryCache as rust_rtps_pim::structure::RTPSHistoryCache>::CacheChange: RTPSCacheChange,
+    ReaderLocator:
+        BestEffortBehavior<'a, HistoryCache, PSM::DataSubmessageType, PSM::GapSubmessageType>,
 {
     let mut data_submessage_list: Vec<RtpsSubmessageType<'a, PSM>> = vec![];
     let mut gap_submessage_list: Vec<RtpsSubmessageType<'a, PSM>> = vec![];
@@ -77,7 +78,8 @@ pub fn send_data<HistoryCache, ReaderLocator, Transport>(
     Transport: TransportWrite,
 {
     for reader_locator in reader_locators {
-        let submessages = create_messages(writer_cache, reader_locator, last_change_sequence_number);
+        let submessages =
+            create_messages(writer_cache, reader_locator, last_change_sequence_number);
         let message = Transport::RTPSMessageType::new(header, submessages);
         transport.write(&message, reader_locator.locator());
     }
@@ -85,11 +87,41 @@ pub fn send_data<HistoryCache, ReaderLocator, Transport>(
 
 #[cfg(test)]
 mod tests {
+    use super::*;
+
     use rust_rtps_pim::structure::RTPSHistoryCache;
 
-    use crate::transport::TransportWrite;
-
     struct MockCacheChange;
+    impl RTPSCacheChange for MockCacheChange {
+        type DataType = Vec<u8>;
+        type InstanceHandleType = ();
+        type InlineQosType = ();
+
+        fn kind(&self) -> rust_rtps_pim::structure::types::ChangeKind {
+            todo!()
+        }
+
+        fn writer_guid(&self) -> &rust_rtps_pim::structure::types::GUID {
+            todo!()
+        }
+
+        fn instance_handle(&self) -> &Self::InstanceHandleType {
+            todo!()
+        }
+
+        fn sequence_number(&self) -> &SequenceNumber {
+            todo!()
+        }
+
+        fn data_value(&self) -> &Self::DataType {
+            todo!()
+        }
+
+        fn inline_qos(&self) -> &Self::InlineQosType {
+            todo!()
+        }
+    }
+
     struct MockHistoryCache;
     impl RTPSHistoryCache for MockHistoryCache {
         type CacheChange = MockCacheChange;
@@ -125,25 +157,84 @@ mod tests {
         }
     }
 
-    // struct MockTransport;
-    // impl TransportWrite for MockTransport {
-    //     type RtpsMessageHeaderType = ();
-    //     type RTPSMessageType = ();
+    #[derive(Debug, PartialEq)]
+    struct MockPSM;
 
-    //     fn write<'a>(
-    //     &mut self,
-    //     message: &rust_rtps_pim::messages::RTPSMessage::Constructed,
-    //     destination_locator: &rust_rtps_pim::structure::types::Locator,
-    // ) {
-    //     todo!()
-    // }
-    // }
+    impl AckNackSubmessagePIM for MockPSM {
+        type AckNackSubmessageType = ();
+    }
+
+    impl<'a> DataSubmessagePIM<'a> for MockPSM {
+        type DataSubmessageType = ();
+    }
+    impl DataFragSubmessagePIM for MockPSM {
+        type DataFragSubmessageType = ();
+    }
+    impl GapSubmessagePIM for MockPSM {
+        type GapSubmessageType = u8;
+    }
+    impl HeartbeatSubmessagePIM for MockPSM {
+        type HeartbeatSubmessageType = ();
+    }
+    impl HeartbeatFragSubmessagePIM for MockPSM {
+        type HeartbeatFragSubmessageType = ();
+    }
+    impl InfoDestinationSubmessagePIM for MockPSM {
+        type InfoDestinationSubmessageType = ();
+    }
+    impl InfoReplySubmessagePIM for MockPSM {
+        type InfoReplySubmessageType = ();
+    }
+    impl InfoSourceSubmessagePIM for MockPSM {
+        type InfoSourceSubmessageType = ();
+    }
+    impl InfoTimestampSubmessagePIM for MockPSM {
+        type InfoTimestampSubmessageType = ();
+    }
+    impl NackFragSubmessagePIM for MockPSM {
+        type NackFragSubmessageType = ();
+    }
+    impl PadSubmessagePIM for MockPSM {
+        type PadSubmessageType = ();
+    }
 
     #[test]
     fn send_data_happy() {
+        struct MockReaderLocator;
+        impl<'a, HistoryCache> BestEffortBehavior<'a, HistoryCache, (), u8> for MockReaderLocator {
+            fn best_effort_send_unsent_data(
+                &mut self,
+                _last_change_sequence_number: &SequenceNumber,
+                _writer_cache: &'a HistoryCache,
+                mut send_data: impl FnMut(()),
+                mut send_gap: impl FnMut(u8),
+            ) {
+                send_data(());
+                send_data(());
+                send_data(());
+                send_gap(1);
+                send_gap(2);
+            }
+        }
+
         let writer_cache = MockHistoryCache;
-        let reader_locators = [0];
+        let mut reader_locator = MockReaderLocator;
         let last_change_sequence_number = 1_i64;
+        let submessages = create_messages::<MockPSM, _, _>(
+            &writer_cache,
+            &mut reader_locator,
+            last_change_sequence_number,
+        );
+        assert_eq!(
+            submessages,
+            vec![
+                RtpsSubmessageType::Data(()),
+                RtpsSubmessageType::Data(()),
+                RtpsSubmessageType::Data(()),
+                RtpsSubmessageType::Gap(1),
+                RtpsSubmessageType::Gap(2),
+            ]
+        )
         // let transport = MockTransport;
     }
 }
