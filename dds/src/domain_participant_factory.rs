23a8d59777d97d6beb6880b1e7b01539c1924c6b
Joao Rebelo <2730147+jrebelo@users.noreply.github.com>
2021-07-12 11:10:16 +0200

    Move send function to message_sender

--- dds/src/domain_participant_factory.rs
+++ dds/src/domain_participant_factory.rs
@@ -1,7 +1,7 @@
-use std::{cell::{Cell, RefCell}, sync::{
+use std::sync::{
     atomic::{self, AtomicBool},
     Arc,
-}};
+};
 
 use rust_dds_api::{
     dcps_psm::{DomainId, StatusMask},
@@ -13,19 +13,10 @@ use rust_dds_rtps_implementation::{
     rtps_impl::rtps_participant_impl::RTPSParticipantImpl, utils::shared_object::RtpsShared,
 };
 use rust_rtps_pim::{
-    behavior::stateless_writer::{RTPSReaderLocator, RTPSStatelessWriter, StatelessWriterBehavior},
-    messages::{
-        submessages::{RtpsSubmessagePIM, RtpsSubmessageType},
-        RTPSMessage, RtpsMessageHeader,
-    },
-    structure::{
-        types::{Locator, LOCATOR_INVALID},
-        RTPSEntity, RTPSParticipant,
-    },
+    messages::{RTPSMessage, RtpsMessageHeader},
+    structure::{RTPSEntity, RTPSParticipant},
 };
-use rust_rtps_udp_psm::{
-    message::RTPSMessageUdp, psm::RtpsUdpPsm, submessages::gap::GapSubmessageUdp,
-};
+use rust_rtps_udp_psm::{message::RTPSMessageUdp, psm::RtpsUdpPsm};
 
 use crate::udp_transport::UdpTransport;
 
@@ -112,7 +103,7 @@ impl DomainParticipantFactory {
                         for writer in writer_group.writer_list() {
                             let mut writer = writer.lock();
                             let destined_submessages =
-                                create_submessages::<RtpsUdpPsm, _>(&mut *writer);
+                                rust_dds_rtps_implementation::utils::message_sender::create_submessages::<RtpsUdpPsm, _>(&mut *writer);
                             for (dst_locator, submessages) in destined_submessages {
                                 let message = RTPSMessageUdp::new(&header, submessages);
                                 transport.write(&message, &dst_locator);
@@ -140,92 +131,4 @@ impl DomainParticipantFactory {
     }
 
     // pub fn delete_participant(_a_participant: impl DomainParticipant) {}
-}
-
-pub fn create_submessages<'a, PSM, Behavior>(
-    writer: Behavior,
-) -> Vec<(Locator, Vec<RtpsSubmessageType<'a, PSM>>)>
-where
-    PSM: RtpsSubmessagePIM<'a>,
-    Behavior: StatelessWriterBehavior<PSM::DataSubmessageType, PSM::GapSubmessageType>,
-    Behavior::ReaderLocator: RTPSReaderLocator,
-{
-    let mut dst_locator = LOCATOR_INVALID;
-    let submessages = RefCell::new(Vec::new());
-    writer.send_unsent_data(
-        |reader_locator, data| {
-            dst_locator = *reader_locator.locator();
-            submessages.borrow_mut().push(RtpsSubmessageType::<PSM>::Data(data));
-        },
-        |_reader_locator, gap| {
-            submessages.borrow_mut().push(
-                // *reader_locator.locator(),
-                RtpsSubmessageType::<PSM>::Gap(gap),
-            );
-        },
-    );
-    vec![(dst_locator, submessages.take())]
-}
-
-#[cfg(test)]
-mod tests {
-    use super::*;
-
-    #[derive(PartialEq, Debug)]
-    struct MockPSM;
-
-    impl<'a> RtpsSubmessagePIM<'a> for MockPSM {
-        type AckNackSubmessageType = ();
-        type DataSubmessageType = u8;
-        type DataFragSubmessageType = ();
-        type GapSubmessageType = ();
-        type HeartbeatSubmessageType = ();
-        type HeartbeatFragSubmessageType = ();
-        type InfoDestinationSubmessageType = ();
-        type InfoReplySubmessageType = ();
-        type InfoSourceSubmessageType = ();
-        type InfoTimestampSubmessageType = ();
-        type NackFragSubmessageType = ();
-        type PadSubmessageType = ();
-    }
-
-    struct MockReaderLocator;
-
-    impl RTPSReaderLocator for MockReaderLocator {
-        fn locator(&self) -> &rust_rtps_pim::structure::types::Locator {
-            &LOCATOR_INVALID
-        }
-
-        fn expects_inline_qos(&self) -> bool {
-            todo!()
-        }
-    }
-
-    #[test]
-    fn message_send_test() {
-        struct MockBehavior;
-
-        impl StatelessWriterBehavior<u8, ()> for MockBehavior {
-            type ReaderLocator = MockReaderLocator;
-
-            fn send_unsent_data(
-                self,
-                mut send_data: impl FnMut(&Self::ReaderLocator, u8),
-                _send_gap: impl FnMut(&Self::ReaderLocator, ()),
-            ) {
-                send_data(&MockReaderLocator, 0);
-                send_data(&MockReaderLocator, 2);
-            }
-        }
-
-        let writer = MockBehavior;
-        let destined_submessages = create_submessages::<MockPSM, _>(writer);
-        let (dst_locator, submessages) = &destined_submessages[0];
-
-        assert_eq!(dst_locator, &LOCATOR_INVALID);
-        assert_eq!(
-            submessages,
-            &vec![RtpsSubmessageType::Data(0), RtpsSubmessageType::Data(2)]
-        );
-    }
 }
--- dds_rtps_implementation/src/utils/message_sender.rs
+++ dds_rtps_implementation/src/utils/message_sender.rs
@@ -1,164 +1,97 @@
-// use rust_rtps_pim::{
-//     behavior::{
-//         stateless_writer::{BestEffortBehavior, RTPSReaderLocator, RTPSStatelessWriter},
-//         RTPSWriter,
-//     },
-//     messages::{
-//         submessages::{RtpsSubmessagePIM, RtpsSubmessageType},
-//         RTPSMessage, RtpsMessageHeader,
-//     },
-//     structure::types::{Locator, SequenceNumber},
-// };
+use std::cell::RefCell;
 
-// // pub trait ReaderLocatorMessageSender<'a, HistoryCache, PSM>
-// // where
-// //     PSM: RtpsSubmessagePIM<'a>,
-// // {
-// //     fn create_submessages(
-// //         &mut self,
-// //         writer_cache: &'a HistoryCache,
-// //         last_change_sequence_number: SequenceNumber,
-// //     ) -> Vec<RtpsSubmessageType<'a, PSM>>;
-// // }
+use rust_rtps_pim::{
+    behavior::stateless_writer::{RTPSReaderLocator, StatelessWriterBehavior},
+    messages::submessages::{RtpsSubmessagePIM, RtpsSubmessageType},
+    structure::types::{Locator, LOCATOR_INVALID},
+};
 
-// // impl<'a, HistoryCache, PSM, T> ReaderLocatorMessageSender<'a, HistoryCache, PSM> for T
-// // where
-// //     PSM: RtpsSubmessagePIM<'a>,
-// //     T: BestEffortBehavior<'a, HistoryCache, PSM::DataSubmessageType, PSM::GapSubmessageType> + 'a,
-// // {
-// //     fn create_submessages(
-// //         &mut self,
-// //         writer_cache: &'a HistoryCache,
-// //         last_change_sequence_number: SequenceNumber,
-// //     ) -> Vec<RtpsSubmessageType<'a, PSM>> {
-// //         let mut data_submessage_list: Vec<RtpsSubmessageType<'a, PSM>> = vec![];
-// //         let mut gap_submessage_list: Vec<RtpsSubmessageType<'a, PSM>> = vec![];
+pub fn create_submessages<'a, PSM, StatelessWriter>(
+    writer: StatelessWriter,
+) -> Vec<(Locator, Vec<RtpsSubmessageType<'a, PSM>>)>
+where
+    PSM: RtpsSubmessagePIM<'a>,
+    StatelessWriter: StatelessWriterBehavior<PSM::DataSubmessageType, PSM::GapSubmessageType>,
+    StatelessWriter::ReaderLocator: RTPSReaderLocator,
+{
+    let mut dst_locator = LOCATOR_INVALID;
+    let submessages = RefCell::new(Vec::new());
+    writer.send_unsent_data(
+        |reader_locator, data| {
+            dst_locator = *reader_locator.locator();
+            submessages
+                .borrow_mut()
+                .push(RtpsSubmessageType::<PSM>::Data(data));
+        },
+        |_reader_locator, gap| {
+            submessages.borrow_mut().push(
+                // *reader_locator.locator(),
+                RtpsSubmessageType::<PSM>::Gap(gap),
+            );
+        },
+    );
+    vec![(dst_locator, submessages.take())]
+}
 
-// //         let mut submessages = vec![];
+#[cfg(test)]
+mod tests {
+    use super::*;
 
-// //         self.best_effort_send_unsent_data(
-// //             &last_change_sequence_number,
-// //             writer_cache,
-// //             |data_submessage| data_submessage_list.push(RtpsSubmessageType::Data(data_submessage)),
-// //             |gap_submessage| gap_submessage_list.push(RtpsSubmessageType::Gap(gap_submessage)),
-// //         );
+    #[derive(PartialEq, Debug)]
+    struct MockPSM;
 
-// //         for data_submessage in data_submessage_list {
-// //             submessages.push(data_submessage)
-// //         }
-// //         for gap_submessage in gap_submessage_list {
-// //             submessages.push(gap_submessage);
-// //         }
-// //         submessages
-// //     }
-// // }
+    impl<'a> RtpsSubmessagePIM<'a> for MockPSM {
+        type AckNackSubmessageType = ();
+        type DataSubmessageType = u8;
+        type DataFragSubmessageType = ();
+        type GapSubmessageType = ();
+        type HeartbeatSubmessageType = ();
+        type HeartbeatFragSubmessageType = ();
+        type InfoDestinationSubmessageType = ();
+        type InfoReplySubmessageType = ();
+        type InfoSourceSubmessageType = ();
+        type InfoTimestampSubmessageType = ();
+        type NackFragSubmessageType = ();
+        type PadSubmessageType = ();
+    }
 
-// // pub trait StatelessWriterMessageSender<Message, Sender> {
-// //     fn create_messages(self, header: &RtpsMessageHeader, send_message: Sender) -> Vec<Message>;
-// // }
+    struct MockReaderLocator;
 
-// // impl<'a, Message, Sender, PSM, T> StatelessWriterMessageSender<Message, Sender> for &'a mut T
-// // where
-// //     T: RTPSStatelessWriter + RTPSWriter,
-// //     Sender: Fn(Message, &'a Locator),
-// //     PSM: RtpsSubmessagePIM<'a>,
-// //     T::ReaderLocatorType: RTPSReaderLocator,
-// //     T::ReaderLocatorType: ReaderLocatorMessageSender<'a, T::HistoryCacheType, PSM>,
-// //     Message: RTPSMessage<SubmessageType = RtpsSubmessageType<'a, PSM>>,
-// // {
-// //     fn create_messages(self, header: &RtpsMessageHeader, send_message: Sender) -> Vec<Message> {
-// //         let mut messages = vec![];
-// //         let last_change_sequence_number = *self.last_change_sequence_number();
-// //         let (writer_cache, reader_locators) = self.writer_cache_and_reader_locators();
-// //         for reader_locator in reader_locators {
-// //             let submessages =
-// //                 reader_locator.create_submessages(writer_cache, last_change_sequence_number);
-// //             let message = Message::new(header, submessages);
-// //             messages.push(message);
-// //             // send_message(message, reader_locator.locator())
-// //         }
-// //         messages
-// //     }
-// // }
-// // impl<T> StatelessWriterMessageSender for T
-// // where
-// //     T: RTPSStatelessWriter + RTPSWriter,
-// //     T::ReaderLocatorType:
-// //         RTPSReaderLocator + for<'a> ReaderLocatorMessageSender<'a, T::HistoryCacheType>,
-// // {
-// //     fn send_data(&mut self, header: &RtpsMessageHeader, transport: &mut Transport) {
-// //         let last_change_sequence_number = *self.last_change_sequence_number();
-// //         let (writer_cache, reader_locators) = self.writer_cache_and_reader_locators();
-// //         for reader_locator in reader_locators {
-// //             let submessages =
-// //                 reader_locator.create_messages(writer_cache, last_change_sequence_number);
-// //             let message = Transport::RTPSMessageType::new(header, submessages);
-// //             transport.write(&message, reader_locator.locator());
-// //         }
-// //     }
-// // }
+    impl RTPSReaderLocator for MockReaderLocator {
+        fn locator(&self) -> &rust_rtps_pim::structure::types::Locator {
+            &LOCATOR_INVALID
+        }
 
-// #[cfg(test)]
-// mod tests {
-//     use super::*;
+        fn expects_inline_qos(&self) -> bool {
+            todo!()
+        }
+    }
 
-//     #[derive(Debug, PartialEq)]
-//     struct MockDataSubmessage;
-//     #[derive(Debug, PartialEq)]
-//     struct MockGapSubmessage;
+    #[test]
+    fn message_send_test() {
+        struct MockBehavior;
 
-//     #[derive(Debug, PartialEq)]
-//     struct MockPSM;
+        impl StatelessWriterBehavior<u8, ()> for MockBehavior {
+            type ReaderLocator = MockReaderLocator;
 
-//     impl<'a> RtpsSubmessagePIM<'a> for MockPSM {
-//         type AckNackSubmessageType = ();
-//         type DataSubmessageType = MockDataSubmessage;
-//         type DataFragSubmessageType = ();
-//         type GapSubmessageType = MockGapSubmessage;
-//         type HeartbeatSubmessageType = ();
-//         type HeartbeatFragSubmessageType = ();
-//         type InfoDestinationSubmessageType = ();
-//         type InfoReplySubmessageType = ();
-//         type InfoSourceSubmessageType = ();
-//         type InfoTimestampSubmessageType = ();
-//         type NackFragSubmessageType = ();
-//         type PadSubmessageType = ();
-//     }
+            fn send_unsent_data(
+                self,
+                mut send_data: impl FnMut(&Self::ReaderLocator, u8),
+                _send_gap: impl FnMut(&Self::ReaderLocator, ()),
+            ) {
+                send_data(&MockReaderLocator, 0);
+                send_data(&MockReaderLocator, 2);
+            }
+        }
 
-//     #[test]
-//     fn reader_locator_send_data_happy() {
-//         struct MockReaderLocator;
-//         impl<'a, HistoryCache>
-//             BestEffortBehavior<'a, HistoryCache, MockDataSubmessage, MockGapSubmessage>
-//             for MockReaderLocator
-//         {
-//             fn best_effort_send_unsent_data(
-//                 &mut self,
-//                 _last_change_sequence_number: &SequenceNumber,
-//                 _writer_cache: &'a HistoryCache,
-//                 mut send_data: impl FnMut(MockDataSubmessage),
-//                 mut send_gap: impl FnMut(MockGapSubmessage),
-//             ) {
-//                 send_data(MockDataSubmessage);
-//                 send_data(MockDataSubmessage);
-//                 send_data(MockDataSubmessage);
-//                 send_gap(MockGapSubmessage);
-//                 send_gap(MockGapSubmessage);
-//             }
-//         }
-//         let mut reader_locator = MockReaderLocator;
-//         let last_change_sequence_number = 1_i64;
-//         let submessages: Vec<RtpsSubmessageType<MockPSM>> =
-//             reader_locator.create_submessages(&(), last_change_sequence_number);
-//         assert_eq!(
-//             submessages,
-//             vec![
-//                 RtpsSubmessageType::Data(MockDataSubmessage),
-//                 RtpsSubmessageType::Data(MockDataSubmessage),
-//                 RtpsSubmessageType::Data(MockDataSubmessage),
-//                 RtpsSubmessageType::Gap(MockGapSubmessage),
-//                 RtpsSubmessageType::Gap(MockGapSubmessage),
-//             ]
-//         )
-//     }
-// }
+        let writer = MockBehavior;
+        let destined_submessages = create_submessages::<MockPSM, _>(writer);
+        let (dst_locator, submessages) = &destined_submessages[0];
+
+        assert_eq!(dst_locator, &LOCATOR_INVALID);
+        assert_eq!(
+            submessages,
+            &vec![RtpsSubmessageType::Data(0), RtpsSubmessageType::Data(2)]
+        );
+    }
+}
