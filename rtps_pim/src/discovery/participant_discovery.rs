5f7ca99a6fb50bfb3a630c502f0fe53e8f132325
Joao Rebelo <2730147+jrebelo@users.noreply.github.com>
2021-07-26 15:38:44 +0200

    Add subscriptions discovery

--- rtps_pim/src/discovery/participant_discovery.rs
+++ rtps_pim/src/discovery/participant_discovery.rs
@@ -15,7 +15,11 @@ use crate::{
 
 use super::{
     sedp::{
-        builtin_endpoints::ENTITYID_SEDP_BUILTIN_PUBLICATIONS_ANNOUNCER,
+        builtin_endpoints::{
+            ENTITYID_SEDP_BUILTIN_PUBLICATIONS_ANNOUNCER,
+            ENTITYID_SEDP_BUILTIN_SUBSCRIPTIONS_ANNOUNCER,
+            ENTITYID_SEDP_BUILTIN_SUBSCRIPTIONS_DETECTOR,
+        },
         sedp_participant::SedpParticipant,
     },
     spdp::spdp_discovered_participant_data::SPDPdiscoveredParticipantData,
@@ -34,6 +38,12 @@ pub fn discover_new_remote_participant<Participant, ParticipantData>(
         RTPSReaderProxyOperations,
     Participant::BuiltinPublicationsReader: RTPSStatefulReaderOperations + RTPSStatefulReader,
     <Participant::BuiltinPublicationsReader as RTPSStatefulReader>::WriterProxyType:
+        RTPSWriterProxyOperations,
+    Participant::BuiltinSubscriptionsWriter: RTPSStatefulWriterOperations + RTPSStatefulWriter,
+    <Participant::BuiltinSubscriptionsWriter as RTPSStatefulWriter>::ReaderProxyType:
+        RTPSReaderProxyOperations,
+    Participant::BuiltinSubscriptionsReader: RTPSStatefulReaderOperations + RTPSStatefulReader,
+    <Participant::BuiltinSubscriptionsReader as RTPSStatefulReader>::WriterProxyType:
         RTPSWriterProxyOperations,
 {
     // Check that the domainId of the discovered participant equals the local one.
@@ -98,6 +108,57 @@ pub fn discover_new_remote_participant<Participant, ParticipantData>(
                 data_max_size_serialized,
             );
             sedp_builtin_publications_reader.matched_writer_add(proxy);
+        }
+    }
+
+    if participant_data
+        .available_builtin_endpoints()
+        .has(BuiltinEndpointSet::BUILTIN_ENDPOINT_SUBSCRIPTIONS_DETECTOR)
+    {
+        if let Some(sedp_builtin_subscriptions_writer) =
+            local_participant.sedp_builtin_subscriptions_writer()
+        {
+            let guid = GUID::new(
+                participant_data.guid_prefix(),
+                ENTITYID_SEDP_BUILTIN_SUBSCRIPTIONS_DETECTOR,
+            );
+            let remote_group_entity_id = ENTITYID_UNKNOWN;
+            let expects_inline_qos = false;
+            let is_active = true;
+            let proxy = RTPSReaderProxyOperations::new(
+                guid,
+                remote_group_entity_id,
+                participant_data.metatraffic_unicast_locator_list(),
+                participant_data.metatraffic_multicast_locator_list(),
+                expects_inline_qos,
+                is_active,
+            );
+            sedp_builtin_subscriptions_writer.matched_reader_add(proxy);
+        }
+    }
+
+    if participant_data
+        .available_builtin_endpoints()
+        .has(BuiltinEndpointSet::BUILTIN_ENDPOINT_SUBSCRIPTIONS_ANNOUNCER)
+    {
+        if let Some(sedp_builtin_subscriptions_reader) =
+            local_participant.sedp_builtin_subscriptions_reader()
+        {
+            let guid = GUID::new(
+                participant_data.guid_prefix(),
+                ENTITYID_SEDP_BUILTIN_SUBSCRIPTIONS_ANNOUNCER,
+            );
+            let remote_group_entity_id = ENTITYID_UNKNOWN;
+            let data_max_size_serialized = None;
+
+            let proxy = RTPSWriterProxyOperations::new(
+                guid,
+                remote_group_entity_id,
+                participant_data.metatraffic_unicast_locator_list(),
+                participant_data.metatraffic_multicast_locator_list(),
+                data_max_size_serialized,
+            );
+            sedp_builtin_subscriptions_reader.matched_writer_add(proxy);
         }
     }
 }
