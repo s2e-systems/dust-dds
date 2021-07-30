18900978b93ab32a0fff02f3083cba72ddface0f
Stefan Kimmer <skimmer@s2e-systems.com>
2021-07-30 14:09:34 +0200

    Revert "add more serde_derives"
    This reverts commit a357e1fcd0dbc2484b13a96f01eca350f5447f11.

--- rtps_pim/src/structure/types.rs
+++ rtps_pim/src/structure/types.rs
@@ -44,9 +44,9 @@ pub const ENTITYID_PARTICIPANT: EntityId = EntityId {
 pub type SequenceNumber = i64;
 pub const SEQUENCENUMBER_UNKNOWN: SequenceNumber = i64::MIN;
 
-pub type LocatorKind = i32;
-pub type LocatorPort = u32;
-pub type LocatorAddress = [u8; 16];
+type LocatorKind = i32;
+type LocatorPort = u32;
+type LocatorAddress = [u8; 16];
 
 pub const LOCATOR_KIND_INVALID: LocatorKind = -1;
 pub const LOCATOR_KIND_RESERVED: LocatorKind = 0;
@@ -115,6 +115,11 @@ pub struct GUID {
     entity_id: EntityId,
 }
 
+pub const GUID_UNKNOWN: GUID = GUID {
+    prefix: GUIDPREFIX_UNKNOWN,
+    entity_id: ENTITYID_UNKNOWN,
+};
+
 impl GUID {
     pub fn new(prefix: GuidPrefix, entity_id: EntityId) -> Self {
         Self { prefix, entity_id }
@@ -128,11 +133,6 @@ impl GUID {
         &self.entity_id
     }
 }
-
-pub const GUID_UNKNOWN: GUID = GUID {
-    prefix: GUIDPREFIX_UNKNOWN,
-    entity_id: ENTITYID_UNKNOWN,
-};
 
 #[derive(Clone, Copy, PartialEq, Debug)]
 pub enum TopicKind {
--- rtps_udp_psm/src/builtin_endpoints/serde_derives.rs
+++ rtps_udp_psm/src/builtin_endpoints/serde_derives.rs
@@ -1,10 +1,9 @@
-use rust_rtps_pim::structure::types::{EntityId, EntityKind, GuidPrefix, ProtocolVersion, GUID, Locator, LocatorKind, LocatorPort, LocatorAddress};
+use rust_rtps_pim::structure::types::{EntityId, EntityKind};
 use rust_rtps_pim::behavior::types::Duration;
-use rust_rtps_pim::messages::types::Count;
 
 #[derive(serde::Serialize)]
 #[serde(remote = "EntityKind")]
-pub enum EntityKindDef {
+enum EntityKindDef {
     UserDefinedUnknown,
     BuiltInUnknown,
     BuiltInParticipant,
@@ -24,7 +23,7 @@ pub enum EntityKindDef {
 
 #[derive(serde::Serialize)]
 #[serde(remote = "EntityId")]
-pub struct EntityIdDef {
+struct EntityIdDef {
     entity_key: [u8; 3],
     #[serde(with = "EntityKindDef")]
     entity_kind: EntityKind,
@@ -32,38 +31,7 @@ pub struct EntityIdDef {
 
 #[derive(serde::Serialize)]
 #[serde(remote = "Duration")]
-pub struct DurationDef {
+struct DurationDef {
     seconds: i32,
     fraction: u32,
 }
-
-#[derive(serde::Serialize)]
-#[serde(remote = "ProtocolVersion")]
-pub struct ProtocolVersionDef {
-    major: u8,
-    minor: u8,
-}
-#[derive(serde::Serialize)]
-#[serde(remote = "GUID")]
-pub struct GuidDef {
-    #[serde(getter = "GUID::prefix")]
-    prefix: GuidPrefix,
-    #[serde(getter = "GUID::entity_id", with = "EntityIdDef")]
-    entity_id: EntityId,
-}
-
-#[derive(serde::Serialize)]
-#[serde(remote = "Locator")]
-pub struct LocatorDef {
-    #[serde(getter = "Locator::kind")]
-    kind: LocatorKind,
-    #[serde(getter = "Locator::port")]
-    port: LocatorPort,
-    #[serde(getter = "Locator::address")]
-    address: LocatorAddress,
-}
-
-#[derive(serde::Serialize)]
-#[serde(remote = "Count")]
-pub struct CountDef(i32);
-
