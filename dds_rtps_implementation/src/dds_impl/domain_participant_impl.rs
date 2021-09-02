f9df10e2bb546395d4f3187504e81d374f0f039e
Joao Rebelo <2730147+jrebelo@users.noreply.github.com>
2021-09-02 14:17:18 +0200

    Clean-up of obsolete methods on participant

--- dds_rtps_implementation/src/dds_impl/domain_participant_impl.rs
+++ dds_rtps_implementation/src/dds_impl/domain_participant_impl.rs
@@ -83,99 +83,6 @@ impl DomainParticipantImpl {
         }
     }
 
-    pub fn create_publisher(
-        &mut self,
-        qos: Option<PublisherQos>,
-        _a_listener: Option<&'static dyn PublisherListener>,
-        _mask: StatusMask,
-    ) -> Option<RtpsWeak<PublisherImpl>> {
-        todo!()
-        // let publisher_qos = qos.unwrap_or(self.default_publisher_qos.clone());
-        // self.user_defined_publisher_counter += 1;
-        // let entity_id = EntityId::new(
-        //     [self.user_defined_publisher_counter, 0, 0],
-        //     EntityKind::UserDefinedWriterGroup,
-        // );
-        // let guid = Guid::new(*self.rtps_participant.guid().prefix(), entity_id);
-        // let rtps_group = RtpsGroupImpl::new(guid);
-        // let data_writer_storage_list = Vec::new();
-        // let publisher_storage =
-        //     PublisherImpl::new(publisher_qos, rtps_group, data_writer_storage_list);
-        // let publisher_storage_shared = RtpsShared::new(publisher_storage);
-        // let publisher_storage_weak = publisher_storage_shared.downgrade();
-        // self.user_defined_publisher_storage
-        //     .push(publisher_storage_shared);
-        // Some(publisher_storage_weak)
-    }
-
-    pub fn delete_publisher(&mut self, a_publisher: &RtpsWeak<PublisherImpl>) -> DDSResult<()> {
-        let publisher_storage = a_publisher.upgrade()?;
-        self.user_defined_publisher_storage
-            .retain(|x| x != &publisher_storage);
-        Ok(())
-    }
-
-    pub fn create_subscriber(
-        &mut self,
-        qos: Option<SubscriberQos>,
-        _a_listener: Option<&'static dyn SubscriberListener>,
-        _mask: StatusMask,
-    ) -> Option<RtpsWeak<SubscriberImpl>> {
-        todo!()
-        // let subscriber_qos = qos.unwrap_or(self.default_subscriber_qos.clone());
-        // self.user_defined_subscriber_counter += 1;
-        // let entity_id = EntityId::new(
-        //     [self.user_defined_subscriber_counter, 0, 0],
-        //     EntityKind::UserDefinedWriterGroup,
-        // );
-        // let guid = Guid::new(*self.rtps_participant.guid().prefix(), entity_id);
-        // let rtps_group = RtpsGroupImpl::new(guid);
-        // let data_reader_storage_list = Vec::new();
-        // let subscriber_storage =
-        //     SubscriberImpl::new(subscriber_qos, rtps_group, data_reader_storage_list);
-        // let subscriber_storage_shared = RtpsShared::new(subscriber_storage);
-        // let subscriber_storage_weak = subscriber_storage_shared.downgrade();
-        // self.user_defined_subscriber_storage
-        //     .push(subscriber_storage_shared);
-        // Some(subscriber_storage_weak)
-    }
-
-    pub fn delete_subscriber(&mut self, a_subscriber: &RtpsWeak<SubscriberImpl>) -> DDSResult<()> {
-        let subscriber_storage = a_subscriber.upgrade()?;
-        self.user_defined_subscriber_storage
-            .retain(|x| x != &subscriber_storage);
-        Ok(())
-    }
-
-    pub fn get_builtin_subscriber(&self) -> RtpsWeak<SubscriberImpl> {
-        self.builtin_subscriber_storage[0].clone().downgrade()
-    }
-
-    pub fn create_topic<T>(
-        &mut self,
-        _topic_name: &str,
-        qos: Option<TopicQos>,
-        _a_listener: Option<&'static dyn TopicListener<DataPIM = T>>,
-        _mask: StatusMask,
-    ) -> Option<RtpsWeak<TopicImpl>> {
-        todo!()
-        // let topic_qos = qos.unwrap_or(self.default_topic_qos.clone());
-        // let topic_storage = TopicImpl::new(topic_qos);
-        // let topic_storage_shared = RtpsShared::new(topic_storage);
-        // let topic_storage_weak = topic_storage_shared.downgrade();
-        // self.topic_storage.push(topic_storage_shared);
-        // Some(topic_storage_weak)
-    }
-
-    pub fn set_qos(&mut self, qos: Option<DomainParticipantQos>) -> DDSResult<()> {
-        self.qos = qos.unwrap_or_default();
-        Ok(())
-    }
-
-    pub fn get_qos(&self) -> &DomainParticipantQos {
-        &self.qos
-    }
-
     pub fn send_builtin_data(&mut self) {
         // for publisher in &self.builtin_publisher_storage {
         //     let publisher_lock = publisher.lock();
@@ -227,31 +134,6 @@ impl DomainParticipantImpl {
             );
         }
     }
-
-    /// Get a reference to the domain participant storage's builtin subscriber storage.
-    pub fn builtin_subscriber_storage(&self) -> &[RtpsShared<SubscriberImpl>] {
-        &self.builtin_subscriber_storage
-    }
-
-    /// Get a reference to the domain participant storage's rtps participant.
-    pub fn rtps_participant(&self) -> &RtpsParticipantImpl {
-        &self.rtps_participant
-    }
-
-    /// Get a reference to the domain participant storage's builtin publisher storage.
-    pub fn builtin_publisher_storage(&self) -> &[RtpsShared<PublisherImpl>] {
-        &self.builtin_publisher_storage
-    }
-
-    /// Get a reference to the domain participant storage's user defined subscriber storage.
-    pub fn user_defined_subscriber_storage(&self) -> &[RtpsShared<SubscriberImpl>] {
-        self.user_defined_subscriber_storage.as_slice()
-    }
-
-    /// Get a reference to the domain participant storage's user defined publisher storage.
-    pub fn user_defined_publisher_storage(&self) -> &[RtpsShared<PublisherImpl>] {
-        self.user_defined_publisher_storage.as_slice()
-    }
 }
 
 impl<'p> PublisherGAT<'p> for DomainParticipantImpl {
@@ -262,6 +144,23 @@ impl<'p> PublisherGAT<'p> for DomainParticipantImpl {
         _a_listener: Option<&'static dyn PublisherListener>,
         _mask: StatusMask,
     ) -> Option<Self::PublisherType> {
+        // let publisher_qos = qos.unwrap_or(self.default_publisher_qos.clone());
+        // self.user_defined_publisher_counter += 1;
+        // let entity_id = EntityId::new(
+        //     [self.user_defined_publisher_counter, 0, 0],
+        //     EntityKind::UserDefinedWriterGroup,
+        // );
+        // let guid = Guid::new(*self.rtps_participant.guid().prefix(), entity_id);
+        // let rtps_group = RtpsGroupImpl::new(guid);
+        // let data_writer_storage_list = Vec::new();
+        // let publisher_storage =
+        //     PublisherImpl::new(publisher_qos, rtps_group, data_writer_storage_list);
+        // let publisher_storage_shared = RtpsShared::new(publisher_storage);
+        // let publisher_storage_weak = publisher_storage_shared.downgrade();
+        // self.user_defined_publisher_storage
+        //     .push(publisher_storage_shared);
+        // Some(publisher_storage_weak)
+
         // let publisher_storage_weak = self
         //     .domain_participant_storage
         //     .lock()
@@ -273,6 +172,11 @@ impl<'p> PublisherGAT<'p> for DomainParticipantImpl {
     }
 
     fn delete_publisher_gat(&self, _a_publisher: &Self::PublisherType) -> DDSResult<()> {
+        // let publisher_storage = a_publisher.upgrade()?;
+        // self.user_defined_publisher_storage
+        //     .retain(|x| x != &publisher_storage);
+        // Ok(())
+
         // if std::ptr::eq(a_publisher.get_participant(), self) {
         //     self.domain_participant_storage
         //         .lock()
@@ -295,6 +199,23 @@ impl<'s> SubscriberGAT<'s> for DomainParticipantImpl {
         _a_listener: Option<&'static dyn SubscriberListener>,
         _mask: StatusMask,
     ) -> Option<Self::SubscriberType> {
+        // let subscriber_qos = qos.unwrap_or(self.default_subscriber_qos.clone());
+        // self.user_defined_subscriber_counter += 1;
+        // let entity_id = EntityId::new(
+        //     [self.user_defined_subscriber_counter, 0, 0],
+        //     EntityKind::UserDefinedWriterGroup,
+        // );
+        // let guid = Guid::new(*self.rtps_participant.guid().prefix(), entity_id);
+        // let rtps_group = RtpsGroupImpl::new(guid);
+        // let data_reader_storage_list = Vec::new();
+        // let subscriber_storage =
+        //     SubscriberImpl::new(subscriber_qos, rtps_group, data_reader_storage_list);
+        // let subscriber_storage_shared = RtpsShared::new(subscriber_storage);
+        // let subscriber_storage_weak = subscriber_storage_shared.downgrade();
+        // self.user_defined_subscriber_storage
+        //     .push(subscriber_storage_shared);
+        // Some(subscriber_storage_weak)
+
         // let subscriber_storage_weak = self
         //     .domain_participant_storage
         //     .lock()
@@ -305,6 +226,11 @@ impl<'s> SubscriberGAT<'s> for DomainParticipantImpl {
     }
 
     fn delete_subscriber_gat(&self, _a_subscriber: &Self::SubscriberType) -> DDSResult<()> {
+        // let subscriber_storage = a_subscriber.upgrade()?;
+        // self.user_defined_subscriber_storage
+        //     .retain(|x| x != &subscriber_storage);
+        // Ok(())
+
         // if std::ptr::eq(a_subscriber.get_participant(), self) {
         //     self.domain_participant_storage
         //         .lock()
@@ -318,6 +244,8 @@ impl<'s> SubscriberGAT<'s> for DomainParticipantImpl {
     }
 
     fn get_builtin_subscriber_gat(&'s self) -> Self::SubscriberType {
+        // self.builtin_subscriber_storage[0].clone().downgrade()
+
         // let subscriber_storage_weak = self
         //     .domain_participant_storage
         //     .lock()
@@ -337,6 +265,13 @@ impl<'t, T: 'static> TopicGAT<'t, T> for DomainParticipantImpl {
         _a_listener: Option<&'static dyn TopicListener<DataPIM = T>>,
         _mask: StatusMask,
     ) -> Option<Self::TopicType> {
+        // let topic_qos = qos.unwrap_or(self.default_topic_qos.clone());
+        // let topic_storage = TopicImpl::new(topic_qos);
+        // let topic_storage_shared = RtpsShared::new(topic_storage);
+        // let topic_storage_weak = topic_storage_shared.downgrade();
+        // self.topic_storage.push(topic_storage_shared);
+        // Some(topic_storage_weak)
+
         // let topic_storage_weak = self
         //     .domain_participant_storage
         //     .lock()
@@ -462,6 +397,8 @@ impl Entity for DomainParticipantImpl {
     type Listener = &'static dyn DomainParticipantListener;
 
     fn set_qos(&self, _qos: Option<Self::Qos>) -> DDSResult<()> {
+        // self.qos = qos.unwrap_or_default();
+        // Ok(())
         todo!()
         // self.domain_participant_storage.lock().set_qos(qos)
     }
