fdad64467ecfc67c1d9433f1c9384d066dc57991
Joao Rebelo <2730147+jrebelo@users.noreply.github.com>
2021-09-02 15:20:49 +0200

    Add test for write w timestamp

--- dds_rtps_implementation/src/dds_impl/data_writer_impl.rs
+++ dds_rtps_implementation/src/dds_impl/data_writer_impl.rs
@@ -1,7 +1,11 @@
+use std::sync::Mutex;
+
 use rust_dds_api::{
     dcps_psm::InstanceHandle,
-    infrastructure::qos::DataWriterQos,
-    publication::{data_writer::DataWriter, publisher::Publisher},
+    infrastructure::{entity::Entity, qos::DataWriterQos},
+    publication::{
+        data_writer::DataWriter, data_writer_listener::DataWriterListener, publisher::Publisher,
+    },
     return_type::DDSResult,
     topic::topic::Topic,
 };
@@ -14,58 +18,22 @@ use crate::{dds_type::DDSType, rtps_impl::rtps_writer_impl::RtpsWriterImpl};
 
 pub struct DataWriterImpl {
     qos: DataWriterQos,
-    rtps_writer_impl: RtpsWriterImpl,
+    rtps_writer_impl: Mutex<RtpsWriterImpl>,
 }
 
 impl DataWriterImpl {
     pub fn new(qos: DataWriterQos, rtps_writer_impl: RtpsWriterImpl) -> Self {
         Self {
             qos,
-            rtps_writer_impl,
+            rtps_writer_impl: Mutex::new(rtps_writer_impl),
         }
-    }
-
-    /// Get a reference to the data writer storage's rtps data writer.
-    pub fn rtps_data_writer(&self) -> &RtpsWriterImpl {
-        &self.rtps_writer_impl
-    }
-
-    /// Get a mutable reference to the data writer storage's rtps data writer.
-    pub fn rtps_data_writer_mut(&mut self) -> &mut RtpsWriterImpl {
-        &mut self.rtps_writer_impl
-    }
-
-    pub fn set_qos(&mut self, qos: Option<DataWriterQos>) -> DDSResult<()> {
-        let qos = qos.unwrap_or_default();
-        qos.is_consistent()?;
-        self.qos = qos;
-        Ok(())
-    }
-
-    pub fn get_qos(&self) -> &DataWriterQos {
-        &self.qos
-    }
-
-    pub fn write_w_timestamp<T: DDSType + 'static>(
-        &mut self,
-        _data: T,
-        _handle: Option<InstanceHandle>,
-        _timestamp: rust_dds_api::dcps_psm::Time,
-    ) -> DDSResult<()> {
-        todo!()
-        // let data = cdr::serialize::<_, _, cdr::CdrLe>(&data, cdr::Infinite).unwrap();
-        // let change = self
-        //     .rtps_writer_impl
-        //     .new_change(ChangeKind::Alive, data.as_slice(), &[], 0);
-        // let writer_cache = self.rtps_writer_impl.writer_cache_mut();
-        // let time = rust_rtps_pim::messages::types::Time(0);
-        // writer_cache.set_source_timestamp(Some(time));
-        // writer_cache.add_change(&change);
-        // Ok(())
     }
 }
 
-impl<T> DataWriter<T> for DataWriterImpl {
+impl<T> DataWriter<T> for DataWriterImpl
+where
+    T: serde::Serialize,
+{
     fn register_instance(&self, _instance: T) -> DDSResult<Option<InstanceHandle>> {
         unimplemented!()
     }
@@ -105,11 +73,18 @@ impl<T> DataWriter<T> for DataWriterImpl {
 
     fn write_w_timestamp(
         &self,
-        _data: T,
+        data: T,
         _handle: Option<InstanceHandle>,
         _timestamp: rust_dds_api::dcps_psm::Time,
     ) -> DDSResult<()> {
-        todo!()
+        let data = cdr::serialize::<_, _, cdr::CdrLe>(&data, cdr::Infinite).unwrap();
+        let mut rtps_writer_impl_lock = self.rtps_writer_impl.lock().unwrap();
+        let change = rtps_writer_impl_lock.new_change(ChangeKind::Alive, data.as_slice(), &[], 0);
+        let writer_cache = rtps_writer_impl_lock.writer_cache_mut();
+        let time = rust_rtps_pim::messages::types::Time(0);
+        writer_cache.set_source_timestamp(Some(time));
+        writer_cache.add_change(&change);
+        Ok(())
     }
 
     fn dispose(&self, _data: T, _handle: Option<InstanceHandle>) -> DDSResult<()> {
@@ -185,5 +160,106 @@ impl<T> DataWriter<T> for DataWriterImpl {
         _subscription_handles: &mut [InstanceHandle],
     ) -> DDSResult<()> {
         todo!()
+    }
+}
+
+impl Entity for DataWriterImpl {
+    type Qos = DataWriterQos;
+    type Listener = &'static dyn DataWriterListener<DataPIM = ()>;
+
+    fn set_qos(&self, _qos: Option<Self::Qos>) -> DDSResult<()> {
+        // let qos = qos.unwrap_or_default();
+        // qos.is_consistent()?;
+        // self.qos = qos;
+        // Ok(())
+        todo!()
+    }
+
+    fn get_qos(&self) -> DDSResult<Self::Qos> {
+        // &self.qos
+        todo!()
+    }
+
+    fn set_listener(
+        &self,
+        _a_listener: Option<Self::Listener>,
+        _mask: rust_dds_api::dcps_psm::StatusMask,
+    ) -> DDSResult<()> {
+        todo!()
+    }
+
+    fn get_listener(&self) -> DDSResult<Option<Self::Listener>> {
+        todo!()
+    }
+
+    fn get_statuscondition(
+        &self,
+    ) -> DDSResult<rust_dds_api::infrastructure::entity::StatusCondition> {
+        todo!()
+    }
+
+    fn get_status_changes(&self) -> DDSResult<rust_dds_api::dcps_psm::StatusMask> {
+        todo!()
+    }
+
+    fn enable(&self) -> DDSResult<()> {
+        todo!()
+    }
+
+    fn get_instance_handle(&self) -> DDSResult<InstanceHandle> {
+        todo!()
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use rust_rtps_pim::{
+        behavior::writer::stateful_writer::RtpsStatefulWriterOperations,
+        structure::types::{ReliabilityKind, TopicKind, GUID_UNKNOWN},
+    };
+
+    use super::*;
+
+    #[test]
+    fn write_w_timestamp() {
+        let guid = GUID_UNKNOWN;
+        let topic_kind = TopicKind::WithKey;
+        let reliability_level = ReliabilityKind::BestEffort;
+        let unicast_locator_list = &[];
+        let multicast_locator_list = &[];
+        let push_mode = true;
+        let heartbeat_period = rust_rtps_pim::behavior::types::Duration::new(0, 200_000_000);
+        let nack_response_delay = rust_rtps_pim::behavior::types::DURATION_ZERO;
+        let nack_suppression_duration = rust_rtps_pim::behavior::types::DURATION_ZERO;
+        let data_max_size_serialized = None;
+        let rtps_writer = RtpsStatefulWriterOperations::new(
+            guid,
+            topic_kind,
+            reliability_level,
+            unicast_locator_list,
+            multicast_locator_list,
+            push_mode,
+            heartbeat_period,
+            nack_response_delay,
+            nack_suppression_duration,
+            data_max_size_serialized,
+        );
+        let data_writer_impl = DataWriterImpl::new(DataWriterQos::default(), rtps_writer);
+
+        data_writer_impl
+            .write_w_timestamp(
+                (7u8, 3u8),
+                None,
+                rust_dds_api::dcps_psm::Time { sec: 0, nanosec: 0 },
+            )
+            .unwrap();
+
+        let rtps_writer_lock = data_writer_impl.rtps_writer_impl.lock().unwrap();
+        let change = rtps_writer_lock
+            .writer_cache()
+            .get_change(&(1i64.into()))
+            .unwrap();
+
+        assert_eq!(change.data_value(), &[0, 1, 0, 0, 7, 3]);
     }
 }
