bc36e22de79c76721c84c4efc59bc54d9f817578
Joao Rebelo <2730147+jrebelo@users.noreply.github.com>
2021-09-09 11:15:23 +0200

    Move files into dedicate folder for builtin data representation

---
+++ dds_rtps_implementation/src/data_representation_builtin_endpoints/mod.rs
@@ -0,0 +1,1 @@
+pub mod spdp_discovered_participant_data;
--- dds_rtps_implementation/src/lib.rs
+++ dds_rtps_implementation/src/lib.rs
@@ -1,4 +1,4 @@
-pub mod data_builtin_endpoints;
+pub mod data_representation_builtin_endpoints;
 pub mod dds_impl;
 pub mod dds_type;
 pub mod rtps_impl;
