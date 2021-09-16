b8f434e453e0b0c352fcb025e502bb527e6d77e3
Stefan Kimmer <skimmer@s2e-systems.com>
2021-09-16 14:53:26 +0200

    Add decription to behavior and message types

--- rtps_pim/src/behavior/types.rs
+++ rtps_pim/src/behavior/types.rs
@@ -1,14 +1,17 @@
 ///
 /// This files shall only contain the types as listed in the DDSI-RTPS Version 2.3
-/// Table 8.46 - Types definitions for the Behavior Module
+/// Table 8.4.6 - Types definitions for the Behavior Module
 ///
 
+
+/// Duration_t
+/// Type used to hold time differences.
+/// Should have at least nano-second resolution.
 #[derive(Clone, Copy, PartialEq, Debug)]
 pub struct Duration {
     pub seconds: i32,
     pub fraction: u32,
 }
-
 impl Duration {
     pub fn new(seconds: i32, fraction: u32) -> Self { Self { seconds, fraction } }
     pub fn seconds(&self) -> &i32 {
@@ -18,19 +21,19 @@ impl Duration {
         &self.fraction
     }
 }
-
 pub const DURATION_ZERO: Duration = Duration {
     seconds: 0,
     fraction: 0,
 };
-
 pub const DURATION_INFINITE: Duration = Duration {
     seconds: 0x7fffffff,
     fraction: 0xffffffff,
 };
 
-pub type ParticipantMessageData = ();
 
+/// ChangeForReaderStatusKind
+/// Enumeration used to indicate the status of a ChangeForReader. It can take the values:
+/// UNSENT, UNACKNOWLEDGED, REQUESTED, ACKNOWLEDGED, UNDERWAY
 #[derive(Clone, Copy, PartialEq, Debug)]
 pub enum ChangeForReaderStatusKind {
     Unsent,
@@ -40,6 +43,10 @@ pub enum ChangeForReaderStatusKind {
     Underway,
 }
 
+
+/// ChangeFromWriterStatusKind
+/// Enumeration used to indicate the status of a ChangeFromWriter. It can take the values:
+/// LOST, MISSING, RECEIVED, UNKNOWN
 #[derive(Clone, Copy, PartialEq, Debug)]
 pub enum ChangeFromWriterStatusKind {
     Lost,
@@ -47,3 +54,12 @@ pub enum ChangeFromWriterStatusKind {
     Received,
     Unknown,
 }
+
+
+/// InstanceHandle_t
+/// Already defined in Structure module Table 8.2
+
+
+/// ParticipantMessageData
+/// Type used to hold data exchanged between Participants. The most notable use of this type is for the Writer Liveliness Protocol.
+pub type ParticipantMessageData = ();
--- rtps_pim/src/messages/types.rs
+++ rtps_pim/src/messages/types.rs
@@ -2,14 +2,26 @@
 /// Table 8.13 - Types used to define RTPS messages
 ///
 
+/// ProtocolId_t
+/// Enumeration used to identify the protocol.
+/// The following values are reserved by the protocol: PROTOCOL_RTPS
 #[derive(Clone, Copy, PartialEq, Debug)]
 #[allow(non_camel_case_types)]
 pub enum ProtocolId {
     PROTOCOL_RTPS,
 }
 
+
+/// SubmessageFlag
+/// Type used to specify a Submessage flag.
+/// A Submessage flag takes a boolean value and affects the parsing of the Submessage by the receiver.
 pub type SubmessageFlag = bool;
 
+
+/// SubmessageKind
+/// Enumeration used to identify the kind of Submessage.
+/// The following values are reserved by this version of the protocol:
+/// DATA, GAP, HEARTBEAT, ACKNACK, PAD, INFO_TS, INFO_REPLY, INFO_DST, INFO_SRC, DATA_FRAG, NACK_FRAG, HEARTBEAT_FRAG
 #[derive(Clone, Copy, PartialEq, Debug)]
 #[allow(non_camel_case_types)]
 pub enum SubmessageKind {
@@ -28,6 +40,12 @@ pub enum SubmessageKind {
     UNKNOWN,
 }
 
+
+/// Time_t
+/// Type used to hold a timestamp.
+/// Should have at least nano-second resolution.
+/// The following values are reserved by the protocol: TIME_ZERO
+/// TIME_INVALID TIME_INFINITE
 #[derive(Clone, Copy, PartialEq, Debug)]
 pub struct Time(pub u64);
 
@@ -35,14 +53,28 @@ pub const TIME_ZERO: Time = Time(0);
 pub const TIME_INVALID: Time = Time(u64::MAX);
 pub const TIME_INFINITE: Time = Time(u64::MAX - 1);
 
+
+/// Count_t
+/// Type used to hold a count that is incremented monotonically, used to identify message duplicates.
 #[derive(Clone, Copy, PartialEq, Debug)]
 pub struct Count(pub i32);
 
+
+/// ParameterId_t
+/// Type used to uniquely identify a parameter in a parameter list.
+/// Used extensively by the Discovery Module mainly to define QoS Parameters. A range of values is reserved for protocol-defined parameters, while another range can be used for vendor-defined parameters, see 8.3.5.9.
 #[derive(Clone, Copy, PartialEq, Debug)]
 pub struct ParameterId(pub u16);
 
+
+/// FragmentNumber_t
+/// Type used to hold fragment numbers.
+/// Must be possible to represent using 32 bits.
 #[derive(Clone, Copy, PartialEq, Debug)]
 pub struct FragmentNumber(pub u32);
 
+
+/// GroupDigest_t
+/// Type used to hold a digest value that uniquely identifies a group of Entities belonging to the same Participant.
 #[derive(Debug, PartialEq)]
 pub struct GroupDigest(pub [u8; 4]);
