c1a7555abb3ab917a0080d7bdffdc4c796545515
Joao Rebelo <2730147+jrebelo@users.noreply.github.com>
2021-10-07 10:34:27 +0200

    Implement subscriber API on impl object

--- dds_rtps_implementation/src/dds_impl/subscriber_impl.rs
+++ dds_rtps_implementation/src/dds_impl/subscriber_impl.rs
@@ -1,17 +1,21 @@
+use std::sync::Mutex;
+
 use rust_dds_api::{
     dcps_psm::StatusMask,
     infrastructure::{
+        entity::Entity,
         qos::{DataReaderQos, SubscriberQos},
         qos_policy::ReliabilityQosPolicyKind,
     },
     return_type::DDSResult,
-    subscription::data_reader_listener::DataReaderListener,
+    subscription::{
+        data_reader_listener::DataReaderListener,
+        subscriber::{DataReaderGAT, Subscriber},
+        subscriber_listener::SubscriberListener,
+    },
 };
 use rust_rtps_pim::{
-    behavior::{
-        reader::{reader::RtpsReader, stateless_reader::RtpsStatelessReader},
-        stateless_reader_behavior::StatelessReaderBehavior,
-    },
+    behavior::reader::{reader::RtpsReader, stateless_reader::RtpsStatelessReader},
     messages::{submessage_elements::Parameter, submessages::DataSubmessage},
     structure::{
         types::{
@@ -42,7 +46,7 @@ impl<T> DataReaderObject for T where T: Send + Sync + ProcessDataSubmessage {}
 pub struct SubscriberImpl {
     qos: SubscriberQos,
     rtps_group: RtpsGroup,
-    data_reader_list: Vec<RtpsShared<dyn DataReaderObject>>,
+    data_reader_list: Mutex<Vec<RtpsShared<dyn DataReaderObject>>>,
     user_defined_data_reader_counter: u8,
     default_data_reader_qos: DataReaderQos,
 }
@@ -56,19 +60,28 @@ impl SubscriberImpl {
         Self {
             qos,
             rtps_group,
-            data_reader_list,
+            data_reader_list: Mutex::new(data_reader_list),
             user_defined_data_reader_counter: 0,
             default_data_reader_qos: DataReaderQos::default(),
         }
     }
+}
 
-    pub fn create_datareader<T: DdsType + 'static>(
-        &mut self,
-        _a_topic: (),
+impl<T> DataReaderGAT<'_, '_, T> for SubscriberImpl
+where
+    T: DdsType,
+{
+    type TopicType = ();
+
+    type DataReaderType = RtpsWeak<DataReaderImpl<T>>;
+
+    fn create_datareader_gat(
+        &'_ self,
+        _a_topic: &'_ Self::TopicType,
         qos: Option<DataReaderQos>,
         _a_listener: Option<&'static dyn DataReaderListener<DataType = T>>,
         _mask: StatusMask,
-    ) -> Option<RtpsWeak<DataReaderImpl<T>>> {
+    ) -> Option<Self::DataReaderType> {
         let qos = qos.unwrap_or(self.default_data_reader_qos.clone());
         qos.is_consistent().ok()?;
 
@@ -111,18 +124,123 @@ impl SubscriberImpl {
         let reader_storage = DataReaderImpl::new(qos, rtps_reader);
         let reader_storage_shared = rtps_shared_new(reader_storage);
         let reader_storage_weak = rtps_shared_downgrade(&reader_storage_shared);
-        self.data_reader_list.push(reader_storage_shared);
+        self.data_reader_list
+            .lock()
+            .unwrap()
+            .push(reader_storage_shared);
         Some(reader_storage_weak)
     }
 
-    pub fn set_qos(&mut self, qos: Option<SubscriberQos>) -> DDSResult<()> {
+    fn delete_datareader_gat(&self, _a_datareader: &Self::DataReaderType) -> DDSResult<()> {
+        todo!()
+    }
+
+    fn lookup_datareader_gat(
+        &'_ self,
+        _topic: &'_ Self::TopicType,
+    ) -> Option<Self::DataReaderType> {
+        todo!()
+    }
+}
+
+impl Subscriber for SubscriberImpl {
+    fn begin_access(&self) -> DDSResult<()> {
+        todo!()
+    }
+
+    fn end_access(&self) -> DDSResult<()> {
+        todo!()
+    }
+
+    fn get_datareaders(
+        &self,
+        _readers: &mut [&mut dyn rust_dds_api::subscription::data_reader::AnyDataReader],
+        _sample_states: &[rust_dds_api::dcps_psm::SampleStateKind],
+        _view_states: &[rust_dds_api::dcps_psm::ViewStateKind],
+        _instance_states: &[rust_dds_api::dcps_psm::InstanceStateKind],
+    ) -> DDSResult<()> {
+        todo!()
+    }
+
+    fn notify_datareaders(&self) -> DDSResult<()> {
+        todo!()
+    }
+
+    fn get_participant(&self) -> &dyn rust_dds_api::domain::domain_participant::DomainParticipant {
+        todo!()
+    }
+
+    fn get_sample_lost_status(
+        &self,
+        _status: &mut rust_dds_api::dcps_psm::SampleLostStatus,
+    ) -> DDSResult<()> {
+        todo!()
+    }
+
+    fn delete_contained_entities(&self) -> DDSResult<()> {
+        todo!()
+    }
+
+    fn set_default_datareader_qos(&self, _qos: Option<DataReaderQos>) -> DDSResult<()> {
+        todo!()
+    }
+
+    fn get_default_datareader_qos(&self) -> DDSResult<DataReaderQos> {
+        todo!()
+    }
+
+    fn copy_from_topic_qos(
+        &self,
+        _a_datareader_qos: &mut DataReaderQos,
+        _a_topic_qos: &rust_dds_api::infrastructure::qos::TopicQos,
+    ) -> DDSResult<()> {
+        todo!()
+    }
+}
+
+impl Entity for SubscriberImpl {
+    type Qos = SubscriberQos;
+    type Listener = &'static dyn SubscriberListener;
+
+    fn set_qos(&mut self, qos: Option<Self::Qos>) -> DDSResult<()> {
         let qos = qos.unwrap_or_default();
         self.qos = qos;
         Ok(())
     }
 
-    pub fn get_qos(&self) -> &SubscriberQos {
-        &self.qos
+    fn get_qos(&self) -> DDSResult<Self::Qos> {
+        // &self.qos
+        todo!()
+    }
+
+    fn set_listener(
+        &self,
+        _a_listener: Option<Self::Listener>,
+        _mask: StatusMask,
+    ) -> DDSResult<()> {
+        todo!()
+    }
+
+    fn get_listener(&self) -> DDSResult<Option<Self::Listener>> {
+        todo!()
+    }
+
+    fn get_statuscondition(
+        &self,
+    ) -> DDSResult<rust_dds_api::infrastructure::entity::StatusCondition> {
+        todo!()
+    }
+
+    fn get_status_changes(&self) -> DDSResult<StatusMask> {
+        todo!()
+    }
+
+    fn enable(&self) -> DDSResult<()> {
+        todo!()
+    }
+
+    fn get_instance_handle(&self) -> DDSResult<rust_dds_api::dcps_psm::InstanceHandle> {
+        todo!()
     }
 }
 
@@ -132,9 +250,19 @@ impl ProcessDataSubmessage for SubscriberImpl {
         source_guid_prefix: GuidPrefix,
         data: &DataSubmessage<Vec<Parameter<'_>>>,
     ) {
-        for reader in &self.data_reader_list {
+        let data_reader_list = self.data_reader_list.lock().unwrap();
+        for reader in data_reader_list.iter() {
             rtps_shared_write_lock(reader).process_data_submessage(source_guid_prefix, data);
             //  rtps_reader_mut() {
         }
+    }
+}
+
+#[cfg(test)]
+mod tests {
+
+    #[test]
+    fn lookup_datareader() {
+        // todo!()
     }
 }
