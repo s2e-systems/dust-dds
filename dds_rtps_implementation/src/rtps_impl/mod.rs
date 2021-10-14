17f42895a7f6ee5b5684bdb0849a66fa97ffe460
Joao Rebelo <2730147+jrebelo@users.noreply.github.com>
2021-10-14 16:34:09 +0200

    Remove obsolete structs

--- dds_rtps_implementation/src/rtps_impl/mod.rs
+++ dds_rtps_implementation/src/rtps_impl/mod.rs
@@ -4,13 +4,6 @@ pub mod rtps_writer_history_cache_impl;
 pub mod rtps_stateful_reader_impl;
 pub mod rtps_stateless_reader_impl;
 
-pub mod rtps_participant_impl;
-
-pub mod rtps_group_impl;
-
-pub mod rtps_writer_impl;
-pub mod rtps_reader_impl;
-
 pub mod rtps_reader_locator_impl;
 pub mod rtps_reader_proxy_impl;
 
