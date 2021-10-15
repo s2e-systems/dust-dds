1f3abb57953b735c585be60f08279ebfc4f0ffbc
Joao Rebelo <2730147+jrebelo@users.noreply.github.com>
2021-10-15 09:55:45 +0200

    Add missing sedp types

--- dds_rtps_implementation/src/data_representation_builtin_endpoints/mod.rs
+++ dds_rtps_implementation/src/data_representation_builtin_endpoints/mod.rs
@@ -2,3 +2,5 @@ pub mod dds_serialize_deserialize_impl;
 pub mod parameter_id_values;
 pub mod spdp_discovered_participant_data;
 pub mod sedp_discovered_reader_data;
+pub mod sedp_discovered_writer_data;
+pub mod sedp_discovered_topic_data;
