ddd78e1be5b602dae1d8570fcc12642ddf9db9a5
Joao Rebelo <2730147+jrebelo@users.noreply.github.com>
2021-10-15 09:48:11 +0200

    Add builtin data to sedp discovered reader

--- dds_rtps_implementation/src/data_representation_builtin_endpoints/sedp_discovered_reader_data.rs
+++ dds_rtps_implementation/src/data_representation_builtin_endpoints/sedp_discovered_reader_data.rs
@@ -1,8 +1,10 @@
+use rust_dds_api::builtin_topics::SubscriptionBuiltinTopicData;
+
 use crate::{dds_type::DdsType, rtps_impl::rtps_reader_proxy_impl::RtpsReaderProxyImpl};
 
 pub struct SedpDiscoveredReaderData {
     pub reader_proxy: RtpsReaderProxyImpl,
-    pub subscriptions_builtin_topic_data: (),
+    pub subscriptions_builtin_topic_data: SubscriptionBuiltinTopicData,
 }
 
 impl DdsType for SedpDiscoveredReaderData {
