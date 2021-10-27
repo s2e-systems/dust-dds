6d887fa94affac62ccfebe29425d4f273f263438
Joao Rebelo <2730147+jrebelo@users.noreply.github.com>
2021-10-27 16:39:15 +0200

    Add stateful writer behavior

--- rtps_pim/src/behavior/writer/stateful_writer.rs
+++ rtps_pim/src/behavior/writer/stateful_writer.rs
@@ -5,9 +5,17 @@ use core::{
 
 use crate::{
     behavior::types::Duration,
-    messages::submessages::{DataSubmessage, GapSubmessage},
+    messages::{
+        submessage_elements::{
+            CountSubmessageElement, EntityIdSubmessageElement, ParameterListSubmessageElement,
+            SequenceNumberSetSubmessageElement, SequenceNumberSubmessageElement,
+            SerializedDataSubmessageElement,
+        },
+        submessages::{AckNackSubmessage, DataSubmessage, GapSubmessage, HeartbeatSubmessage},
+        types::Count,
+    },
     structure::{
-        types::{Guid, ReliabilityKind, SequenceNumber, TopicKind},
+        types::{ChangeKind, Guid, ReliabilityKind, SequenceNumber, TopicKind, ENTITYID_UNKNOWN},
         RtpsHistoryCacheOperations,
     },
 };
@@ -81,122 +89,280 @@ pub trait RtpsStatefulWriterOperations<L> {
     fn is_acked_by_all(&self) -> bool;
 }
 
-pub trait StatefulWriterBehavior<S, P, D> {
-    type ReaderProxy;
-
-    fn send_unsent_data(
-        &mut self,
-        send_data: impl FnMut(&Self::ReaderProxy, DataSubmessage<P, D>),
-        send_gap: impl FnMut(&Self::ReaderProxy, GapSubmessage<S>),
-    );
-}
-
-impl<S, L, C, R, RP, P, D> StatefulWriterBehavior<S, P, D> for RtpsStatefulWriter<L, C, R>
+impl<L, C, R, RP, P, D, SV> RtpsStatefulWriter<L, C, R>
 where
     for<'b> &'b mut R: IntoIterator<Item = &'b mut RP>,
-    RP: RtpsReaderProxyOperations,
+    RP: RtpsReaderProxyOperations<SequenceNumberVector = SV> + Deref<Target = RtpsReaderProxy<L>>,
+    SV: ExactSizeIterator,
     C: for<'a> RtpsHistoryCacheOperations<'a, GetChangeDataType = D, GetChangeParameterType = P>,
-    S: FromIterator<SequenceNumber>,
 {
-    type ReaderProxy = RP;
-
-    fn send_unsent_data(
+    pub fn best_effort_behavior<S>(
         &mut self,
-        mut send_data: impl FnMut(&Self::ReaderProxy, DataSubmessage<P, D>),
-        mut send_gap: impl FnMut(&Self::ReaderProxy, GapSubmessage<S>),
-    ) {
-        let reliability_level = self.writer.reliability_level;
-        let last_change_sequence_number = self.writer.last_change_sequence_number;
-        for reader_locator in &mut self.matched_readers {
-            match reliability_level {
-                ReliabilityKind::BestEffort => todo!(),
-                ReliabilityKind::Reliable => todo!(),
+        send_data: &mut impl FnMut(&RP, DataSubmessage<P, D>),
+        send_gap: &mut impl FnMut(&RP, GapSubmessage<S>),
+    ) where
+        S: FromIterator<SequenceNumber>,
+    {
+        for reader_proxy in &mut self.matched_readers {
+            while let Some(seq_num) = reader_proxy.next_unsent_change() {
+                if let Some(change) = self.writer.writer_cache.get_change(&seq_num) {
+                    let endianness_flag = true;
+                    let inline_qos_flag = true;
+                    let (data_flag, key_flag) = match change.kind {
+                        ChangeKind::Alive => (true, false),
+                        ChangeKind::NotAliveDisposed | ChangeKind::NotAliveUnregistered => {
+                            (false, true)
+                        }
+                        _ => todo!(),
+                    };
+                    let non_standard_payload_flag = false;
+                    let reader_id = EntityIdSubmessageElement {
+                        value: ENTITYID_UNKNOWN,
+                    };
+                    let writer_id = EntityIdSubmessageElement {
+                        value: change.writer_guid.entity_id,
+                    };
+                    let writer_sn = SequenceNumberSubmessageElement {
+                        value: change.sequence_number,
+                    };
+                    let inline_qos = ParameterListSubmessageElement {
+                        parameter: change.inline_qos,
+                    };
+                    let serialized_payload = SerializedDataSubmessageElement {
+                        value: change.data_value,
+                    };
+                    let data_submessage = DataSubmessage {
+                        endianness_flag,
+                        inline_qos_flag,
+                        data_flag,
+                        key_flag,
+                        non_standard_payload_flag,
+                        reader_id,
+                        writer_id,
+                        writer_sn,
+                        inline_qos,
+                        serialized_payload,
+                    };
+                    send_data(reader_proxy, data_submessage)
+                } else {
+                    let endianness_flag = true;
+                    let reader_id = EntityIdSubmessageElement {
+                        value: ENTITYID_UNKNOWN,
+                    };
+                    let writer_id = EntityIdSubmessageElement {
+                        value: self.writer.guid.entity_id,
+                    };
+                    let gap_start = SequenceNumberSubmessageElement { value: seq_num };
+                    let set = core::iter::empty().collect();
+                    let gap_list = SequenceNumberSetSubmessageElement { base: seq_num, set };
+                    let gap_submessage = GapSubmessage {
+                        endianness_flag,
+                        reader_id,
+                        writer_id,
+                        gap_start,
+                        gap_list,
+                    };
+                    send_gap(reader_proxy, gap_submessage)
+                }
+            }
+        }
+    }
+
+    pub fn reliable_behavior<S>(
+        &mut self,
+        send_data: &mut impl FnMut(&RP, DataSubmessage<P, D>),
+        send_gap: &mut impl FnMut(&RP, GapSubmessage<S>),
+    ) where
+        S: FromIterator<SequenceNumber>,
+    {
+        for reader_proxy in &mut self.matched_readers {
+            // Pushing state
+            while let Some(seq_num) = reader_proxy.next_unsent_change() {
+                if let Some(change) = self.writer.writer_cache.get_change(&seq_num) {
+                    let endianness_flag = true;
+                    let inline_qos_flag = true;
+                    let (data_flag, key_flag) = match change.kind {
+                        ChangeKind::Alive => (true, false),
+                        ChangeKind::NotAliveDisposed | ChangeKind::NotAliveUnregistered => {
+                            (false, true)
+                        }
+                        _ => todo!(),
+                    };
+                    let non_standard_payload_flag = false;
+                    let reader_id = EntityIdSubmessageElement {
+                        value: ENTITYID_UNKNOWN,
+                    };
+                    let writer_id = EntityIdSubmessageElement {
+                        value: *change.writer_guid.entity_id(),
+                    };
+                    let writer_sn = SequenceNumberSubmessageElement {
+                        value: change.sequence_number,
+                    };
+                    let inline_qos = ParameterListSubmessageElement {
+                        parameter: change.inline_qos,
+                    };
+                    let serialized_payload = SerializedDataSubmessageElement {
+                        value: change.data_value,
+                    };
+                    let data_submessage = DataSubmessage {
+                        endianness_flag,
+                        inline_qos_flag,
+                        data_flag,
+                        key_flag,
+                        non_standard_payload_flag,
+                        reader_id,
+                        writer_id,
+                        writer_sn,
+                        inline_qos,
+                        serialized_payload,
+                    };
+                    send_data(reader_proxy, data_submessage)
+                } else {
+                    let endianness_flag = true;
+                    let reader_id = EntityIdSubmessageElement {
+                        value: ENTITYID_UNKNOWN,
+                    };
+                    let writer_id = EntityIdSubmessageElement {
+                        value: ENTITYID_UNKNOWN,
+                    };
+                    let gap_start = SequenceNumberSubmessageElement { value: seq_num };
+                    let set = core::iter::empty().collect();
+                    let gap_list = SequenceNumberSetSubmessageElement { base: seq_num, set };
+                    let gap_submessage = GapSubmessage {
+                        endianness_flag,
+                        reader_id,
+                        writer_id,
+                        gap_start,
+                        gap_list,
+                    };
+                    send_gap(reader_proxy, gap_submessage)
+                }
+            }
+        }
+    }
+
+    pub fn send_heartbeat(&self, count: Count) -> HeartbeatSubmessage {
+        let endianness_flag = true;
+        let final_flag = false;
+        let liveliness_flag = false;
+        let reader_id = EntityIdSubmessageElement {
+            value: ENTITYID_UNKNOWN,
+        };
+        let writer_id = EntityIdSubmessageElement {
+            value: self.writer.guid.entity_id,
+        };
+        let first_sn = SequenceNumberSubmessageElement {
+            value: self.writer.writer_cache.get_seq_num_min().unwrap_or(0),
+        };
+        let last_sn = SequenceNumberSubmessageElement {
+            value: self.writer.writer_cache.get_seq_num_min().unwrap_or(0),
+        };
+        let count = CountSubmessageElement { value: count };
+        HeartbeatSubmessage {
+            endianness_flag,
+            final_flag,
+            liveliness_flag,
+            reader_id,
+            writer_id,
+            first_sn,
+            last_sn,
+            count,
+        }
+    }
+
+    pub fn process_acknack_submessage<S>(
+        &mut self,
+        acknack: Option<AckNackSubmessage<SV>>,
+        after_nack_response_delay: bool,
+        send_data: &mut impl FnMut(&RP, DataSubmessage<P, D>),
+        send_gap: &mut impl FnMut(&RP, GapSubmessage<S>),
+    ) where
+        S: FromIterator<SequenceNumber>,
+    {
+        if let Some(acknack) = acknack {
+            if let Some(reader_proxy) = (&mut self.matched_readers)
+                .into_iter()
+                .find(|x| &x.remote_reader_guid.entity_id == &acknack.reader_id.value)
+            {
+                reader_proxy.acked_changes_set(acknack.reader_sn_state.base - 1);
+                reader_proxy.requested_changes_set(acknack.reader_sn_state.set);
+            }
+        }
+
+        if after_nack_response_delay {
+            for reader_proxy in &mut self.matched_readers {
+                // Pushing state
+                while let Some(seq_num) = reader_proxy.next_requested_change() {
+                    if let Some(change) = self.writer.writer_cache.get_change(&seq_num) {
+                        let endianness_flag = true;
+                        let inline_qos_flag = true;
+                        let (data_flag, key_flag) = match change.kind {
+                            ChangeKind::Alive => (true, false),
+                            ChangeKind::NotAliveDisposed | ChangeKind::NotAliveUnregistered => {
+                                (false, true)
+                            }
+                            _ => todo!(),
+                        };
+                        let non_standard_payload_flag = false;
+                        let reader_id = EntityIdSubmessageElement {
+                            value: ENTITYID_UNKNOWN,
+                        };
+                        let writer_id = EntityIdSubmessageElement {
+                            value: *change.writer_guid.entity_id(),
+                        };
+                        let writer_sn = SequenceNumberSubmessageElement {
+                            value: change.sequence_number,
+                        };
+                        let inline_qos = ParameterListSubmessageElement {
+                            parameter: change.inline_qos,
+                        };
+                        let serialized_payload = SerializedDataSubmessageElement {
+                            value: change.data_value,
+                        };
+                        let data_submessage = DataSubmessage {
+                            endianness_flag,
+                            inline_qos_flag,
+                            data_flag,
+                            key_flag,
+                            non_standard_payload_flag,
+                            reader_id,
+                            writer_id,
+                            writer_sn,
+                            inline_qos,
+                            serialized_payload,
+                        };
+                        send_data(reader_proxy, data_submessage)
+                    } else {
+                        let endianness_flag = true;
+                        let reader_id = EntityIdSubmessageElement {
+                            value: ENTITYID_UNKNOWN,
+                        };
+                        let writer_id = EntityIdSubmessageElement {
+                            value: ENTITYID_UNKNOWN,
+                        };
+                        let gap_start = SequenceNumberSubmessageElement { value: seq_num };
+                        let set = core::iter::empty().collect();
+                        let gap_list = SequenceNumberSetSubmessageElement { base: seq_num, set };
+                        let gap_submessage = GapSubmessage {
+                            endianness_flag,
+                            reader_id,
+                            writer_id,
+                            gap_start,
+                            gap_list,
+                        };
+                        send_gap(reader_proxy, gap_submessage)
+                    }
+                }
             }
         }
     }
 }
 
-// fn best_effort_send_unsent_data<RL, S, P, D>(
-//     reader_locator: &mut RL,
-//     writer_cache: &impl for<'a> RtpsHistoryCacheOperations<
-//         'a,
-//         GetChangeDataType = D,
-//         GetChangeParameterType = P,
-//     >,
-//     last_change_sequence_number: &SequenceNumber,
-//     send_data: &mut impl FnMut(&RL, DataSubmessage<P, D>),
-//     send_gap: &mut impl FnMut(&RL, GapSubmessage<S>),
-// ) where
-//     RL: RtpsReaderLocatorOperations,
-//     S: FromIterator<SequenceNumber>,
-// {
-//     while let Some(seq_num) = reader_locator.next_unsent_change(&last_change_sequence_number) {
-//         if let Some(change) = writer_cache.get_change(&seq_num) {
-//             let endianness_flag = true;
-//             let inline_qos_flag = true;
-//             let (data_flag, key_flag) = match change.kind {
-//                 ChangeKind::Alive => (true, false),
-//                 ChangeKind::NotAliveDisposed | ChangeKind::NotAliveUnregistered => (false, true),
-//                 _ => todo!(),
-//             };
-//             let non_standard_payload_flag = false;
-//             let reader_id = EntityIdSubmessageElement {
-//                 value: ENTITYID_UNKNOWN,
-//             };
-//             let writer_id = EntityIdSubmessageElement {
-//                 value: *change.writer_guid.entity_id(),
-//             };
-//             let writer_sn = SequenceNumberSubmessageElement {
-//                 value: change.sequence_number,
-//             };
-//             let inline_qos = ParameterListSubmessageElement {
-//                 parameter: change.inline_qos,
-//             };
-//             let serialized_payload = SerializedDataSubmessageElement {
-//                 value: change.data_value,
-//             };
-//             let data_submessage = DataSubmessage {
-//                 endianness_flag,
-//                 inline_qos_flag,
-//                 data_flag,
-//                 key_flag,
-//                 non_standard_payload_flag,
-//                 reader_id,
-//                 writer_id,
-//                 writer_sn,
-//                 inline_qos,
-//                 serialized_payload,
-//             };
-//             send_data(reader_locator, data_submessage)
-//         } else {
-//             let endianness_flag = true;
-//             let reader_id = EntityIdSubmessageElement {
-//                 value: ENTITYID_UNKNOWN,
-//             };
-//             let writer_id = EntityIdSubmessageElement {
-//                 value: ENTITYID_UNKNOWN,
-//             };
-//             let gap_start = SequenceNumberSubmessageElement { value: seq_num };
-//             let set = core::iter::empty().collect();
-//             let gap_list = SequenceNumberSetSubmessageElement { base: seq_num, set };
-//             let gap_submessage = GapSubmessage {
-//                 endianness_flag,
-//                 reader_id,
-//                 writer_id,
-//                 gap_start,
-//                 gap_list,
-//             };
-//             send_gap(reader_locator, gap_submessage)
-//         }
-//     }
-// }
-
-// pub fn reliable_send_unsent_data(
-//     reader_locator: &mut impl RtpsReaderLocatorOperations,
+pub fn reliable_send_unsent_data(//     reader_locator: &mut impl RtpsReaderLocatorOperations,
 //     last_change_sequence_number: SequenceNumber,
 //     mut send: impl FnMut(SequenceNumber),
-// ) {
-//     while let Some(seq_num) = reader_locator.next_unsent_change(&last_change_sequence_number) {
-//         send(seq_num)
-//     }
+) {
+    //     while let Some(seq_num) = reader_locator.next_unsent_change(&last_change_sequence_number) {
+    //         send(seq_num)
+}
 // }
