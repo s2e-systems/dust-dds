dd5f92783902224ec6cad21613922d635eb42643
Joao Rebelo <2730147+jrebelo@users.noreply.github.com>
2021-12-22 11:59:50 +0100

    Remove cache change struct

--- rtps_pim/src/structure/cache_change.rs
+++ rtps_pim/src/structure/cache_change.rs
@@ -1,14 +1,5 @@
 use super::types::{ChangeKind, Guid, InstanceHandle, SequenceNumber};
 
-pub struct RtpsCacheChange<P, D> {
-    pub kind: ChangeKind,
-    pub writer_guid: Guid,
-    pub instance_handle: InstanceHandle,
-    pub sequence_number: SequenceNumber,
-    pub data_value: D,
-    pub inline_qos: P,
-}
-
 pub trait RtpsCacheChangeConstructor {
     type DataType: ?Sized;
     type ParameterListType: ?Sized;
