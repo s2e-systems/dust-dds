c03eed63d1d1a834e567d1e914fae7cf792b572e
Stefan Kimmer <skimmer@s2e-systems.com>
2022-01-26 21:09:33 +0100

    use HistoryCache as argument in next_unsent_change

--- dds_rtps_implementation/src/rtps_impl/rtps_reader_locator_impl.rs
+++ dds_rtps_implementation/src/rtps_impl/rtps_reader_locator_impl.rs
@@ -2,8 +2,13 @@ use rust_rtps_pim::{
     behavior::writer::reader_locator::{
         RtpsReaderLocatorAttributes, RtpsReaderLocatorConstructor, RtpsReaderLocatorOperations,
     },
-    structure::types::{Locator, SequenceNumber},
+    structure::{
+        history_cache::RtpsHistoryAttributes,
+        types::{Locator, SequenceNumber},
+    },
 };
+
+use super::rtps_writer_history_cache_impl::WriterHistoryCache;
 pub struct RtpsReaderLocatorImpl {
     requested_changes: Vec<SequenceNumber>,
     unsent_changes: Vec<SequenceNumber>,
@@ -33,11 +38,26 @@ impl RtpsReaderLocatorConstructor for RtpsReaderLocatorImpl {
 
 impl RtpsReaderLocatorAttributes for RtpsReaderLocatorImpl {
     type CacheChangeType = SequenceNumber;
+    type HistoryCacheType = WriterHistoryCache;
 
     fn requested_changes(&self) -> &[Self::CacheChangeType] {
         &self.requested_changes
     }
-    fn unsent_changes(&self) -> &[Self::CacheChangeType] {
+    fn unsent_changes(
+        &mut self,
+        history_cache: &Self::HistoryCacheType,
+    ) -> &[Self::CacheChangeType] {
+        self.unsent_changes = history_cache
+            .changes()
+            .iter()
+            .filter_map(|f| {
+                if f.sequence_number > self.last_sent_sequence_number {
+                    Some(f.sequence_number)
+                } else {
+                    None
+                }
+            })
+            .collect();
         &self.unsent_changes
     }
     fn locator(&self) -> &Locator {
@@ -50,6 +70,7 @@ impl RtpsReaderLocatorAttributes for RtpsReaderLocatorImpl {
 
 impl RtpsReaderLocatorOperations for RtpsReaderLocatorImpl {
     type CacheChangeType = SequenceNumber;
+    type HistoryCacheType = WriterHistoryCache;
 
     fn next_requested_change(&mut self) -> Option<Self::CacheChangeType> {
         if self.requested_changes.is_empty() {
@@ -61,20 +82,13 @@ impl RtpsReaderLocatorOperations for RtpsReaderLocatorImpl {
 
     fn next_unsent_change(
         &mut self,
-        last_change: &Self::CacheChangeType,
+        history_cache: &Self::HistoryCacheType,
     ) -> Option<Self::CacheChangeType> {
-        if self.unsent_changes.iter().all(|c| c != last_change) {
-            self.unsent_changes.push(*last_change);
-        }
-
-        self.unsent_changes = (self.last_sent_sequence_number + 1..=*self.unsent_changes.last().unwrap()).collect();
-
-        if &self.last_sent_sequence_number < last_change {
-            self.last_sent_sequence_number = self.last_sent_sequence_number + 1;
-            Some(self.last_sent_sequence_number)
-        } else {
-            None
-        }
+        let unsent_change = self.unsent_changes(history_cache).first().cloned();
+        if let Some(unsent_change) = unsent_change {
+            self.last_sent_sequence_number = unsent_change;
+        };
+        unsent_change
     }
 
     fn requested_changes_set(&mut self, req_seq_num_set: &[Self::CacheChangeType]) {
@@ -82,20 +96,42 @@ impl RtpsReaderLocatorOperations for RtpsReaderLocatorImpl {
     }
 }
 
-
 #[cfg(test)]
 mod tests {
-    use rust_rtps_pim::structure::types::LOCATOR_INVALID;
+    use rust_rtps_pim::structure::{
+        cache_change::RtpsCacheChangeConstructor,
+        history_cache::{RtpsHistoryCacheConstructor, RtpsHistoryCacheOperations},
+        types::{ChangeKind, GUID_UNKNOWN, LOCATOR_INVALID},
+    };
+
+    use crate::rtps_impl::rtps_writer_history_cache_impl::WriterCacheChange;
 
     use super::*;
 
     #[test]
     fn reader_locator_next_unsent_change() {
+        let mut hc = WriterHistoryCache::new();
+        hc.add_change(WriterCacheChange::new(
+            &ChangeKind::Alive,
+            &GUID_UNKNOWN,
+            &0,
+            &1,
+            &[],
+            &[],
+        ));
+        hc.add_change(WriterCacheChange::new(
+            &ChangeKind::Alive,
+            &GUID_UNKNOWN,
+            &0,
+            &2,
+            &[],
+            &[],
+        ));
         let mut reader_locator = RtpsReaderLocatorImpl::new(LOCATOR_INVALID, false);
 
-        assert_eq!(reader_locator.next_unsent_change(&2), Some(1));
-        assert_eq!(reader_locator.next_unsent_change(&2), Some(2));
-        assert_eq!(reader_locator.next_unsent_change(&2), None);
+        assert_eq!(reader_locator.next_unsent_change(&hc), Some(1));
+        assert_eq!(reader_locator.next_unsent_change(&hc), Some(2));
+        assert_eq!(reader_locator.next_unsent_change(&hc), None);
     }
 
     #[test]
@@ -114,12 +150,38 @@ mod tests {
 
     #[test]
     fn reader_locator_unsent_changes() {
+        let mut hc = WriterHistoryCache::new();
+        hc.add_change(WriterCacheChange::new(
+            &ChangeKind::Alive,
+            &GUID_UNKNOWN,
+            &0,
+            &2,
+            &[],
+            &[],
+        ));
+        hc.add_change(WriterCacheChange::new(
+            &ChangeKind::Alive,
+            &GUID_UNKNOWN,
+            &0,
+            &4,
+            &[],
+            &[],
+        ));
+        hc.add_change(WriterCacheChange::new(
+            &ChangeKind::Alive,
+            &GUID_UNKNOWN,
+            &0,
+            &6,
+            &[],
+            &[],
+        ));
         let mut reader_locator = RtpsReaderLocatorImpl::new(LOCATOR_INVALID, false);
 
-        reader_locator.next_unsent_change(&3);
-        let unsent_changes = reader_locator.unsent_changes();
-        let expected_unsent_changes = vec![1, 2, 3];
+        let unsent_changes = reader_locator.unsent_changes(&hc);
+        assert_eq!(unsent_changes, vec![2, 4, 6]);
 
-        assert_eq!(unsent_changes, expected_unsent_changes);
+        reader_locator.next_unsent_change(&hc);
+        let unsent_changes = reader_locator.unsent_changes(&hc);
+        assert_eq!(unsent_changes, vec![4, 6]);
     }
 }
--- rtps_pim/src/behavior/writer/reader_locator.rs
+++ rtps_pim/src/behavior/writer/reader_locator.rs
@@ -7,18 +7,20 @@ pub trait RtpsReaderLocatorConstructor {
 
 pub trait RtpsReaderLocatorAttributes {
     type CacheChangeType;
+    type HistoryCacheType;
 
     fn requested_changes(&self) -> &[Self::CacheChangeType];
-    fn unsent_changes(&self) -> &[Self::CacheChangeType];
+    fn unsent_changes(&mut self, history_cache: &Self::HistoryCacheType) -> &[Self::CacheChangeType];
     fn locator(&self) -> &Locator;
     fn expects_inline_qos(&self) -> &bool;
 }
 
 pub trait RtpsReaderLocatorOperations {
     type CacheChangeType;
+    type HistoryCacheType;
 
     fn next_requested_change(&mut self) -> Option<Self::CacheChangeType>;
-    fn next_unsent_change(&mut self, last_change: &Self::CacheChangeType) -> Option<Self::CacheChangeType>;
+    fn next_unsent_change(&mut self, history_cache: &Self::HistoryCacheType) -> Option<Self::CacheChangeType>;
     fn requested_changes_set(&mut self, req_seq_num_set: &[Self::CacheChangeType]);
     // unsent_changes() and requested_changes() functions are not present
     // in Operations since it's already present in Attributes
