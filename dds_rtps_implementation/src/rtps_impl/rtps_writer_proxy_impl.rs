f2afe7108dc55e64bf744fa85bc9d3ba83c45861
Joao Rebelo <2730147+jrebelo@users.noreply.github.com>
2022-03-08 18:28:02 +0100

    Implement writer proxy operations

--- dds_rtps_implementation/src/rtps_impl/rtps_writer_proxy_impl.rs
+++ dds_rtps_implementation/src/rtps_impl/rtps_writer_proxy_impl.rs
@@ -14,6 +14,17 @@ pub struct RtpsWriterProxyImpl {
     multicast_locator_list: Vec<Locator>,
     data_max_size_serialized: Option<i32>,
     remote_group_entity_id: EntityId,
+
+    // Every change below the first_available_seq num is LOST (i.e. no longer available in the RTPS Writer)
+    // Every change above the last_available_seq_num is UNKNOWN (i.e. may or may not be available yet at the RTPS Writer.)
+    first_available_seq_num: SequenceNumber,
+    last_available_seq_num: SequenceNumber,
+
+    // Changes for which a GAP message is received are irrelevant
+    irrelevant_changes: Vec<SequenceNumber>,
+
+    // Changes which have been received
+    received_changes: Vec<SequenceNumber>,
 }
 
 impl RtpsWriterProxyConstructor for RtpsWriterProxyImpl {
@@ -30,6 +41,10 @@ impl RtpsWriterProxyConstructor for RtpsWriterProxyImpl {
             multicast_locator_list: multicast_locator_list.to_vec(),
             data_max_size_serialized,
             remote_group_entity_id,
+            first_available_seq_num: 0,
+            last_available_seq_num: 0,
+            irrelevant_changes: Vec::new(),
+            received_changes: Vec::new(),
         }
     }
 }
@@ -65,30 +80,78 @@ impl RtpsWriterProxyOperations for RtpsWriterProxyOperationsImpl<'_> {
     type SequenceNumberListType = Vec<SequenceNumber>;
 
     fn available_changes_max(&self) -> SequenceNumber {
-        todo!()
+        // seq_num := MAX { change.sequenceNumber SUCH-THAT
+        // ( change IN this.changes_from_writer
+        // AND ( change.status == RECEIVED
+        // OR change.status == LOST) ) };
+        // return seq_num;
+
+        // Changes below first_available_seq_num are LOST (or RECEIVED, but in any case not MISSING).
+        // Change that are IRRELEVANT are not considered
+        let max_received_change_seq_num = *self
+            .writer_proxy
+            .received_changes
+            .iter()
+            .filter(|&x| !self.writer_proxy.irrelevant_changes.contains(x))
+            .max()
+            .unwrap_or(&0);
+        i64::max(
+            self.writer_proxy.first_available_seq_num,
+            max_received_change_seq_num,
+        )
     }
 
-    fn irrelevant_change_set(&mut self, _a_seq_num: SequenceNumber) {
-        todo!()
+    fn irrelevant_change_set(&mut self, a_seq_num: SequenceNumber) {
+        // This operation modifies the status of a ChangeFromWriter to indicate that the CacheChange with the
+        // SequenceNumber_t ‘a_seq_num’ is irrelevant to the RTPS Reader. Logical action in the virtual machine:
+        // FIND change FROM this.changes_from_writer SUCH-THAT
+        // (change.sequenceNumber == a_seq_num);
+        // change.status := RECEIVED; change.is_relevant := FALSE;
+        self.writer_proxy.irrelevant_changes.push(a_seq_num)
     }
 
-    fn lost_changes_update(&mut self, _first_available_seq_num: SequenceNumber) {
-        todo!()
+    fn lost_changes_update(&mut self, first_available_seq_num: SequenceNumber) {
+        // FOREACH change IN this.changes_from_writer
+        // SUCH-THAT ( change.status == UNKNOWN OR change.status == MISSING
+        // AND seq_num < first_available_seq_num ) DO {
+        // change.status := LOST;
+        // }
+        self.writer_proxy.first_available_seq_num = first_available_seq_num
     }
 
     fn missing_changes(&self) -> Self::SequenceNumberListType {
-        todo!()
+        // The changes with status ‘MISSING’ represent the set of changes available in the HistoryCache of the RTPS Writer represented by the RTPS WriterProxy that have not been received by the RTPS Reader.
+        // return { change IN this.changes_from_writer SUCH-THAT change.status == MISSING};
+        let mut missing_changes = Vec::new();
+
+        // Changes below first_available_seq_num are LOST (or RECEIVED, but in any case not MISSING) and above last_available_seq_num are unknown.
+        // In between those two numbers, every change that is not RECEIVED or IRRELEVANT is MISSING
+        for seq_num in
+            self.writer_proxy.first_available_seq_num..=self.writer_proxy.last_available_seq_num
+        {
+            let received = self.writer_proxy.received_changes.contains(&seq_num);
+            let irrelevant = self.writer_proxy.irrelevant_changes.contains(&seq_num);
+            if !(irrelevant || received) {
+                missing_changes.push(seq_num)
+            }
+        }
+        missing_changes
     }
 
-    fn missing_changes_update(&mut self, _last_available_seq_num: SequenceNumber) {
-        todo!()
+    fn missing_changes_update(&mut self, last_available_seq_num: SequenceNumber) {
+        // FOREACH change IN this.changes_from_writer
+        // SUCH-THAT ( change.status == UNKNOWN
+        // AND seq_num <= last_available_seq_num ) DO {
+        // change.status := MISSING;
+        // }
+        self.writer_proxy.last_available_seq_num = last_available_seq_num;
     }
 
-    fn received_change_set(&mut self, _a_seq_num: SequenceNumber) {
+    fn received_change_set(&mut self, a_seq_num: SequenceNumber) {
         // FIND change FROM this.changes_from_writer
         //     SUCH-THAT change.sequenceNumber == a_seq_num;
         // change.status := RECEIVED
-        println!("/!\\ RtpsWriterProxyImpl: received_change_set from RtpsWriterProxyOperations not implemented");
+        self.writer_proxy.received_changes.push(a_seq_num);
     }
 }
 
