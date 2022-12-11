32bdaf60b4e248b9fc3febfbea93b8bd255dbbc9
Joao Rebelo <jrebelo@s2e-systems.com>
2022-12-11 10:25:35 +0100

    Fix clippy warnings

--- dds/src/infrastructure/qos.rs
+++ dds/src/infrastructure/qos.rs
@@ -96,14 +96,14 @@ impl DataWriterQos {
     pub fn is_consistent(&self) -> DdsResult<()> {
         // The setting of RESOURCE_LIMITS max_samples must be consistent with the max_samples_per_instance. For these two
         // values to be consistent they must verify that “max_samples >= max_samples_per_instanc
-        if !(self.resource_limits.max_samples >= self.resource_limits.max_samples_per_instance) {
+        if self.resource_limits.max_samples < self.resource_limits.max_samples_per_instance {
             return Err(DdsError::InconsistentPolicy);
         }
 
         // The setting of RESOURCE_LIMITS max_samples_per_instance must be consistent with the HISTORY depth. For these two
         // QoS to be consistent, they must verify that “depth <= max_samples_per_instance.”
         if self.history.kind == HistoryQosPolicyKind::KeepLast
-            && !(self.history.depth as usize <= self.resource_limits.max_samples_per_instance)
+            && self.history.depth as usize > self.resource_limits.max_samples_per_instance
         {
             return Err(DdsError::InconsistentPolicy);
         }
@@ -189,21 +189,21 @@ impl DataReaderQos {
     pub fn is_consistent(&self) -> DdsResult<()> {
         // The setting of RESOURCE_LIMITS max_samples must be consistent with the max_samples_per_instance. For these two
         // values to be consistent they must verify that “max_samples >= max_samples_per_instance.”
-        if !(self.resource_limits.max_samples >= self.resource_limits.max_samples_per_instance) {
+        if self.resource_limits.max_samples < self.resource_limits.max_samples_per_instance {
             return Err(DdsError::InconsistentPolicy);
         }
 
         // The setting of RESOURCE_LIMITS max_samples_per_instance must be consistent with the HISTORY depth. For these two
         // QoS to be consistent, they must verify that “depth <= max_samples_per_instance.”
         if self.history.kind == HistoryQosPolicyKind::KeepLast
-            && !(self.history.depth as usize <= self.resource_limits.max_samples_per_instance)
+            && self.history.depth as usize > self.resource_limits.max_samples_per_instance
         {
             return Err(DdsError::InconsistentPolicy);
         }
 
         // The setting of the DEADLINE policy must be set consistently with that of the TIME_BASED_FILTER. For these two policies
         // to be consistent the settings must be such that “deadline period>= minimum_separation.”
-        if !(self.deadline.period >= self.time_based_filter.minimum_separation) {
+        if self.deadline.period < self.time_based_filter.minimum_separation {
             return Err(DdsError::InconsistentPolicy);
         }
 
@@ -269,14 +269,14 @@ impl TopicQos {
     pub fn is_consistent(&self) -> DdsResult<()> {
         // The setting of RESOURCE_LIMITS max_samples must be consistent with the max_samples_per_instance. For these two
         // values to be consistent they must verify that “max_samples >= max_samples_per_instance.”
-        if !(self.resource_limits.max_samples >= self.resource_limits.max_samples_per_instance) {
+        if self.resource_limits.max_samples < self.resource_limits.max_samples_per_instance {
             return Err(DdsError::InconsistentPolicy);
         }
 
         // The setting of RESOURCE_LIMITS max_samples_per_instance must be consistent with the HISTORY depth. For these two
         // QoS to be consistent, they must verify that “depth <= max_samples_per_instance.”
         if self.history.kind == HistoryQosPolicyKind::KeepLast
-            && !(self.history.depth as usize <= self.resource_limits.max_samples_per_instance)
+            && self.history.depth as usize > self.resource_limits.max_samples_per_instance
         {
             return Err(DdsError::InconsistentPolicy);
         }
