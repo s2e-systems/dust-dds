2c3988ca7a67b2eb76d62a0f1c1a1811c7c04e1b
Stefan Kimmer <54408686+stkimmer@users.noreply.github.com>
2022-12-16 11:19:37 +0100

    DDS-241 Add interoperability tests (#27)
    * Add initial files
    
    * Make it best effort
    
    * make publisher C
    
    * Make subscriber C
    
    * move compatibility
    
    * Add dockerfile for interoperability tests
    
    * Add interoperability tests
    
    * Adjust name
    
    * revert subscriber timeouts
    
    * Rename jobs
    
    * Make home/circleci/project folder empty on new docker image
    
    * Adjust checkout path
    
    * Update cmake search path
    
    * fix cmake build
    
    * remove cd..
    
    * add s
    
    * remove ..
    
    * Rename Compatibilty to Interoperability and put the test in second place in pipeline
    
    * Fix conflicts
    
    Co-authored-by: Joao Rebelo <jrebelo@s2e-systems.com>

---
+++ dds/tests/interoperability/publisher/main.c
@@ -0,0 +1,62 @@
+#include "ddsc/dds.h"
+#include "HelloWorld.h"
+
+int main(int argc, char *argv[])
+{
+	const char *topic_name = "HelloWorld";
+
+	const dds_entity_t participant = dds_create_participant(DDS_DOMAIN_DEFAULT, NULL /*qos*/, NULL /*listener*/);
+	if (participant < 0)
+	{
+		DDS_FATAL("dds_create_participant: %s\n", dds_strretcode(-participant));
+	}
+	const dds_entity_t topic = dds_create_topic(participant, &HelloWorldType_desc, topic_name, NULL /*qos*/, NULL /*listener*/);
+	if (topic < 0)
+	{
+		DDS_FATAL("dds_create_topic: %s\n", dds_strretcode(-topic));
+	}
+	dds_qos_t *qos = dds_create_qos();
+	dds_qset_reliability(qos, DDS_RELIABILITY_RELIABLE, DDS_SECS(1));
+
+	const dds_entity_t data_writer = dds_create_writer(participant, topic, qos, NULL /*listener*/);
+	if (data_writer < 0)
+	{
+		DDS_FATAL("dds_create_writer: %s\n", dds_strretcode(-data_writer));
+	}
+
+	dds_return_t rc;
+
+	rc = dds_set_status_mask(data_writer, DDS_PUBLICATION_MATCHED_STATUS);
+	if (rc != DDS_RETCODE_OK)
+	{
+		DDS_FATAL("dds_set_status_mask: %s\n", dds_strretcode(-rc));
+	}
+
+	dds_entity_t waitset = dds_create_waitset(participant);
+
+	rc = dds_waitset_attach(waitset, data_writer, data_writer);
+	if (rc != DDS_RETCODE_OK)
+	{
+		DDS_FATAL("dds_waitset_attach: %s\n", dds_strretcode(-rc));
+	}
+
+	dds_attach_t wsresults[1];
+	const size_t wsresultsize = 1U;
+	rc = dds_waitset_wait(waitset, wsresults, wsresultsize, DDS_SECS(60));
+	if (rc == DDS_RETCODE_TIMEOUT) {
+		DDS_FATAL("dds_waitset_wait: timeout");
+	}
+	if (rc != wsresultsize)
+	{
+		DDS_FATAL("dds_waitset_wait: %s\n", dds_strretcode(-rc));
+	}
+
+
+	HelloWorldType msg = {8, "Hello world!"};
+	dds_write(data_writer, &msg);
+
+	rc = dds_wait_for_acks(data_writer, DDS_SECS(30));
+	if (rc != DDS_RETCODE_OK) {
+		DDS_FATAL("dds_wait_for_acks: %s\n", dds_strretcode(-rc));
+	}
+}
