c2d631c51db420f0b2e1f5bf857d4a1e7a11a563
Joao Rebelo <jrebelo@s2e-systems.com>
2023-01-06 16:57:54 +0100

    Add take_next_instance implementation

--- dds/src/implementation/rtps/reader.rs
+++ dds/src/implementation/rtps/reader.rs
@@ -624,6 +624,29 @@ impl RtpsReader {
             None => Err(DdsError::NoData),
         }
     }
+
+    pub fn take_next_instance<Foo>(
+        &mut self,
+        max_samples: i32,
+        previous_handle: Option<InstanceHandle>,
+        sample_states: &[SampleStateKind],
+        view_states: &[ViewStateKind],
+        instance_states: &[InstanceStateKind],
+    ) -> DdsResult<Vec<Sample<Foo>>>
+    where
+        Foo: for<'de> DdsDeserialize<'de>,
+    {
+        match self.next_instance(previous_handle) {
+            Some(next_handle) => self.take(
+                max_samples,
+                sample_states,
+                view_states,
+                instance_states,
+                Some(next_handle),
+            ),
+            None => Err(DdsError::NoData),
+        }
+    }
 }
 
 #[cfg(test)]
