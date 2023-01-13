9ab62c6578a5b75638005de706ad0c41e6f67669
Joao Rebelo <jrebelo@s2e-systems.com>
2023-01-13 17:31:33 +0100

    Extend test for reading discovered entity data

--- dds/tests/builtin_readers.rs
+++ dds/tests/builtin_readers.rs
@@ -5,15 +5,23 @@ use dust_dds::{
     },
     domain::domain_participant_factory::DomainParticipantFactory,
     infrastructure::{
-        qos::{DomainParticipantQos, QosKind},
-        qos_policy::UserDataQosPolicy,
+        qos::{DataReaderQos, DataWriterQos, DomainParticipantQos, QosKind, TopicQos},
+        qos_policy::{TopicDataQosPolicy, UserDataQosPolicy},
         status::NO_STATUS,
     },
     subscription::sample_info::{ANY_INSTANCE_STATE, ANY_SAMPLE_STATE, ANY_VIEW_STATE},
+    topic_definition::type_support::{DdsSerde, DdsType},
 };
 
 mod utils;
 use crate::utils::domain_id_generator::TEST_DOMAIN_ID_GENERATOR;
+
+#[derive(Debug, PartialEq, serde::Serialize, serde::Deserialize, DdsType, DdsSerde)]
+struct MyData {
+    #[key]
+    id: u8,
+    value: u8,
+}
 
 #[test]
 fn builtin_reader_access() {
@@ -45,13 +53,16 @@ fn builtin_reader_access() {
 #[test]
 fn get_discovery_data_from_builtin_reader() {
     let domain_id = TEST_DOMAIN_ID_GENERATOR.generate_unique_domain_id();
-    let participant_user_data = vec![1, 2, 3, 4];
+    let participant_user_data = vec![1, 2];
+    let topic_user_data = vec![3, 4];
+    let reader_user_data = vec![5, 6];
+    let writer_user_data = vec![7, 8];
 
     let participant = DomainParticipantFactory::get_instance()
         .create_participant(domain_id, QosKind::Default, None, NO_STATUS)
         .unwrap();
 
-    let _participant2 = DomainParticipantFactory::get_instance()
+    let participant2 = DomainParticipantFactory::get_instance()
         .create_participant(
             domain_id,
             QosKind::Specific(DomainParticipantQos {
@@ -65,6 +76,55 @@ fn get_discovery_data_from_builtin_reader() {
         )
         .unwrap();
 
+    let topic = participant2
+        .create_topic::<MyData>(
+            "topic_name",
+            QosKind::Specific(TopicQos {
+                topic_data: TopicDataQosPolicy {
+                    value: topic_user_data.clone(),
+                },
+                ..Default::default()
+            }),
+            None,
+            NO_STATUS,
+        )
+        .unwrap();
+
+    let publisher = participant2
+        .create_publisher(QosKind::Default, None, NO_STATUS)
+        .unwrap();
+
+    let _data_writer = publisher
+        .create_datawriter(
+            &topic,
+            QosKind::Specific(DataWriterQos {
+                user_data: UserDataQosPolicy {
+                    value: writer_user_data.clone(),
+                },
+                ..Default::default()
+            }),
+            None,
+            NO_STATUS,
+        )
+        .unwrap();
+
+    let subscriber = participant2
+        .create_subscriber(QosKind::Default, None, NO_STATUS)
+        .unwrap();
+
+    let _data_reader = subscriber
+        .create_datareader(
+            &topic,
+            QosKind::Specific(DataReaderQos {
+                user_data: UserDataQosPolicy {
+                    value: reader_user_data.clone(),
+                },
+                ..Default::default()
+            }),
+            None,
+            NO_STATUS,
+        )
+        .unwrap();
     let builtin_subscriber = participant.get_builtin_subscriber().unwrap();
 
     let participants_reader = builtin_subscriber
@@ -72,17 +132,17 @@ fn get_discovery_data_from_builtin_reader() {
         .unwrap()
         .unwrap();
 
-    let _topics_reader = builtin_subscriber
+    let topics_reader = builtin_subscriber
         .lookup_datareader::<TopicBuiltinTopicData>("DCPSTopic")
         .unwrap()
         .unwrap();
 
-    let _publications_reader = builtin_subscriber
+    let publications_reader = builtin_subscriber
         .lookup_datareader::<PublicationBuiltinTopicData>("DCPSPublication")
         .unwrap()
         .unwrap();
 
-    let _subscriptions_reader = builtin_subscriber
+    let subscriptions_reader = builtin_subscriber
         .lookup_datareader::<SubscriptionBuiltinTopicData>("DCPSSubscription")
         .unwrap()
         .unwrap();
@@ -90,6 +150,18 @@ fn get_discovery_data_from_builtin_reader() {
     std::thread::sleep(std::time::Duration::from_millis(2000));
 
     let participant_samples = participants_reader
+        .read(1, ANY_SAMPLE_STATE, ANY_VIEW_STATE, ANY_INSTANCE_STATE)
+        .unwrap();
+
+    let topic_samples = topics_reader
+        .read(1, ANY_SAMPLE_STATE, ANY_VIEW_STATE, ANY_INSTANCE_STATE)
+        .unwrap();
+
+    let subscription_samples = subscriptions_reader
+        .read(1, ANY_SAMPLE_STATE, ANY_VIEW_STATE, ANY_INSTANCE_STATE)
+        .unwrap();
+
+    let publication_samples = publications_reader
         .read(1, ANY_SAMPLE_STATE, ANY_VIEW_STATE, ANY_INSTANCE_STATE)
         .unwrap();
 
@@ -101,5 +173,30 @@ fn get_discovery_data_from_builtin_reader() {
             .user_data
             .value,
         &participant_user_data
+    );
+
+    assert_eq!(
+        &topic_samples[0].data.as_ref().unwrap().topic_data.value,
+        &topic_user_data
+    );
+
+    assert_eq!(
+        &subscription_samples[0]
+            .data
+            .as_ref()
+            .unwrap()
+            .user_data
+            .value,
+        &reader_user_data
+    );
+
+    assert_eq!(
+        &publication_samples[0]
+            .data
+            .as_ref()
+            .unwrap()
+            .user_data
+            .value,
+        &writer_user_data
     );
 }
