eb119734bfd82aebc1d216ec38b7efa189ce1dc7
Stefan Kimmer <skimmer@s2e-systems.com>
2023-01-30 09:44:48 +0100

    remove From RtpsWriterCacheChange for DataSubmessage

--- dds/src/implementation/rtps/history_cache.rs
+++ dds/src/implementation/rtps/history_cache.rs
@@ -146,55 +146,6 @@ impl RtpsWriterCacheChange {
     }
 }
 
-impl PartialEq for RtpsWriterCacheChange {
-    fn eq(&self, other: &Self) -> bool {
-        self.kind == other.kind
-            && self.writer_guid == other.writer_guid
-            && self.sequence_number == other.sequence_number
-            && self.instance_handle == other.instance_handle
-    }
-}
-
-impl<'a> From<&'a RtpsWriterCacheChange> for DataSubmessage<'a> {
-    fn from(val: &'a RtpsWriterCacheChange) -> Self {
-        let endianness_flag = true;
-        let inline_qos_flag = true;
-        let (data_flag, key_flag) = match val.kind() {
-            ChangeKind::Alive => (true, false),
-            ChangeKind::NotAliveDisposed | ChangeKind::NotAliveUnregistered => (false, true),
-            _ => todo!(),
-        };
-        let non_standard_payload_flag = false;
-        let reader_id = ENTITYID_UNKNOWN;
-        let writer_id = val.writer_guid().entity_id();
-        let writer_sn = val.sequence_number();
-        let inline_qos = ParameterList {
-            parameter: val
-                .inline_qos()
-                .iter()
-                .map(|p| Parameter {
-                    parameter_id: p.parameter_id.0,
-                    length: p.value.len() as i16,
-                    value: p.value.as_ref(),
-                })
-                .collect(),
-        };
-        let serialized_payload = SerializedPayload::new(val.data_value());
-        DataSubmessage {
-            endianness_flag,
-            inline_qos_flag,
-            data_flag,
-            key_flag,
-            non_standard_payload_flag,
-            reader_id,
-            writer_id,
-            writer_sn,
-            inline_qos,
-            serialized_payload,
-        }
-    }
-}
-
 impl RtpsWriterCacheChange {
     pub fn new(
         kind: ChangeKind,
