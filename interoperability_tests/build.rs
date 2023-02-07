34dde6620c73e000c238c6ef435da8d0a593fff7
Stefan Kimmer <skimmer@s2e-systems.com>
2023-02-07 14:28:03 +0100

    Add big data cyclone dds

--- interoperability_tests/build.rs
+++ interoperability_tests/build.rs
@@ -26,4 +26,24 @@ fn main() {
                 .expect("Failed to write to file");
         }
     }
+
+
+    let idl_path = "BigData.idl";
+    let idl_src = std::fs::read_to_string(idl_path).expect("(;_;) Couldn't read IDL source file!");
+
+    let result = parser::IdlParser::parse(parser::Rule::specification, &idl_src)
+        .expect("Couldn't parse IDL file");
+
+    let mut file = File::create("big_data.rs").expect("Failed to create file");
+
+    let spec = syntax::specification()
+        .analyse(result.into())
+        .expect("Couldn't analyse IDL syntax");
+
+    for def in spec.value {
+        for line in rust::definition(def) {
+            file.write_all(line.as_bytes())
+                .expect("Failed to write to file");
+        }
+    }
 }
