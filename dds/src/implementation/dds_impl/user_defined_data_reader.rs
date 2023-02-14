eabfb2bafb5ade898a033b792e86fa242160e7e8
Stefan Kimmer <skimmer@s2e-systems.com>
2023-02-14 09:44:32 +0100

    add timer cancellation

--- dds/src/implementation/dds_impl/user_defined_data_reader.rs
+++ dds/src/implementation/dds_impl/user_defined_data_reader.rs
@@ -1,6 +1,9 @@
 use std::{
     collections::HashMap,
-    sync::{Arc, Mutex},
+    sync::{
+        atomic::{AtomicBool, Ordering},
+        Arc, RwLock,
+    },
     thread::JoinHandle,
 };
 
@@ -231,40 +234,51 @@ impl UserDefinedDataReader {
 }
 
 struct Timer {
-    func: Box<dyn Fn() + Send>,
+    func: Box<dyn Fn() + Send + Sync>,
     duration: std::time::Duration,
     instant: std::time::Instant,
 }
 
 impl Timer {
-    pub fn new(duration: std::time::Duration, func: impl Fn() + 'static + Send) -> Self {
+    pub fn new(duration: std::time::Duration, func: impl Fn() + 'static + Send + Sync) -> Self {
         Self {
             func: Box::new(func),
             duration,
             instant: std::time::Instant::now(),
         }
     }
+    fn is_elapsed(&self) -> bool {
+        std::time::Instant::now() - self.instant > self.duration
+    }
 }
 struct TimerFactory {
-    instance_timers: Arc<Mutex<HashMap<InstanceHandle, Timer>>>,
-    thread_handle: JoinHandle<()>,
+    instance_timers: Arc<RwLock<HashMap<InstanceHandle, Timer>>>,
+    thread_handle: Option<JoinHandle<()>>,
+    should_stop: Arc<std::sync::atomic::AtomicBool>,
 }
 
 impl TimerFactory {
     fn new() -> Self {
-        let instance_timers = Arc::new(Mutex::new(HashMap::<InstanceHandle, Timer>::new()));
+        let instance_timers = Arc::new(RwLock::new(HashMap::<InstanceHandle, Timer>::new()));
         let t = instance_timers.clone();
+        let should_stop = Arc::new(AtomicBool::new(false));
+        let should_stop_clone = should_stop.clone();
         let thread_handle = std::thread::spawn(move || loop {
-            std::thread::sleep(std::time::Duration::from_millis(10));
-            for (_k, v) in t.lock().unwrap().iter() {
-                if std::time::Instant::now() - v.instant > v.duration {
-                    (v.func)()
+            if should_stop_clone.load(Ordering::Relaxed) {
+                break;
+            } else {
+                std::thread::sleep(std::time::Duration::from_millis(1));
+                for (_k, v) in t.read().unwrap().iter() {
+                    if v.is_elapsed() {
+                        (v.func)()
+                    }
                 }
             }
         });
         Self {
             instance_timers,
-            thread_handle,
+            thread_handle: Some(thread_handle),
+            should_stop,
         }
     }
 
@@ -272,12 +286,24 @@ impl TimerFactory {
         &self,
         duration: std::time::Duration,
         id: InstanceHandle,
-        func: impl Fn() + 'static + Send,
+        func: impl Fn() + 'static + Send + Sync,
     ) {
         self.instance_timers
-            .lock()
+            .write()
             .unwrap()
             .insert(id, Timer::new(duration, func));
+    }
+
+    fn cancel_timers(&self) {
+        self.instance_timers.write().unwrap().clear();
+    }
+}
+
+impl Drop for TimerFactory {
+    fn drop(&mut self) {
+        self.cancel_timers();
+        self.should_stop.store(true, Ordering::Relaxed);
+        self.thread_handle.take().unwrap().join().unwrap();
     }
 }
 
