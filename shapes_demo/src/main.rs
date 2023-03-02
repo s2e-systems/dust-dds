3e51ca359f04149c5a590fcc286ec3fb07fb22f2
Stefan Kimmer <skimmer@s2e-systems.com>
2023-03-02 12:30:46 +0100

    Degrade eframe and fix serde version

--- shapes_demo/src/main.rs
+++ shapes_demo/src/main.rs
@@ -22,7 +22,7 @@ use eframe::{
     Theme,
 };
 
-fn main() -> Result<(), eframe::Error> {
+fn main() {
     let options = eframe::NativeOptions {
         initial_window_size: Some(egui::vec2(500.0, 500.0)),
         default_theme: Theme::Light,
@@ -33,7 +33,7 @@ fn main() -> Result<(), eframe::Error> {
         "Dust DDS Shapes Demo",
         options,
         Box::new(|_cc| Box::new(MyApp::new())),
-    )
+    );
 }
 
 struct MyApp {
