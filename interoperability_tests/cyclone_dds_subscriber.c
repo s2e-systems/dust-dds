270a08656af3c97538b63eb0a89391e774465e1b
Stefan Kimmer <skimmer@s2e-systems.com>
2023-03-06 13:32:45 +0100

    add wait to cyclone subscribers

--- interoperability_tests/cyclone_dds_subscriber.c
+++ interoperability_tests/cyclone_dds_subscriber.c
@@ -86,4 +86,10 @@ int main(int argc, char *argv[])
 		msg = (HelloWorldType *)samples[0];
 		printf("Received: HelloWorldType { id: %d, msg: \"%c\" }\n", msg->id, msg->msg);
 	}
+
+	rc = dds_waitset_wait(waitset, wsresults, wsresultsize, DDS_SECS(30));
+	if (rc == 0)
+	{
+		DDS_FATAL("dds_waitset_wait: timeout: Probably subscription not un-matched");
+	}
 }
