f92e98f9e90532ae6bf0c26d2c7de5f1654089b0
Joao Rebelo <jrebelo@s2e-systems.com>
2023-03-31 12:50:36 +0200

    Add compilation of BigData idl

--- interoperability_tests/build.rs
+++ interoperability_tests/build.rs
@@ -10,4 +10,14 @@ fn main() {
 
     file.write_all(compiled_idl.as_bytes())
         .expect("Failed to write to file");
+
+    let idl_path = "BigData.idl";
+    let idl_src = std::fs::read_to_string(idl_path).expect("(;_;) Couldn't read IDL source file!");
+
+    let compiled_idl = dust_idlgen::compile_idl(&idl_src).expect("Couldn't parse IDL file");
+
+    let mut file = File::create("big_data.rs").expect("Failed to create file");
+
+    file.write_all(compiled_idl.as_bytes())
+        .expect("Failed to write to file");
 }
