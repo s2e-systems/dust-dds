44a8ff8ee2f41926274742be17e9bd940c3dd1d0
Joao Rebelo <jrebelo@s2e-systems.com>
2023-04-06 13:27:22 +0200

    Mark race condition test ignored

--- dds/tests/write_read_samples.rs
+++ dds/tests/write_read_samples.rs
@@ -2012,6 +2012,7 @@ fn write_read_unregistered_samples_are_also_disposed() {
 }
 
 #[test]
+#[ignore = "removing expired data causes race condition"]
 fn transient_local_writer_does_not_deliver_lifespan_expired_data() {
     let domain_id = TEST_DOMAIN_ID_GENERATOR.generate_unique_domain_id();
 
