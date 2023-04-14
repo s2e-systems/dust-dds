26bfbdec76b1290a3f5ea4fa15a08f976e869190
Stefan Kimmer <skimmer@s2e-systems.com>
2023-04-14 13:22:19 +0200

    Add doc

--- dds/src/implementation/configuration.rs
+++ dds/src/implementation/configuration.rs
@@ -4,6 +4,10 @@ use serde::Deserialize;
 #[derive(Deserialize, JsonSchema, Debug, PartialEq, Eq)]
 #[serde(default)]
 /// Configuration that can be dynamically loaded
+/// The environment DUST_DDS_CONFIGURATION variable can be set
+/// as a json to modify the default configuration.
+///
+/// E.g. $Env:DUST_DDS_CONFIGURATION='{"interface_name":"Wi-Fi"}'
 pub struct DustDdsConfiguration {
     /// # Domain tag
     /// Domain tag to use for the participant
