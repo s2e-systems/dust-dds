69f3d7c3f7fe9b456d841d3a8ceec8d76c13bb13
Stefan Kimmer <skimmer@s2e-systems.com>
2023-05-11 15:39:15 +0200

    add two_participants_should_get_subscription_matched test

--- dds/tests/discovery.rs
+++ dds/tests/discovery.rs
@@ -271,6 +271,50 @@ fn updated_writers_are_announced_to_reader() {
 }
 
 #[test]
+fn two_participants_should_get_subscription_matched() {
+    let domain_id = TEST_DOMAIN_ID_GENERATOR.generate_unique_domain_id();
+    let domain_participant_factory = DomainParticipantFactory::get_instance();
+
+    let dp1 = domain_participant_factory
+        .create_participant(domain_id, QosKind::Default, None, NO_STATUS)
+        .unwrap();
+    let topic1 = dp1
+        .create_topic::<UserType>("topic_name", QosKind::Default, None, NO_STATUS)
+        .unwrap();
+    let publisher = dp1
+        .create_publisher(QosKind::Default, None, NO_STATUS)
+        .unwrap();
+    let data_writer = publisher
+        .create_datawriter::<UserType>(&topic1, QosKind::Default, None, NO_STATUS)
+        .unwrap();
+
+    let dp2 = domain_participant_factory
+        .create_participant(domain_id, QosKind::Default, None, NO_STATUS)
+        .unwrap();
+    let topic2 = dp2
+    .create_topic::<UserType>("topic_name", QosKind::Default, None, NO_STATUS)
+    .unwrap();
+    let subscriber = dp2
+        .create_subscriber(QosKind::Default, None, NO_STATUS)
+        .unwrap();
+
+    let _data_reader = subscriber
+        .create_datareader::<UserType>(&topic2, QosKind::Default, None, NO_STATUS)
+        .unwrap();
+    let cond = data_writer.get_statuscondition().unwrap();
+    cond.set_enabled_statuses(&[StatusKind::PublicationMatched])
+        .unwrap();
+
+    let mut wait_set = WaitSet::new();
+    wait_set
+        .attach_condition(Condition::StatusCondition(cond))
+        .unwrap();
+    wait_set.wait(Duration::new(5, 0)).unwrap();
+
+    assert_eq!(data_writer.get_matched_subscriptions().unwrap().len(), 1);
+}
+
+#[test]
 fn participant_records_discovered_topics() {
     let domain_id = TEST_DOMAIN_ID_GENERATOR.generate_unique_domain_id();
     let domain_participant_factory = DomainParticipantFactory::get_instance();
