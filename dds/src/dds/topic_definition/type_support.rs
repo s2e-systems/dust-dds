fe11de8e4128c6afc6df3361e2bd1559b64eb386
Joao Rebelo <jrebelo@s2e-systems.com>
2023-10-23 23:11:34 +0200

    Add new dds serialize and serializer traits

--- dds/src/dds/topic_definition/type_support.rs
+++ dds/src/dds/topic_definition/type_support.rs
@@ -529,3 +529,154 @@ where
 {
     type OwningKeyHolder = T;
 }
+
+pub trait NewDdsSerialize {
+    fn serialize(&self, serializer: &mut impl DdsSerializer) -> DdsResult<()>;
+}
+
+impl NewDdsSerialize for bool {
+    fn serialize(&self, serializer: &mut impl DdsSerializer) -> DdsResult<()> {
+        serializer.serialize_bool(*self)
+    }
+}
+
+impl NewDdsSerialize for i8 {
+    fn serialize(&self, serializer: &mut impl DdsSerializer) -> DdsResult<()> {
+        serializer.serialize_i8(*self)
+    }
+}
+
+impl NewDdsSerialize for i16 {
+    fn serialize(&self, serializer: &mut impl DdsSerializer) -> DdsResult<()> {
+        serializer.serialize_i16(*self)
+    }
+}
+
+impl NewDdsSerialize for i32 {
+    fn serialize(&self, serializer: &mut impl DdsSerializer) -> DdsResult<()> {
+        serializer.serialize_i32(*self)
+    }
+}
+
+impl NewDdsSerialize for i64 {
+    fn serialize(&self, serializer: &mut impl DdsSerializer) -> DdsResult<()> {
+        serializer.serialize_i64(*self)
+    }
+}
+
+impl NewDdsSerialize for u8 {
+    fn serialize(&self, serializer: &mut impl DdsSerializer) -> DdsResult<()> {
+        serializer.serialize_u8(*self)
+    }
+}
+
+impl NewDdsSerialize for u16 {
+    fn serialize(&self, serializer: &mut impl DdsSerializer) -> DdsResult<()> {
+        serializer.serialize_u16(*self)
+    }
+}
+
+impl NewDdsSerialize for u32 {
+    fn serialize(&self, serializer: &mut impl DdsSerializer) -> DdsResult<()> {
+        serializer.serialize_u32(*self)
+    }
+}
+
+impl NewDdsSerialize for u64 {
+    fn serialize(&self, serializer: &mut impl DdsSerializer) -> DdsResult<()> {
+        serializer.serialize_u64(*self)
+    }
+}
+
+impl NewDdsSerialize for f32 {
+    fn serialize(&self, serializer: &mut impl DdsSerializer) -> DdsResult<()> {
+        serializer.serialize_f32(*self)
+    }
+}
+
+impl NewDdsSerialize for f64 {
+    fn serialize(&self, serializer: &mut impl DdsSerializer) -> DdsResult<()> {
+        serializer.serialize_f64(*self)
+    }
+}
+
+impl NewDdsSerialize for char {
+    fn serialize(&self, serializer: &mut impl DdsSerializer) -> DdsResult<()> {
+        serializer.serialize_char(*self)
+    }
+}
+
+impl NewDdsSerialize for str {
+    fn serialize(&self, serializer: &mut impl DdsSerializer) -> DdsResult<()> {
+        serializer.serialize_str(self)
+    }
+}
+
+impl NewDdsSerialize for String {
+    fn serialize(&self, serializer: &mut impl DdsSerializer) -> DdsResult<()> {
+        serializer.serialize_str(self)
+    }
+}
+
+impl<T> NewDdsSerialize for [T]
+where
+    T: NewDdsSerialize,
+{
+    fn serialize(&self, serializer: &mut impl DdsSerializer) -> DdsResult<()> {
+        serializer.serialize_seq(self)
+    }
+}
+
+impl<T> NewDdsSerialize for Vec<T>
+where
+    T: NewDdsSerialize,
+{
+    fn serialize(&self, serializer: &mut impl DdsSerializer) -> DdsResult<()> {
+        serializer.serialize_seq(self)
+    }
+}
+
+impl<T> NewDdsSerialize for &'_ T
+where
+    T: NewDdsSerialize,
+{
+    fn serialize(&self, serializer: &mut impl DdsSerializer) -> DdsResult<()> {
+        T::serialize(*self, serializer)
+    }
+}
+
+impl<T> NewDdsSerialize for &'_ mut T
+where
+    T: NewDdsSerialize,
+{
+    fn serialize(&self, serializer: &mut impl DdsSerializer) -> DdsResult<()> {
+        T::serialize(*self, serializer)
+    }
+}
+
+impl<T> NewDdsSerialize for Box<T>
+where
+    T: NewDdsSerialize,
+{
+    fn serialize(&self, serializer: &mut impl DdsSerializer) -> DdsResult<()> {
+        self.as_ref().serialize(serializer)
+    }
+}
+
+pub trait DdsSerializer {
+    fn serialize_bool(&mut self, v: bool) -> DdsResult<()>;
+    fn serialize_i8(&mut self, v: i8) -> DdsResult<()>;
+    fn serialize_i16(&mut self, v: i16) -> DdsResult<()>;
+    fn serialize_i32(&mut self, v: i32) -> DdsResult<()>;
+    fn serialize_i64(&mut self, v: i64) -> DdsResult<()>;
+    fn serialize_u8(&mut self, v: u8) -> DdsResult<()>;
+    fn serialize_u16(&mut self, v: u16) -> DdsResult<()>;
+    fn serialize_u32(&mut self, v: u32) -> DdsResult<()>;
+    fn serialize_u64(&mut self, v: u64) -> DdsResult<()>;
+    fn serialize_f32(&mut self, v: f32) -> DdsResult<()>;
+    fn serialize_f64(&mut self, v: f64) -> DdsResult<()>;
+    fn serialize_char(&mut self, v: char) -> DdsResult<()>;
+    fn serialize_str(&mut self, v: &str) -> DdsResult<()>;
+    fn serialize_seq(&mut self, v: &[impl NewDdsSerialize]) -> DdsResult<()>;
+    fn serialize_array<const N: usize>(&mut self, v: &[impl NewDdsSerialize; N]);
+}
