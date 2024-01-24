a6a9062c49a4550fd4fdbf6f8751eea09dad0d46
Joao Rebelo <jrebelo@s2e-systems.com>
2024-01-24 11:59:04 +0100

    Add simple struct test

---
+++ dds_gen/src/generator/rust.rs
@@ -0,0 +1,422 @@
+use crate::parser::{IdlPair, Rule};
+
+pub fn generate_rust_source(pair: IdlPair, writer: &mut String) -> Result<(), String> {
+    match pair.as_rule() {
+        Rule::EOI => (),
+        Rule::alpha => todo!(),
+        Rule::digit => todo!(),
+        Rule::octal_digit => todo!(),
+        Rule::hex_digit => todo!(),
+        Rule::escape => todo!(),
+        Rule::octal_escape => todo!(),
+        Rule::hex_escape => todo!(),
+        Rule::unicode_escape => todo!(),
+        Rule::newline => todo!(),
+        Rule::WHITESPACE => todo!(),
+        Rule::path_spec => todo!(),
+        Rule::include_directive => todo!(),
+        Rule::other_directive => todo!(),
+        Rule::block_comment => todo!(),
+        Rule::line_comment => todo!(),
+        Rule::COMMENT => todo!(),
+        Rule::identifier => identifier(pair, writer)?,
+        Rule::character_literal => todo!(),
+        Rule::string_literal => todo!(),
+        Rule::wide_character_literal => todo!(),
+        Rule::wide_string_literal => todo!(),
+        Rule::integer_literal => todo!(),
+        Rule::decimal_integer_literal => todo!(),
+        Rule::octal_integer_literal => todo!(),
+        Rule::hex_integer_literal => todo!(),
+        Rule::fixed_pt_literal => todo!(),
+        Rule::floating_pt_literal => todo!(),
+        Rule::integral_part => todo!(),
+        Rule::fractional_part => todo!(),
+        Rule::exponent => todo!(),
+        Rule::float_suffix => todo!(),
+        Rule::specification => specification(pair, writer)?,
+        Rule::definition => definition(pair, writer)?,
+        Rule::module_dcl => todo!(),
+        Rule::scoped_name => todo!(),
+        Rule::const_dcl => todo!(),
+        Rule::const_type => todo!(),
+        Rule::const_expr => todo!(),
+        Rule::or_expr => todo!(),
+        Rule::xor_expr => todo!(),
+        Rule::and_expr => todo!(),
+        Rule::lshift_expr => todo!(),
+        Rule::rshift_expr => todo!(),
+        Rule::add_expr => todo!(),
+        Rule::sub_expr => todo!(),
+        Rule::mul_expr => todo!(),
+        Rule::div_expr => todo!(),
+        Rule::mod_expr => todo!(),
+        Rule::unary_expr => todo!(),
+        Rule::unary_operator => todo!(),
+        Rule::primary_expr => todo!(),
+        Rule::literal => todo!(),
+        Rule::boolean_literal => todo!(),
+        Rule::positive_int_const => todo!(),
+        Rule::type_dcl => type_dcl(pair, writer)?,
+        Rule::type_spec => type_spec(pair, writer)?,
+        Rule::simple_type_spec => simple_type_spec(pair, writer)?,
+        Rule::base_type_spec => base_type_spec(pair, writer)?,
+        Rule::floating_pt_type => todo!(),
+        Rule::float => todo!(),
+        Rule::double => todo!(),
+        Rule::long_double => todo!(),
+        Rule::integer_type => integer_type(pair, writer)?,
+        Rule::signed_int => signed_int(pair, writer)?,
+        Rule::signed_short_int => todo!(),
+        Rule::signed_long_int => signed_long_int(pair, writer)?,
+        Rule::signed_longlong_int => todo!(),
+        Rule::unsigned_int => todo!(),
+        Rule::unsigned_short_int => todo!(),
+        Rule::unsigned_long_int => todo!(),
+        Rule::unsigned_longlong_int => todo!(),
+        Rule::char_type => todo!(),
+        Rule::wide_char_type => todo!(),
+        Rule::boolean_type => todo!(),
+        Rule::octet_type => todo!(),
+        Rule::template_type_spec => todo!(),
+        Rule::sequence_type => todo!(),
+        Rule::string_type => todo!(),
+        Rule::wide_string_type => todo!(),
+        Rule::fixed_pt_type => todo!(),
+        Rule::fixed_pt_const_type => todo!(),
+        Rule::constr_type_dcl => constr_type_dcl(pair, writer)?,
+        Rule::struct_dcl => struct_dcl(pair, writer)?,
+        Rule::struct_def => struct_def(pair, writer)?,
+        Rule::member => member(pair, writer)?,
+        Rule::struct_forward_dcl => todo!(),
+        Rule::union_dcl => todo!(),
+        Rule::union_def => todo!(),
+        Rule::switch_type_spec => todo!(),
+        Rule::switch_body => todo!(),
+        Rule::case => todo!(),
+        Rule::case_label => todo!(),
+        Rule::element_spec => todo!(),
+        Rule::union_forward_dcl => todo!(),
+        Rule::enum_dcl => todo!(),
+        Rule::enumerator => todo!(),
+        Rule::array_declarator => todo!(),
+        Rule::fixed_array_size => todo!(),
+        Rule::native_dcl => todo!(),
+        Rule::simple_declarator => simple_declarator(pair, writer)?,
+        Rule::typedef_dcl => todo!(),
+        Rule::type_declarator => todo!(),
+        Rule::ANY_declarators => todo!(),
+        Rule::ANY_declarator => todo!(),
+        Rule::declarators => declarators(pair, writer)?,
+        Rule::declarator => declarator(pair, writer)?,
+        Rule::ANY_type => todo!(),
+        Rule::except_dcl => todo!(),
+        Rule::interface_dcl => todo!(),
+        Rule::interface_def => todo!(),
+        Rule::interface_forward_dcl => todo!(),
+        Rule::interface_header => todo!(),
+        Rule::interface_kind => todo!(),
+        Rule::interface_inheritance_spec => todo!(),
+        Rule::interface_name => todo!(),
+        Rule::interface_body => todo!(),
+        Rule::export => todo!(),
+        Rule::op_dcl => todo!(),
+        Rule::op_type_spec => todo!(),
+        Rule::parameter_dcls => todo!(),
+        Rule::param_dcl => todo!(),
+        Rule::param_attribute => todo!(),
+        Rule::raises_expr => todo!(),
+        Rule::attr_dcl => todo!(),
+        Rule::readonly_attr_spec => todo!(),
+        Rule::readonly_attr_declarator => todo!(),
+        Rule::attr_spec => todo!(),
+        Rule::attr_declarator => todo!(),
+        Rule::attr_raises_expr => todo!(),
+        Rule::get_excep_expr => todo!(),
+        Rule::set_excep_expr => todo!(),
+        Rule::exception_list => todo!(),
+        Rule::value_dcl => todo!(),
+        Rule::value_def => todo!(),
+        Rule::value_header => todo!(),
+        Rule::value_kind => todo!(),
+        Rule::value_inheritance_spec => todo!(),
+        Rule::value_name => todo!(),
+        Rule::value_element => todo!(),
+        Rule::state_member => todo!(),
+        Rule::init_dcl => todo!(),
+        Rule::init_param_dcls => todo!(),
+        Rule::init_param_dcl => todo!(),
+        Rule::value_forward_dcl => todo!(),
+        Rule::type_id_dcl => todo!(),
+        Rule::type_prefix_dcl => todo!(),
+        Rule::import_dcl => todo!(),
+        Rule::imported_scope => todo!(),
+        Rule::object_type => todo!(),
+        Rule::op_oneway_dcl => todo!(),
+        Rule::in_parameter_dcls => todo!(),
+        Rule::in_param_dcl => todo!(),
+        Rule::op_with_context => todo!(),
+        Rule::context_expr => todo!(),
+        Rule::value_box_def => todo!(),
+        Rule::value_abs_def => todo!(),
+        Rule::value_base_type => todo!(),
+        Rule::component_dcl => todo!(),
+        Rule::component_forward_dcl => todo!(),
+        Rule::component_def => todo!(),
+        Rule::component_header => todo!(),
+        Rule::component_inheritance_spec => todo!(),
+        Rule::component_body => todo!(),
+        Rule::component_export => todo!(),
+        Rule::provides_dcl => todo!(),
+        Rule::interface_type => todo!(),
+        Rule::uses_dcl => todo!(),
+        Rule::home_dcl => todo!(),
+        Rule::home_header => todo!(),
+        Rule::home_inheritance_spec => todo!(),
+        Rule::home_body => todo!(),
+        Rule::home_export => todo!(),
+        Rule::factory_dcl => todo!(),
+        Rule::factory_param_dcls => todo!(),
+        Rule::factory_param_dcl => todo!(),
+        Rule::supported_interface_spec => todo!(),
+        Rule::emits_dcl => todo!(),
+        Rule::publishes_dcl => todo!(),
+        Rule::consumes_dcl => todo!(),
+        Rule::primary_key_spec => todo!(),
+        Rule::finder_dcl => todo!(),
+        Rule::event_dcl => todo!(),
+        Rule::event_forward_dcl => todo!(),
+        Rule::event_abs_def => todo!(),
+        Rule::event_def => todo!(),
+        Rule::event_header => todo!(),
+        Rule::porttype_dcl => todo!(),
+        Rule::porttype_forward_dcl => todo!(),
+        Rule::porttype_def => todo!(),
+        Rule::port_body => todo!(),
+        Rule::port_ref => todo!(),
+        Rule::port_export => todo!(),
+        Rule::port_dcl => todo!(),
+        Rule::connector_dcl => todo!(),
+        Rule::connector_header => todo!(),
+        Rule::connector_inherit_spec => todo!(),
+        Rule::connector_export => todo!(),
+        Rule::template_module_dcl => todo!(),
+        Rule::formal_parameters => todo!(),
+        Rule::formal_parameter => todo!(),
+        Rule::formal_parameter_type => todo!(),
+        Rule::tpl_definition => todo!(),
+        Rule::template_module_inst => todo!(),
+        Rule::actual_parameters => todo!(),
+        Rule::actual_parameter => todo!(),
+        Rule::template_module_ref => todo!(),
+        Rule::formal_parameter_names => todo!(),
+        Rule::map_type => todo!(),
+        Rule::bitset_dcl => todo!(),
+        Rule::bitfield => todo!(),
+        Rule::bitfield_spec => todo!(),
+        Rule::destination_type => todo!(),
+        Rule::bitmask_dcl => todo!(),
+        Rule::bit_value => todo!(),
+        Rule::annotation_dcl => todo!(),
+        Rule::annotation_header => todo!(),
+        Rule::annotation_body => todo!(),
+        Rule::annotation_member => todo!(),
+        Rule::annotation_member_type => todo!(),
+        Rule::ANY_const_type => todo!(),
+        Rule::annotation_appl => todo!(),
+        Rule::annotation_appl_params => todo!(),
+        Rule::annotation_appl_param => todo!(),
+    }
+    Ok(())
+}
+
+fn specification(pair: IdlPair, writer: &mut String) -> Result<(), String> {
+    for definition in pair.into_inner() {
+        generate_rust_source(definition, writer)?;
+    }
+    Ok(())
+}
+
+fn definition(pair: IdlPair, writer: &mut String) -> Result<(), String> {
+    generate_rust_source(
+        pair.into_inner()
+            .next()
+            .expect("Must have an element according to the grammar"),
+        writer,
+    )
+}
+
+fn type_dcl(pair: IdlPair, writer: &mut String) -> Result<(), String> {
+    generate_rust_source(
+        pair.into_inner()
+            .next()
+            .expect("Must have an element according to the grammar"),
+        writer,
+    )
+}
+
+fn constr_type_dcl(pair: IdlPair, writer: &mut String) -> Result<(), String> {
+    generate_rust_source(
+        pair.into_inner()
+            .next()
+            .expect("Must have an element according to the grammar"),
+        writer,
+    )
+}
+
+fn struct_dcl(pair: IdlPair, writer: &mut String) -> Result<(), String> {
+    generate_rust_source(
+        pair.into_inner()
+            .next()
+            .expect("Must have an element according to the grammar"),
+        writer,
+    )
+}
+
+fn struct_def(pair: IdlPair, writer: &mut String) -> Result<(), String> {
+    let inner_pairs = pair.into_inner();
+    let identifier = inner_pairs
+        .clone()
+        .find(|p| p.as_rule() == Rule::identifier)
+        .expect("Identifier must exist according to the grammar");
+
+    writer.push_str("pub struct ");
+    generate_rust_source(identifier, writer)?;
+
+    writer.push_str(" {");
+
+    for member in inner_pairs.filter(|p| p.as_rule() == Rule::member) {
+        generate_rust_source(member, writer)?;
+    }
+
+    writer.push_str("}\n");
+    Ok(())
+}
+
+fn member(pair: IdlPair, writer: &mut String) -> Result<(), String> {
+    let inner_pairs = pair.into_inner();
+
+    let type_spec = inner_pairs
+        .clone()
+        .filter(|p| p.as_rule() == Rule::type_spec)
+        .next()
+        .expect("Declarator must exist according to grammar");
+    let declarators = inner_pairs
+        .clone()
+        .filter(|p| p.as_rule() == Rule::declarators)
+        .next()
+        .expect("Declarator must exist according to grammar");
+
+    generate_rust_source(declarators, writer)?;
+    writer.push_str(": ");
+    generate_rust_source(type_spec, writer)?;
+    writer.push_str(",");
+
+    Ok(())
+}
+
+fn declarators(pair: IdlPair, writer: &mut String) -> Result<(), String> {
+    for declarator in pair.into_inner() {
+        generate_rust_source(declarator, writer)?;
+    }
+    Ok(())
+}
+
+fn declarator(pair: IdlPair, writer: &mut String) -> Result<(), String> {
+    generate_rust_source(
+        pair.into_inner()
+            .next()
+            .expect("Must have an element according to the grammar"),
+        writer,
+    )
+}
+
+fn simple_declarator(pair: IdlPair, writer: &mut String) -> Result<(), String> {
+    generate_rust_source(
+        pair.into_inner()
+            .next()
+            .expect("Must have an element according to the grammar"),
+        writer,
+    )
+}
+
+fn identifier(pair: IdlPair, writer: &mut String) -> Result<(), String> {
+    writer.push_str(pair.as_str());
+    Ok(())
+}
+
+fn type_spec(pair: IdlPair, writer: &mut String) -> Result<(), String> {
+    generate_rust_source(
+        pair.into_inner()
+            .next()
+            .expect("Must have an element according to the grammar"),
+        writer,
+    )
+}
+
+fn simple_type_spec(pair: IdlPair, writer: &mut String) -> Result<(), String> {
+    generate_rust_source(
+        pair.into_inner()
+            .next()
+            .expect("Must have an element according to the grammar"),
+        writer,
+    )
+}
+
+fn base_type_spec(pair: IdlPair, writer: &mut String) -> Result<(), String> {
+    generate_rust_source(
+        pair.into_inner()
+            .next()
+            .expect("Must have an element according to the grammar"),
+        writer,
+    )
+}
+
+fn integer_type(pair: IdlPair, writer: &mut String) -> Result<(), String> {
+    generate_rust_source(
+        pair.into_inner()
+            .next()
+            .expect("Must have an element according to the grammar"),
+        writer,
+    )
+}
+
+fn signed_int(pair: IdlPair, writer: &mut String) -> Result<(), String> {
+    generate_rust_source(
+        pair.into_inner()
+            .next()
+            .expect("Must have an element according to the grammar"),
+        writer,
+    )
+}
+
+fn signed_long_int(pair: IdlPair, writer: &mut String) -> Result<(), String> {
+    writer.push_str("i32");
+    Ok(())
+}
+
+#[cfg(test)]
+mod tests {
+    use pest::Parser;
+
+    use crate::parser::IdlParser;
+
+    use super::*;
+
+    #[test]
+    fn parse_struct() {
+        let mut out = String::new();
+        let p = IdlParser::parse(
+            Rule::struct_def,
+            "struct MyStruct {
+            long a;
+        };",
+        )
+        .unwrap()
+        .next()
+        .unwrap();
+        generate_rust_source(p, &mut out).unwrap();
+        println!("RESULT: {}", out);
+        assert_eq!("pub struct MyStruct {a: i32,}\n", &out);
+    }
+}
--- dds_gen/src/parser/mod.rs
+++ dds_gen/src/parser/mod.rs
@@ -4,5 +4,5 @@ use pest_derive::Parser;
 #[grammar = "parser/idl_v4_grammar.pest"]
 pub struct IdlParser;
 
-pub type Pair<'i> = pest::iterators::Pair<'i, Rule>;
-pub type Pairs<'i> = pest::iterators::Pairs<'i, Rule>;
+pub type IdlPair<'i> = pest::iterators::Pair<'i, Rule>;
+pub type IdlPairList<'i> = pest::iterators::Pairs<'i, Rule>;
