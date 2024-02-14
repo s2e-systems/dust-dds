92ed217a48c1feed430a8f5e80dc50f918746c6f
Stefan Kimmer <skimmer@s2e-systems.com>
2024-02-14 16:00:56 +0100

    Remove derive-more crate

--- dds/src/dds/infrastructure/instance.rs
+++ dds/src/dds/infrastructure/instance.rs
@@ -1,17 +1,18 @@
 /// Type for the instance handle representing an Entity
-#[derive(
-    Clone,
-    Copy,
-    PartialEq,
-    Eq,
-    Debug,
-    Hash,
-    PartialOrd,
-    Ord,
-    derive_more::Constructor,
-    derive_more::AsRef,
-)]
+#[derive(Clone, Copy, PartialEq, Eq, Debug, Hash, PartialOrd, Ord)]
 pub struct InstanceHandle([u8; 16]);
+
+impl InstanceHandle {
+    pub fn new(bytes: [u8; 16]) -> Self {
+        InstanceHandle(bytes)
+    }
+}
+
+impl AsRef<[u8; 16]> for InstanceHandle {
+    fn as_ref(&self) -> &[u8; 16] {
+        &self.0
+    }
+}
 
 impl Default for InstanceHandle {
     fn default() -> Self {
