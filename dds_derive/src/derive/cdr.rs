be1d37838ec5a81c84d620922f5cb8f1bf4d5125
Joao Rebelo <jrebelo@s2e-systems.com>
2024-02-22 20:01:39 +0100

    Add deserialize for enum

--- dds_derive/src/derive/cdr.rs
+++ dds_derive/src/derive/cdr.rs
@@ -75,7 +75,7 @@ pub fn expand_cdr_serialize(input: &DeriveInput) -> Result<TokenStream> {
             })
         }
         syn::Data::Enum(data_enum) => {
-            // Note: Mapping has to be done with a match self strategy because the enum might not be copy so casting it using self as i64 would
+            // Note: Mapping has to be done with a match self strategy because the enum might not be copy so casting it using e.g. "self as i64" would
             // be consuming it.
             let discriminant_mapping = read_enum_variant_discriminant_mapping(data_enum);
             let serialize_enum = if discriminant_mapping.is_empty() {
@@ -84,7 +84,6 @@ pub fn expand_cdr_serialize(input: &DeriveInput) -> Result<TokenStream> {
             } else {
                 let max_discriminant = discriminant_mapping.iter().map(|(_,v)|v).max().expect("Map contains at least a value");
                 let discriminant_type = get_discriminant_type(max_discriminant);
-
 
                 let clauses: Vec<_> = discriminant_mapping.iter().map(|(v,d)| {
                     let i = Index::from(*d);
@@ -116,10 +115,13 @@ pub fn expand_cdr_serialize(input: &DeriveInput) -> Result<TokenStream> {
 }
 
 pub fn expand_cdr_deserialize(input: &DeriveInput) -> Result<TokenStream> {
+    let ident = &input.ident;
+    let (impl_generics, type_generics, where_clause) = input.generics.split_for_impl();
+
     match &input.data {
         syn::Data::Struct(data_struct) => {
             let mut struct_deserialization = quote!();
-            let (_, type_generics, where_clause) = input.generics.split_for_impl();
+
 
             // Create a '__de lifetime bound to all the lifetimes of the struct
             let mut de_lifetime_param =
@@ -135,7 +137,6 @@ pub fn expand_cdr_deserialize(input: &DeriveInput) -> Result<TokenStream> {
                 .chain(generics.params)
                 .collect();
 
-            let ident = &input.ident;
 
             match data_struct.fields.is_empty() {
                 true => struct_deserialization.extend(quote! {Self}),
@@ -173,10 +174,43 @@ pub fn expand_cdr_deserialize(input: &DeriveInput) -> Result<TokenStream> {
                     }
             })
         }
-        syn::Data::Enum(data_enum) => Err(syn::Error::new(
-            data_enum.enum_token.span,
-            "Enum not supported",
-        )),
+        syn::Data::Enum(data_enum) => {
+            let discriminant_mapping = read_enum_variant_discriminant_mapping(data_enum);
+
+            let deserialize_enum = if discriminant_mapping.is_empty() {
+                // Empty enum is the same as empty type. Do nothing
+                quote! {}
+            } else {
+                let max_discriminant = discriminant_mapping.iter().map(|(_,v)|v).max().expect("Map contains at least a value");
+                let discriminant_type = get_discriminant_type(max_discriminant);
+
+                let clauses: Vec<_> = discriminant_mapping.iter().map(|(v,d)| {
+                    let i = Index::from(*d);
+                    quote! {#i => Ok(#ident::#v),}
+                }).collect();
+                let error_msg = format!("Invalid value {{}} for discriminant of {}",ident);
+
+                quote! {
+                    let discriminant : #discriminant_type = dust_dds::serialized_payload::cdr::deserialize::CdrDeserialize::deserialize(deserializer)?;
+
+                    match discriminant {
+                        #(#clauses)*
+                        _ =>  Err(std::io::Error::new(
+                            std::io::ErrorKind::InvalidData,
+                            format!(#error_msg, discriminant),
+                        ))
+                    }
+                }
+            };
+
+            Ok(quote! {
+                impl #impl_generics dust_dds::serialized_payload::cdr::deserialize::CdrDeserialize<'_> for #ident #type_generics #where_clause {
+                    fn deserialize(deserializer: &mut impl dust_dds::serialized_payload::cdr::deserializer::CdrDeserializer<'_>) -> Result<Self, std::io::Error> {
+                        #deserialize_enum
+                    }
+                }
+            })
+        },
         syn::Data::Union(data_union) => Err(syn::Error::new(
             data_union.union_token.span,
             "Union not supported",
@@ -192,7 +226,7 @@ mod tests {
     use super::*;
 
     #[test]
-    fn simple_enum() {
+    fn cdr_serialize_enum() {
         let input = syn::parse2::<DeriveInput>(
             "
             enum SimpleEnum {
@@ -237,7 +271,56 @@ mod tests {
     }
 
     #[test]
-    fn struct_with_basic_types() {
+    fn cdr_deserialize_enum() {
+        let input = syn::parse2::<DeriveInput>(
+            "
+            enum SimpleEnum {
+                a=10,
+                b=2000,
+                c,
+            }
+        "
+            .parse()
+            .unwrap(),
+        )
+        .unwrap();
+
+        let output_token_stream = expand_cdr_deserialize(&input).unwrap();
+        let result = syn::parse2::<ItemImpl>(output_token_stream).unwrap();
+        let expected = syn::parse2::<ItemImpl>(
+            "
+            impl dust_dds::serialized_payload::cdr::deserialize::CdrDeserialize<'_> for SimpleEnum {
+                fn deserialize(deserializer: &mut impl dust_dds::serialized_payload::cdr::deserializer::CdrDeserializer<'_>) -> Result<Self, std::io::Error> {
+                    let discriminant: u16 = dust_dds::serialized_payload::cdr::deserialize::CdrDeserialize::deserialize(deserializer)?;
+
+                    match discriminant {
+                        10 => Ok(SimpleEnum::a),
+                        2000 => Ok(SimpleEnum::b),
+                        2001 => Ok(SimpleEnum::c),
+                        _ => Err(std::io::Error::new(
+                            std::io::ErrorKind::InvalidData,
+                            format!(\"Invalid value {} for discriminant of SimpleEnum\", discriminant),
+                        ))
+                    }
+                }
+            }
+            "
+            .parse()
+            .unwrap(),
+        )
+        .unwrap();
+
+        assert_eq!(
+            result,
+            expected,
+            "\n R: {:?} \n \n L: {:?} \n ",
+            result.clone().into_token_stream().to_string(),
+            expected.clone().into_token_stream().to_string()
+        );
+    }
+
+    #[test]
+    fn cdr_deserialize_struct_with_basic_types() {
         let input = syn::parse2::<DeriveInput>(
             "
             struct MyData {
@@ -278,7 +361,7 @@ mod tests {
     }
 
     #[test]
-    fn struct_with_lifetime() {
+    fn cdr_deserialize_struct_with_lifetime() {
         let input = syn::parse2::<DeriveInput>(
             "
             struct BorrowedData<'a> {
