f50897cd93950d87f6ae741c4ce2ece9bf5b0b3b
Joao Rebelo <jrebelo@s2e-systems.com>
2024-03-15 11:30:41 +0100

    Add rtpsreaderkind

--- dds/src/implementation/rtps/reader.rs
+++ dds/src/implementation/rtps/reader.rs
@@ -1,6 +1,7 @@
 use super::{
     endpoint::RtpsEndpoint,
     types::{Guid, Locator},
+    writer_proxy::RtpsWriterProxy,
 };
 use crate::infrastructure::time::Duration;
 
@@ -36,5 +37,57 @@ impl RtpsReader {
 
     pub fn multicast_locator_list(&self) -> &[Locator] {
         self.endpoint.multicast_locator_list()
+    }
+}
+
+pub struct RtpsStatelessReader {
+    pub rtps_reader: RtpsReader,
+}
+
+impl RtpsStatelessReader {
+    pub fn new(rtps_reader: RtpsReader) -> Self {
+        Self { rtps_reader }
+    }
+}
+
+pub struct RtpsStatefulReader {
+    pub rtps_reader: RtpsReader,
+    pub matched_writers: Vec<RtpsWriterProxy>,
+}
+
+impl RtpsStatefulReader {
+    pub fn new(rtps_reader: RtpsReader) -> Self {
+        Self {
+            rtps_reader,
+            matched_writers: Vec::new(),
+        }
+    }
+}
+
+pub enum RtpsReaderKind {
+    Stateful(RtpsStatefulReader),
+    Stateless(RtpsStatelessReader),
+}
+
+impl RtpsReaderKind {
+    pub fn guid(&self) -> Guid {
+        match self {
+            RtpsReaderKind::Stateful(r) => r.rtps_reader.guid(),
+            RtpsReaderKind::Stateless(r) => r.rtps_reader.guid(),
+        }
+    }
+
+    pub fn unicast_locator_list(&self) -> &[Locator] {
+        match self {
+            RtpsReaderKind::Stateful(r) => r.rtps_reader.unicast_locator_list(),
+            RtpsReaderKind::Stateless(r) => r.rtps_reader.unicast_locator_list(),
+        }
+    }
+
+    pub fn multicast_locator_list(&self) -> &[Locator] {
+        match self {
+            RtpsReaderKind::Stateful(r) => r.rtps_reader.multicast_locator_list(),
+            RtpsReaderKind::Stateless(r) => r.rtps_reader.multicast_locator_list(),
+        }
     }
 }
