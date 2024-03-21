19d348af1f29b186dfeae8aef27c929bb2fcb95e
Joao Rebelo <jrebelo@s2e-systems.com>
2024-03-21 14:48:43 +0100

    Add test for dispose

---
+++ interoperability_tests/dust_dds_subscriber_dispose.rs
@@ -0,0 +1,88 @@
+use dust_dds::{
+    domain::domain_participant_factory::DomainParticipantFactory,
+    infrastructure::{
+        listeners::NoOpListener,
+        qos::{DataReaderQos, QosKind},
+        qos_policy::{
+            DurabilityQosPolicy, DurabilityQosPolicyKind, ReliabilityQosPolicy,
+            ReliabilityQosPolicyKind,
+        },
+        status::{StatusKind, NO_STATUS},
+        time::{Duration, DurationKind},
+        wait_set::{Condition, WaitSet},
+    },
+    subscription::sample_info::{ANY_INSTANCE_STATE, ANY_SAMPLE_STATE, ANY_VIEW_STATE},
+};
+
+mod hello_world {
+    include!("target/idl/hello_world.rs");
+}
+
+fn main() {
+    let domain_id = 0;
+    let participant_factory = DomainParticipantFactory::get_instance();
+
+    let participant = participant_factory
+        .create_participant(domain_id, QosKind::Default, NoOpListener::new(), NO_STATUS)
+        .unwrap();
+
+    let topic = participant
+        .find_topic::<hello_world::HelloWorldType>("HelloWorld", Duration::new(120, 0))
+        .unwrap();
+
+    let subscriber = participant
+        .create_subscriber(QosKind::Default, NoOpListener::new(), NO_STATUS)
+        .unwrap();
+
+    let reader_qos = DataReaderQos {
+        reliability: ReliabilityQosPolicy {
+            kind: ReliabilityQosPolicyKind::Reliable,
+            max_blocking_time: DurationKind::Finite(Duration::new(1, 0)),
+        },
+        durability: DurabilityQosPolicy {
+            kind: DurabilityQosPolicyKind::TransientLocal,
+        },
+        ..Default::default()
+    };
+    let reader = subscriber
+        .create_datareader::<hello_world::HelloWorldType>(
+            &topic,
+            QosKind::Specific(reader_qos),
+            NoOpListener::new(),
+            NO_STATUS,
+        )
+        .unwrap();
+
+    let reader_cond = reader.get_statuscondition();
+    reader_cond
+        .set_enabled_statuses(&[StatusKind::SubscriptionMatched])
+        .unwrap();
+    let mut wait_set = WaitSet::new();
+    wait_set
+        .attach_condition(Condition::StatusCondition(reader_cond.clone()))
+        .unwrap();
+
+    wait_set.wait(Duration::new(60, 0)).unwrap();
+
+    reader_cond
+        .set_enabled_statuses(&[StatusKind::DataAvailable])
+        .unwrap();
+    wait_set.wait(Duration::new(30, 0)).unwrap();
+
+    std::thread::sleep(std::time::Duration::from_secs(2));
+
+    let samples = reader
+        .read(2, ANY_SAMPLE_STATE, ANY_VIEW_STATE, ANY_INSTANCE_STATE)
+        .unwrap();
+
+    let hello_world = samples[0].data().unwrap();
+    println!("Received: {:?}", hello_world);
+
+    assert_eq!(
+        samples[0].sample_info().instance_state,
+        InstanceStateKind::NotAliveDisposed,
+    );
+
+    // Sleep to allow sending acknowledgements
+    std::thread::sleep(std::time::Duration::from_secs(2));
+}
