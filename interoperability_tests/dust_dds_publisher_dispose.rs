2621e70ac964d268befc109bffbe3fa797990eef
Joao Rebelo <jrebelo@s2e-systems.com>
2024-03-22 10:33:54 +0100

    Add test for sending dispose

---
+++ interoperability_tests/dust_dds_publisher_dispose.rs
@@ -0,0 +1,81 @@
+use dust_dds::{
+    domain::domain_participant_factory::DomainParticipantFactory,
+    infrastructure::{
+        listeners::NoOpListener,
+        qos::{DataWriterQos, QosKind},
+        qos_policy::{
+            DurabilityQosPolicy, DurabilityQosPolicyKind, ReliabilityQosPolicy,
+            ReliabilityQosPolicyKind,
+        },
+        status::{StatusKind, NO_STATUS},
+        time::{Duration, DurationKind},
+        wait_set::{Condition, WaitSet},
+    },
+};
+
+mod dispose_data {
+    include!("target/idl/dispose_data.rs");
+}
+
+fn main() {
+    let domain_id = 0;
+    let participant_factory = DomainParticipantFactory::get_instance();
+
+    let participant = participant_factory
+        .create_participant(domain_id, QosKind::Default, NoOpListener::new(), NO_STATUS)
+        .unwrap();
+
+    let topic = participant
+        .create_topic::<dispose_data::DisposeDataType>(
+            "DisposeData",
+            "DisposeDataType",
+            QosKind::Default,
+            NoOpListener::new(),
+            NO_STATUS,
+        )
+        .unwrap();
+
+    let publisher = participant
+        .create_publisher(QosKind::Default, NoOpListener::new(), NO_STATUS)
+        .unwrap();
+
+    let writer_qos = DataWriterQos {
+        reliability: ReliabilityQosPolicy {
+            kind: ReliabilityQosPolicyKind::Reliable,
+            max_blocking_time: DurationKind::Finite(Duration::new(1, 0)),
+        },
+        durability: DurabilityQosPolicy {
+            kind: DurabilityQosPolicyKind::TransientLocal,
+        },
+        ..Default::default()
+    };
+    let writer = publisher
+        .create_datawriter(
+            &topic,
+            QosKind::Specific(writer_qos),
+            NoOpListener::new(),
+            NO_STATUS,
+        )
+        .unwrap();
+    let writer_cond = writer.get_statuscondition();
+    writer_cond
+        .set_enabled_statuses(&[StatusKind::PublicationMatched])
+        .unwrap();
+    let mut wait_set = WaitSet::new();
+    wait_set
+        .attach_condition(Condition::StatusCondition(writer_cond))
+        .unwrap();
+
+    wait_set.wait(Duration::new(60, 0)).unwrap();
+
+    let dispose_msg = dispose_data::DisposeDataType {
+        name: "Very Long Name".to_string(),
+        value: 1,
+    };
+    writer.write(&dispose_msg, None).unwrap();
+    writer.dispose(&dispose_msg, None).unwrap();
+
+    writer
+        .wait_for_acknowledgments(Duration::new(30, 0))
+        .unwrap();
+}
