c859cfc2f4edc745d28766e9ac739802214618a8
Joao Rebelo <jrebelo@s2e-systems.com>
2024-04-12 17:34:45 +0200

    Fix clippy

--- dds/src/implementation/actors/domain_participant_factory_actor.rs
+++ dds/src/implementation/actors/domain_participant_factory_actor.rs
@@ -507,11 +507,8 @@ impl DomainParticipantFactoryActor {
 
         let participant_address_clone = participant_address.clone();
 
-        let mut interval = tokio::time::interval(
-            self.configuration
-                .participant_announcement_interval()
-                .into(),
-        );
+        let mut interval =
+            tokio::time::interval(self.configuration.participant_announcement_interval());
         runtime_handle.spawn(async move {
             loop {
                 if let Ok(p) = participant_address_clone.upgrade() {
