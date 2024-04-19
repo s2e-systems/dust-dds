d403fd5f0de3533ee33c00e5f89ede9925dccb3f
Stefan Kimmer <skimmer@s2e-systems.com>
2024-04-19 16:36:02 +0200

    undo comment out announce interval

--- dds/src/implementation/actors/domain_participant_factory_actor.rs
+++ dds/src/implementation/actors/domain_participant_factory_actor.rs
@@ -487,19 +487,19 @@ impl DomainParticipantFactoryActor {
 
         let mut interval =
             tokio::time::interval(self.configuration.participant_announcement_interval());
-        // runtime_handle.spawn(async move {
-        //     loop {
-        //         interval.tick().await;
-        //         if let Ok(p) = participant_address_clone.upgrade() {
-        //             let r = p.announce_participant().await;
-        //             if r.is_err() {
-        //                 error!("Error announcing participant: {:?}", r);
-        //             }
-        //         } else {
-        //             break;
-        //         }
-        //     }
-        // });
+        runtime_handle.spawn(async move {
+            loop {
+                interval.tick().await;
+                if let Ok(p) = participant_address_clone.upgrade() {
+                    let r = p.announce_participant().await;
+                    if r.is_err() {
+                        error!("Error announcing participant: {:?}", r);
+                    }
+                } else {
+                    break;
+                }
+            }
+        });
 
         Ok(participant_address)
     }
