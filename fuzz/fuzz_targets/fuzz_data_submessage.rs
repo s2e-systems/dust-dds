0e7fa1d36a8f85bcadd496277a3c2f8f15fe3324
Joao Rebelo <jrebelo@s2e-systems.com>
2024-05-28 19:48:08 +0200

    Split fuzz targets

--- fuzz/fuzz_targets/fuzz_data_submessage.rs
+++ fuzz/fuzz_targets/fuzz_data_submessage.rs
@@ -1,11 +1,7 @@
 #![no_main]
 
 use dust_dds::rtps::messages::{
-    overall_structure::SubmessageHeaderRead,
-    submessages::{
-        data::DataSubmessage, gap::GapSubmessage, heartbeat::HeartbeatSubmessage,
-        heartbeat_frag::HeartbeatFragSubmessage,
-    },
+    overall_structure::SubmessageHeaderRead, submessages::data::DataSubmessage,
 };
 use libfuzzer_sys::{fuzz_target, Corpus};
 
@@ -13,8 +9,6 @@ fuzz_target!(|data: &[u8]| -> Corpus {
     if data.len() > 4 {
         let mut data = data;
         let header = SubmessageHeaderRead::try_read_from_bytes(&mut data).unwrap();
-        // HeartbeatSubmessage::try_from_bytes(&header, data).ok();
-        // GapSubmessage::try_from_bytes(&header, data).ok();
         DataSubmessage::try_from_arc_slice(&header, data.into()).ok();
         Corpus::Keep
     } else {
