2721be3afe88746c0ac9995ae2f753110d0f098e
Joao Rebelo <jrebelo@s2e-systems.com>
2024-06-07 13:54:56 +0200

    Create dust dds interoperability test binaries for nested types

--- interoperability_tests/build.rs
+++ interoperability_tests/build.rs
@@ -31,4 +31,12 @@ fn main() {
     let mut file = File::create(compiled_idl_path).expect("Failed to create file");
     file.write_all(compiled_idl.as_bytes())
         .expect("Failed to write to file");
+
+    let idl_path = Path::new("NestedType.idl");
+    let idl_src = std::fs::read_to_string(idl_path).expect("Couldn't read IDL source file!");
+    let compiled_idl = dust_dds_gen::compile_idl(&idl_src).expect("Couldn't parse IDL file");
+    let compiled_idl_path = build_path.join("nested_type.rs");
+    let mut file = File::create(compiled_idl_path).expect("Failed to create file");
+    file.write_all(compiled_idl.as_bytes())
+        .expect("Failed to write to file");
 }
