971a8a48dd02bc555662e68464befe8c25ae82d4
Joao Rebelo <jrebelo@s2e-systems.com>
2024-06-20 16:24:10 +0200

    Add executor

--- dds/src/implementation/runtime/executor.rs
+++ dds/src/implementation/runtime/executor.rs
@@ -1,9 +1,12 @@
 use std::{
     future::Future,
-    pin::pin,
-    sync::Arc,
+    pin::{pin, Pin},
+    sync::{
+        mpsc::{channel, Sender},
+        Arc, Mutex,
+    },
     task::{Context, Poll, Wake, Waker},
-    thread::{self, Thread},
+    thread::{self, JoinHandle, Thread},
 };
 
 pub fn block_on<T>(f: impl Future<Output = T>) -> T {
@@ -20,6 +23,70 @@ pub fn block_on<T>(f: impl Future<Output = T>) -> T {
         match pinned_fut.as_mut().poll(&mut cx) {
             Poll::Ready(t) => return t,
             Poll::Pending => thread::park(),
+        }
+    }
+}
+
+pub struct Task {
+    future: Mutex<Pin<Box<dyn Future<Output = ()> + Send>>>,
+    task_sender: Sender<Arc<Task>>,
+}
+
+impl Wake for Task {
+    fn wake(self: Arc<Self>) {
+        self.task_sender.send(self.clone()).unwrap();
+    }
+}
+
+pub struct ExecutorHandle {
+    task_sender: Sender<Arc<Task>>,
+    thread_handle: Thread,
+}
+
+impl ExecutorHandle {
+    pub fn spawn(&self, f: impl Future<Output = ()> + Send + 'static) {
+        let future = Box::pin(f);
+        self.task_sender
+            .send(Arc::new(Task {
+                future: Mutex::new(future),
+                task_sender: self.task_sender.clone(),
+            }))
+            .unwrap();
+        self.thread_handle.unpark();
+    }
+}
+
+pub struct Executor {
+    task_sender: Sender<Arc<Task>>,
+    executor_thread_handle: JoinHandle<()>,
+}
+
+impl Executor {
+    pub fn new() -> Self {
+        let (task_sender, task_receiver) = channel::<Arc<Task>>();
+        let executor_thread_handle = std::thread::spawn(move || {
+            while let Ok(task) = task_receiver.recv() {
+                let waker = Waker::from(task.clone());
+                let mut cx = Context::from_waker(&waker);
+                let _ = task
+                    .future
+                    .try_lock()
+                    .expect("Only ever locked here")
+                    .as_mut()
+                    .poll(&mut cx);
+            }
+        });
+
+        Self {
+            task_sender,
+            executor_thread_handle,
+        }
+    }
+
+    pub fn handle(&self) -> ExecutorHandle {
+        ExecutorHandle {
+            task_sender: self.task_sender.clone(),
+            thread_handle: self.executor_thread_handle.thread().clone(),
         }
     }
 }
