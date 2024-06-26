d5c41feaed37b46d8c2b86ab8e9a79ddb4615d39
Joao Rebelo <jrebelo@s2e-systems.com>
2024-06-26 09:47:40 +0200

    Add domain participant listener

--- bindings/python/src/domain/domain_participant_listener.rs
+++ bindings/python/src/domain/domain_participant_listener.rs
@@ -1,36 +1,27 @@
 use pyo3::prelude::*;
 
-use crate::infrastructure::status::{
-    LivelinessChangedStatus, RequestedDeadlineMissedStatus, RequestedIncompatibleQosStatus,
-    SampleLostStatus, SampleRejectedStatus, SubscriptionMatchedStatus,
+use crate::{
+    infrastructure::status::{
+        InconsistentTopicStatus, LivelinessChangedStatus, LivelinessLostStatus,
+        OfferedDeadlineMissedStatus, OfferedIncompatibleQosStatus, PublicationMatchedStatus,
+        RequestedDeadlineMissedStatus, RequestedIncompatibleQosStatus, SampleLostStatus,
+        SampleRejectedStatus, SubscriptionMatchedStatus,
+    },
+    topic_definition::topic::Topic,
 };
-
-use super::subscriber::Subscriber;
 
 #[pyclass]
 #[derive(Clone)]
-pub struct SubscriberListener(Py<PyAny>);
-
-impl From<Py<PyAny>> for SubscriberListener {
+pub struct DomainParticipantListener(Py<PyAny>);
+impl From<Py<PyAny>> for DomainParticipantListener {
     fn from(value: Py<PyAny>) -> Self {
         Self(value)
     }
 }
 
-impl dust_dds::subscription::subscriber_listener::SubscriberListener for SubscriberListener {
-    fn on_data_on_readers(
-        &mut self,
-        the_subscriber: dust_dds::subscription::subscriber::Subscriber,
-    ) {
-        let args = (Subscriber::from(the_subscriber),);
-        Python::with_gil(|py| {
-            self.0
-                .bind(py)
-                .call_method("on_data_on_readers", args, None)
-                .unwrap();
-        })
-    }
-
+impl dust_dds::domain::domain_participant_listener::DomainParticipantListener
+    for DomainParticipantListener
+{
     fn on_data_available(
         &mut self,
         _the_reader: &dyn dust_dds::subscription::data_reader::AnyDataReader,
@@ -124,6 +115,79 @@ impl dust_dds::subscription::subscriber_listener::SubscriberListener for Subscri
             self.0
                 .bind(py)
                 .call_method("on_sample_lost", args, None)
+                .unwrap();
+        })
+    }
+
+    fn on_inconsistent_topic(
+        &mut self,
+        the_topic: dust_dds::topic_definition::topic::Topic,
+        status: dust_dds::infrastructure::status::InconsistentTopicStatus,
+    ) {
+        let args = (
+            Topic::from(the_topic),
+            InconsistentTopicStatus::from(status),
+        );
+        Python::with_gil(|py| {
+            self.0
+                .bind(py)
+                .call_method("on_inconsistent_topic", args, None)
+                .unwrap();
+        })
+    }
+
+    fn on_liveliness_lost(
+        &mut self,
+        _the_writer: &dyn dust_dds::publication::data_writer::AnyDataWriter,
+        status: dust_dds::infrastructure::status::LivelinessLostStatus,
+    ) {
+        let args = ((), LivelinessLostStatus::from(status));
+        Python::with_gil(|py| {
+            self.0
+                .bind(py)
+                .call_method("on_liveliness_lost", args, None)
+                .unwrap();
+        })
+    }
+
+    fn on_offered_deadline_missed(
+        &mut self,
+        _the_writer: &dyn dust_dds::publication::data_writer::AnyDataWriter,
+        status: dust_dds::infrastructure::status::OfferedDeadlineMissedStatus,
+    ) {
+        let args = ((), OfferedDeadlineMissedStatus::from(status));
+        Python::with_gil(|py| {
+            self.0
+                .bind(py)
+                .call_method("on_offered_deadline_missed", args, None)
+                .unwrap();
+        })
+    }
+
+    fn on_offered_incompatible_qos(
+        &mut self,
+        _the_writer: &dyn dust_dds::publication::data_writer::AnyDataWriter,
+        status: dust_dds::infrastructure::status::OfferedIncompatibleQosStatus,
+    ) {
+        let args = ((), OfferedIncompatibleQosStatus::from(status));
+        Python::with_gil(|py| {
+            self.0
+                .bind(py)
+                .call_method("on_offered_incompatible_qos", args, None)
+                .unwrap();
+        })
+    }
+
+    fn on_publication_matched(
+        &mut self,
+        _the_writer: &dyn dust_dds::publication::data_writer::AnyDataWriter,
+        status: dust_dds::infrastructure::status::PublicationMatchedStatus,
+    ) {
+        let args = ((), PublicationMatchedStatus::from(status));
+        Python::with_gil(|py| {
+            self.0
+                .bind(py)
+                .call_method("on_publication_matched", args, None)
                 .unwrap();
         })
     }
