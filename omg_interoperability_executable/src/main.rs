7b0ec40a6e41f63c14b6fb0ff290b42480566487
Stefan Kimmer <skimmer@s2e-systems.com>
2024-08-13 10:01:43 +0200

    remove tracing

--- omg_interoperability_executable/src/main.rs
+++ omg_interoperability_executable/src/main.rs
@@ -717,16 +717,16 @@ impl From<RunningError> for Return {
 }
 
 fn main() -> Result<(), Return> {
-    let subscriber = tracing_subscriber::FmtSubscriber::builder()
-        // all spans/events with a level higher than TRACE (e.g, debug, info, warn, etc.)
-        // will be written to stdout.
-        .with_max_level(tracing::Level::DEBUG)
-        .with_span_events(tracing_subscriber::fmt::format::FmtSpan::ACTIVE)
-        .with_writer(std::io::stderr)
-        // completes the builder.
-        .finish();
+    // let subscriber = tracing_subscriber::FmtSubscriber::builder()
+    //     // all spans/events with a level higher than TRACE (e.g, debug, info, warn, etc.)
+    //     // will be written to stdout.
+    //     .with_max_level(tracing::Level::DEBUG)
+    //     .with_span_events(tracing_subscriber::fmt::format::FmtSpan::ACTIVE)
+    //     .with_writer(std::io::stderr)
+    //     // completes the builder.
+    //     .finish();
 
-    tracing::subscriber::set_global_default(subscriber).expect("setting default subscriber failed");
+    // tracing::subscriber::set_global_default(subscriber).expect("setting default subscriber failed");
 
     let (tx, rx) = std::sync::mpsc::channel();
 
