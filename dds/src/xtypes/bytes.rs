194039afd9ffd40ce01ea73d3630c58069deabe6
Stefan Kimmer <skimmer@s2e-systems.com>
2024-09-06 12:20:23 +0200

    optimaze frag bench

--- dds/src/xtypes/bytes.rs
+++ dds/src/xtypes/bytes.rs
@@ -1,6 +1,6 @@
-#[derive(Debug, PartialEq, Eq)]
+#[derive(Debug, PartialEq, Eq, Clone, Copy)]
 pub struct Bytes<'a>(pub &'a [u8]);
 
 #[cfg(feature = "std")]
-#[derive(Debug, PartialEq, Eq)]
+#[derive(Debug, PartialEq, Eq, Clone)]
 pub struct ByteBuf(pub Vec<u8>);
