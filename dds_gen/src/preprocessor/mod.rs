84cf97098b2c43ae9b0ec896309f514b848cadd0
Joao Rebelo <jrebelo@s2e-systems.com>
2025-02-21 12:17:08 +0100

    Move include file to own function

--- dds_gen/src/preprocessor/mod.rs
+++ dds_gen/src/preprocessor/mod.rs
@@ -4,18 +4,22 @@ use std::{
     path::Path,
 };
 
-pub struct Preprocessor;
+pub struct Preprocessor {
+    output: String,
+}
 
 impl Preprocessor {
     pub fn parse(idl_filepath: &Path) -> io::Result<String> {
-        let mut output = String::new();
+        let mut preprocessor = Preprocessor {
+            output: String::new(),
+        };
 
-        Self::parse_file(idl_filepath, &mut output)?;
+        preprocessor.parse_file(idl_filepath)?;
 
-        Ok(output)
+        Ok(preprocessor.output)
     }
 
-    fn parse_file(idl_filepath: &Path, output: &mut String) -> io::Result<()> {
+    fn parse_file(&mut self, idl_filepath: &Path) -> io::Result<()> {
         let idl_file = File::open(idl_filepath)?;
         let mut idl_file_reader = BufReader::new(idl_file);
 
@@ -32,42 +36,44 @@ impl Preprocessor {
                 if first_token == "#include" {
                     let include_file_token = token_iter.next().ok_or(io::Error::new(
                         io::ErrorKind::InvalidData,
-                        "Missing file argument for preprocessor #include",
+                        "Missing file argument for preprocessor directive #include",
                     ))?;
-                    if include_file_token.len() > 2
-                        && ((include_file_token.starts_with('"')
-                            && include_file_token.ends_with('"'))
-                            || (include_file_token.starts_with('<')
-                                && include_file_token.ends_with('>')))
-                    {
-                        let include_filepath = idl_filepath
-                            .parent()
-                            .ok_or(io::Error::new(
-                                io::ErrorKind::Other,
-                                "Failed to get parent path of IDL file",
-                            ))?
-                            .join(&include_file_token[1..include_file_token.len() - 1]);
-                        Self::parse_file(&include_filepath, output)?;
-                        continue;
-                    } else {
-                        return Err(io::Error::new(
-                            io::ErrorKind::InvalidData,
-                            "Invalid #include filename enclosing characters. Expected \"\" or <>",
-                        ));
-                    }
+                    self.include_file(idl_filepath, include_file_token)?;
+                    continue;
                 } else if first_token.starts_with('#') {
                     return Err(std::io::Error::new(
                         io::ErrorKind::InvalidData,
-                        format!("Unknown preprocessor token {first_token}"),
+                        format!("Unknown preprocessor directive {first_token}"),
                     ));
                 }
             }
 
-            output.push_str(&line_buffer);
-            output.push('\n');
+            self.output.push_str(&line_buffer);
+            self.output.push('\n');
         }
 
         Ok(())
+    }
+
+    fn include_file(&mut self, idl_filepath: &Path, include_file_token: &str) -> io::Result<()> {
+        if include_file_token.len() > 2
+            && ((include_file_token.starts_with('"') && include_file_token.ends_with('"'))
+                || (include_file_token.starts_with('<') && include_file_token.ends_with('>')))
+        {
+            let include_filepath = idl_filepath
+                .parent()
+                .ok_or(io::Error::new(
+                    io::ErrorKind::Other,
+                    "Failed to get parent path of IDL file",
+                ))?
+                .join(&include_file_token[1..include_file_token.len() - 1]);
+            self.parse_file(&include_filepath)
+        } else {
+            return Err(io::Error::new(
+                io::ErrorKind::InvalidData,
+                "Invalid #include filename enclosing characters. Expected \"\" or <>",
+            ));
+        }
     }
 }
 
