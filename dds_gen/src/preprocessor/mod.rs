41f97ad1a016f927e762474eebab39a618c6a5fe
Joao Rebelo <2730147+jrebelo@users.noreply.github.com>
2025-02-24 12:15:57 +0000

    Add Idlgen ifdef and ifndef support (#362)
    * Add ifdef with define case
    
    * Add test for case when the definition doesn't exist
    
    * Use pest grammar for preprocessor
    
    * Add ifndef implementation
    
    * Fix preprocessor file path

--- dds_gen/src/preprocessor/mod.rs
+++ dds_gen/src/preprocessor/mod.rs
@@ -1,105 +1,140 @@
 use std::{
     collections::HashMap,
     fs::File,
-    io::{self, BufRead, BufReader},
+    io::{self, Read},
     path::Path,
 };
 
-pub struct Preprocessor {
+use pest::Parser;
+use pest_derive::Parser;
+
+#[derive(Parser)]
+#[grammar = "preprocessor/preprocessor_grammar.pest"]
+pub struct IdlPreprocessorParser;
+
+pub struct Preprocessor<'a> {
+    idl_file_path: &'a Path,
     output: String,
     define_list: HashMap<String, String>,
 }
 
-impl Preprocessor {
-    pub fn new() -> Self {
-        Self {
+impl<'a> Preprocessor<'a> {
+    pub fn parse(idl_filepath: &'a Path) -> io::Result<String> {
+        let mut preprocessor = Preprocessor {
+            idl_file_path: idl_filepath,
             output: String::new(),
             define_list: HashMap::new(),
-        }
-    }
+        };
 
-    pub fn parse(mut self, idl_filepath: &Path) -> io::Result<String> {
-        self.parse_file(idl_filepath)?;
+        preprocessor.parse_file(idl_filepath)?;
 
-        Ok(self.output)
+        Ok(preprocessor.output)
     }
 
     fn parse_file(&mut self, idl_filepath: &Path) -> io::Result<()> {
-        let idl_file = File::open(idl_filepath)?;
-        let mut idl_file_reader = BufReader::new(idl_file);
+        let mut idl_file = File::open(idl_filepath)?;
+        let mut idl_file_contents = String::new();
+        idl_file.read_to_string(&mut idl_file_contents)?;
+        // Make sure file always ends in a newline for correct parsing
+        idl_file_contents.push('\n');
 
-        let mut line_buffer = String::new();
-        loop {
-            line_buffer.clear();
-            let line_read = idl_file_reader.read_line(&mut line_buffer)?;
-            if line_read == 0 {
-                break;
-            }
+        let mut parsed_idl = IdlPreprocessorParser::parse(Rule::file, &idl_file_contents)
+            .map_err(|e| io::Error::new(io::ErrorKind::InvalidData, e.to_string()))?;
 
-            let mut token_iter = line_buffer.split_whitespace();
-            if let Some(first_token) = token_iter.next() {
-                if first_token == "#include" {
-                    let include_file_token = token_iter.next().ok_or(io::Error::new(
-                        io::ErrorKind::InvalidData,
-                        "Missing file argument for preprocessor directive #include",
-                    ))?;
-                    self.include_file(idl_filepath, include_file_token)?;
-                    continue;
-                } else if first_token == "#define" {
-                    let macro_name = token_iter.next().ok_or(io::Error::new(
-                        io::ErrorKind::InvalidData,
-                        "Missing macro name for preprocessor directive #define",
-                    ))?;
-                    let value = token_iter.next().unwrap_or_default();
-                    self.define_list
-                        .insert(macro_name.to_string(), value.to_string());
-                    continue;
-                } else if first_token == "#undef" {
-                    let macro_name = token_iter.next().ok_or(io::Error::new(
-                        io::ErrorKind::InvalidData,
-                        "Missing macro name for preprocessor directive #undef",
-                    ))?;
-                    self.define_list.remove(macro_name);
-                    continue;
-                } else if first_token.starts_with('#') {
-                    return Err(std::io::Error::new(
-                        io::ErrorKind::InvalidData,
-                        format!("Unknown preprocessor directive {first_token}"),
-                    ));
-                }
-            }
-
-            // Replace all occurences of the define macro by their values. The order of this
-            // iteration is not guaranteed
-            for (define_macro, define_value) in self.define_list.iter() {
-                line_buffer = line_buffer.replace(define_macro.as_str(), define_value);
-            }
-            self.output.push_str(&line_buffer);
-            self.output.push('\n');
+        if let Some(p) = parsed_idl.next() {
+            self.generate_preprocessed_idl(p)?;
         }
 
         Ok(())
     }
 
-    fn include_file(&mut self, idl_filepath: &Path, include_file_token: &str) -> io::Result<()> {
-        if include_file_token.len() > 2
-            && ((include_file_token.starts_with('"') && include_file_token.ends_with('"'))
-                || (include_file_token.starts_with('<') && include_file_token.ends_with('>')))
-        {
-            let include_filepath = idl_filepath
-                .parent()
-                .ok_or(io::Error::new(
-                    io::ErrorKind::Other,
-                    "Failed to get parent path of IDL file",
-                ))?
-                .join(&include_file_token[1..include_file_token.len() - 1]);
-            self.parse_file(&include_filepath)
-        } else {
-            Err(io::Error::new(
-                io::ErrorKind::InvalidData,
-                "Invalid #include filename enclosing characters. Expected \"\" or <>",
-            ))
+    fn generate_preprocessed_idl(
+        &mut self,
+        pair: pest::iterators::Pair<'_, Rule>,
+    ) -> io::Result<()> {
+        match pair.as_rule() {
+            Rule::file => {
+                for l in pair.into_inner() {
+                    self.generate_preprocessed_idl(l)?;
+                }
+            }
+            Rule::line => {
+                if let Some(p) = pair.into_inner().next() {
+                    self.generate_preprocessed_idl(p)?;
+                }
+            }
+            Rule::include_file => {
+                let include_filename = pair.as_str();
+                let include_filepath = self
+                    .idl_file_path
+                    .parent()
+                    .ok_or(io::Error::new(
+                        io::ErrorKind::Other,
+                        "Failed to get parent path of IDL file",
+                    ))?
+                    .join(&include_filename[1..include_filename.len() - 1]);
+
+                self.parse_file(&include_filepath)?;
+            }
+            Rule::define_directive => {
+                let mut define_pairs = pair.into_inner();
+                let define_identifier = define_pairs
+                    .next()
+                    .expect("#define identifier must exist according to grammar")
+                    .as_str()
+                    .to_string();
+                let define_value = define_pairs
+                    .next()
+                    .map(|r| r.as_str().to_string())
+                    .unwrap_or_default();
+                self.define_list.insert(define_identifier, define_value);
+            }
+            Rule::ifdef_directive => {
+                let mut ifdef_pairs = pair.into_inner();
+                let ifdef_identifier = ifdef_pairs
+                    .next()
+                    .expect("#ifdef identifier must exist according to grammar")
+                    .as_str();
+                if self.define_list.contains_key(ifdef_identifier) {
+                    for ifdef_line in ifdef_pairs {
+                        self.generate_preprocessed_idl(ifdef_line)?;
+                    }
+                }
+            }
+            Rule::ifndef_directive => {
+                let mut ifdef_pairs = pair.into_inner();
+                let ifdef_identifier = ifdef_pairs
+                    .next()
+                    .expect("#ifndef identifier must exist according to grammar")
+                    .as_str();
+                if !self.define_list.contains_key(ifdef_identifier) {
+                    for ifdef_line in ifdef_pairs {
+                        self.generate_preprocessed_idl(ifdef_line)?;
+                    }
+                }
+            }
+            Rule::other_line => {
+                let mut line = pair.as_str().to_string();
+                // Replace all occurences of the define macro by their values. The order of this
+                // iteration is not guaranteed
+                for (define_macro, define_value) in self.define_list.iter() {
+                    line = line.replace(define_macro.as_str(), define_value);
+                }
+                self.output.push_str(&line);
+                self.output.push('\n');
+            }
+            Rule::directive
+            | Rule::include_directive
+            | Rule::quoted_string
+            | Rule::angle_bracketed_string
+            | Rule::identifier
+            | Rule::value
+            | Rule::WHITESPACE
+            | Rule::NEWLINE
+            | Rule::EOI => (),
         }
+
+        Ok(())
     }
 }
 
@@ -110,9 +145,8 @@ mod tests {
     #[test]
     fn preprocessor_makes_no_changes() {
         let idl_file = Path::new("src/preprocessor/test_resources/simple_struct.idl");
-        let expected =
-            "struct SimpleStruct {\r\n\n    boolean a;\r\n\n    char b;\r\n\n    long i;\r\n\n};\n";
-        let output = Preprocessor::new().parse(idl_file).unwrap();
+        let expected = "struct SimpleStruct {\nboolean a;\nchar b;\nlong i;\n};\n";
+        let output = Preprocessor::parse(idl_file).unwrap();
 
         assert_eq!(output, expected);
     }
@@ -121,8 +155,8 @@ mod tests {
     fn preprocessor_file_with_include() {
         let idl_file = Path::new("src/preprocessor/test_resources/file_with_include.idl");
         let expected =
-            "struct SimpleStruct {\r\n\n    long i;\r\n\n};\nstruct SimpleStruct {\r\n\n    long i;\r\n\n};\n\r\n\nstruct OtherStruct {\r\n\n    long i;\r\n\n};\n";
-        let output = Preprocessor::new().parse(idl_file).unwrap();
+            "struct SimpleStruct {\nlong i;\n};\nstruct SimpleStruct {\nlong i;\n};\n\nstruct OtherStruct {\nlong i;\n};\n";
+        let output = Preprocessor::parse(idl_file).unwrap();
 
         assert_eq!(output, expected);
     }
@@ -130,9 +164,44 @@ mod tests {
     #[test]
     fn preprocessor_file_with_define() {
         let idl_file = Path::new("src/preprocessor/test_resources/file_with_define.idl");
-        let expected =
-            "\r\n\nstruct SimpleStruct {\r\n\n    boolean a;\r\n\n    char b;\r\n\n    long i;\r\n\n};\n";
-        let output = Preprocessor::new().parse(idl_file).unwrap();
+        let expected = "\nstruct SimpleStruct {\nboolean a;\nchar b;\nlong i;\n};\n";
+        let output = Preprocessor::parse(idl_file).unwrap();
+
+        assert_eq!(output, expected);
+    }
+
+    #[test]
+    fn preprocessor_file_with_ifdef() {
+        let idl_file = Path::new("src/preprocessor/test_resources/file_with_ifdef.idl");
+        let expected = "\nstruct SimpleStruct {\nboolean a;\nchar b;\nlong i;\n};\n\n";
+        let output = Preprocessor::parse(idl_file).unwrap();
+
+        assert_eq!(output, expected);
+    }
+
+    #[test]
+    fn preprocessor_file_with_ifdef_not_defined() {
+        let idl_file = Path::new("src/preprocessor/test_resources/file_with_ifdef_not_defined.idl");
+        let expected = "";
+        let output = Preprocessor::parse(idl_file).unwrap();
+
+        assert_eq!(output, expected);
+    }
+
+    #[test]
+    fn preprocessor_file_with_ifndef() {
+        let idl_file = Path::new("src/preprocessor/test_resources/file_with_ifndef.idl");
+        let expected = "struct SimpleStruct {\nboolean a;\nchar b;\nlong i;\n};\n\n";
+        let output = Preprocessor::parse(idl_file).unwrap();
+
+        assert_eq!(output, expected);
+    }
+
+    #[test]
+    fn preprocessor_file_with_ifndef_defined() {
+        let idl_file = Path::new("src/preprocessor/test_resources/file_with_ifndef_defined.idl");
+        let expected = "";
+        let output = Preprocessor::parse(idl_file).unwrap();
 
         assert_eq!(output, expected);
     }
