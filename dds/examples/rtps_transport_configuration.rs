22ab830db1a24a551e58e295866e344ea86aefed
Joao Rebelo <jrebelo@s2e-systems.com>
2025-02-26 11:18:56 +0100

    Add example for transport configuration

---
+++ dds/examples/rtps_transport_configuration.rs
@@ -0,0 +1,51 @@
+use dust_dds::{
+    domain::domain_participant_factory::DomainParticipantFactory,
+    infrastructure::{qos::QosKind, status::NO_STATUS},
+    rtps::transport::RtpsTransportFactory,
+    topic_definition::type_support::DdsType,
+};
+
+#[derive(DdsType, Debug)]
+struct HelloWorldType {
+    #[dust_dds(key)]
+    id: u8,
+    msg: String,
+}
+
+fn main() {
+    let domain_id = 0;
+    let participant_factory = DomainParticipantFactory::get_instance();
+    let fragment_size = 500;
+    let transport = Box::new(RtpsTransportFactory::new(fragment_size));
+
+    participant_factory.set_transport(transport).unwrap();
+
+    let participant = participant_factory
+        .create_participant(domain_id, QosKind::Default, None, NO_STATUS)
+        .unwrap();
+
+    let topic = participant
+        .create_topic::<HelloWorldType>(
+            "HelloWorld",
+            "HelloWorldType",
+            QosKind::Default,
+            None,
+            NO_STATUS,
+        )
+        .unwrap();
+
+    let publisher = participant
+        .create_publisher(QosKind::Default, None, NO_STATUS)
+        .unwrap();
+
+    let writer = publisher
+        .create_datawriter(&topic, QosKind::Default, None, NO_STATUS)
+        .unwrap();
+
+    let hello_world = HelloWorldType {
+        id: 8,
+        msg: "Hello world!".to_string(),
+    };
+
+    writer.write(&hello_world, None).unwrap();
+}
