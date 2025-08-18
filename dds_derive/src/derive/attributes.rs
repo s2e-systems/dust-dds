760c40949afb63b30745bf2a7f1a5c433a855097
krisjyr <kristofer.jurgenstein@mil.ee>
2025-08-18 11:42:07 +0000

    Added functionality to annotations

--- dds_derive/src/derive/attributes.rs
+++ dds_derive/src/derive/attributes.rs
@@ -1,4 +1,4 @@
-use syn::{spanned::Spanned, DeriveInput, Expr, Field, Result};
+use syn::{spanned::Spanned, DeriveInput, Attribute, Expr, Field, Result};
 
 pub enum Extensibility {
     Final,
@@ -8,61 +8,95 @@ pub enum Extensibility {
 
 pub fn get_input_extensibility(input: &DeriveInput) -> Result<Extensibility> {
     let mut extensibility = Extensibility::Final;
-    if let Some(xtypes_attribute) = input
-        .attrs
-        .iter()
-        .find(|attr| attr.path().is_ident("dust_dds"))
-    {
-        xtypes_attribute.parse_nested_meta(|meta| {
-            if meta.path.is_ident("extensibility") {
-                let format_str: syn::LitStr = meta.value()?.parse()?;
-                match format_str.value().as_ref() {
-                    "Final" => {
-                        extensibility = Extensibility::Final;
-                        Ok(())
-                    }
-                    "Appendable" => {
-                        extensibility = Extensibility::Appendable;
-                        Ok(())
-                    }
-                    "Mutable" => {
-                        extensibility = Extensibility::Mutable;
-                        Ok(())
-                    }
-                    _ => Err(syn::Error::new(
-                        meta.path.span(),
-                        r#"Invalid format specified. Valid options are "Final", "Appendable", "Mutable". "#,
-                    )),
+
+    for attr in input.attrs.iter().filter(|a| a.path().is_ident("dust_dds")) {
+        attr.parse_nested_meta(|meta| {
+            let name = meta.path.get_ident().map(|id| id.to_string());
+
+            match name.as_deref() {
+                Some("extensibility") => {
+                    let format_str: syn::LitStr = meta.value()?.parse()?;
+                    extensibility = match format_str.value().as_str() {
+                        "Final" => Extensibility::Final,
+                        "Appendable" => Extensibility::Appendable,
+                        "Mutable" => Extensibility::Mutable,
+                        other => {
+                            return Err(syn::Error::new(
+                                format_str.span(),
+                                format!("Invalid extensibility: `{}`. Use \"Final\", \"Appendable\", or \"Mutable\"", other),
+                            ));
+                        }
+                    };
                 }
-            } else {
-                Ok(())
+                Some("final") => extensibility = Extensibility::Final,
+                Some("appendable") => extensibility = Extensibility::Appendable,
+                Some("mutable") => extensibility = Extensibility::Mutable,
+                _ => {}
             }
-        })?;
-    }
-    Ok(extensibility)
-}
 
-pub struct FieldAttributes {
-    pub key: bool,
-    pub id: Option<Expr>,
-}
-
-pub fn get_field_attributes(field: &Field) -> syn::Result<FieldAttributes> {
-    let mut key = false;
-    let mut id = None;
-    if let Some(xtypes_attribute) = field
-        .attrs
-        .iter()
-        .find(|attr| attr.path().is_ident("dust_dds"))
-    {
-        xtypes_attribute.parse_nested_meta(|meta| {
-            if meta.path.is_ident("key") {
-                key = true;
-            } else if meta.path.is_ident("id") {
-                id = Some(meta.value()?.parse()?);
-            }
             Ok(())
         })?;
     }
-    Ok(FieldAttributes { key, id })
+
+    Ok(extensibility)
 }
+
+
+#[derive(Default)]
+pub struct FieldAttributes {
+    pub key: bool,
+    pub id: Option<Expr>,
+    pub optional: bool,
+    pub hashid: bool,
+    pub must_understand: bool,
+}
+
+
+pub fn get_field_attributes(field: &Field) -> Result<FieldAttributes> {
+    let mut attrs = FieldAttributes::default();
+
+    for attr in field.attrs.iter().filter(|a| a.path().is_ident("dust_dds")) {
+        attr.parse_nested_meta(|meta| {
+            let name = meta.path.get_ident().map(|id| id.to_string());
+
+            match name.as_deref() {
+                Some("key") => {
+                    attrs.key = true;
+                }
+                Some("optional") | Some("Optional") => {
+                    attrs.optional = true;
+                }
+                Some("hashid") => {
+                    attrs.hashid = true;
+                }
+                Some("must_understand") => {
+                    attrs.must_understand = true;
+                }
+                Some("id") => {
+                    let expr: Expr = meta.value()?.parse()?;
+                    if let Expr::Lit(expr_lit) = &expr {
+                        if let syn::Lit::Int(_) = &expr_lit.lit {
+                            attrs.id = Some(expr);
+                        } else {
+                            return Err(syn::Error::new(expr.span(), "Expected integer literal for `id`"));
+                        }
+                    } else {
+                        return Err(syn::Error::new(expr.span(), "Expected literal expression for `id`"));
+                    }
+                }
+                Some(other) => {
+                    return Err(syn::Error::new(
+                        meta.path.span(),
+                        format!("Unknown dust_dds field attribute: `{}`", other),
+                    ));
+                }
+                None => {}
+            }
+
+            Ok(())
+        })?;
+    }
+
+    Ok(attrs)
+}
+
--- dds_derive/src/derive/dds_serialize_data.rs
+++ dds_derive/src/derive/dds_serialize_data.rs
@@ -1,69 +1,137 @@
 use proc_macro2::{Span, TokenStream};
 use quote::quote;
 use syn::{DeriveInput, Result};
+use crate::attributes::{get_input_extensibility, get_field_attributes, Extensibility};
+
 
 pub fn expand_dds_serialize_data(input: &DeriveInput) -> Result<TokenStream> {
-    match &input.data {
-        syn::Data::Struct(_) | syn::Data::Enum(_) => {
-            let serialize_function = quote! {
-                dust_dds::infrastructure::type_support::serialize_rtps_xtypes_xcdr1_le(
-                    self,
-            )};
-
-            let (impl_generics, type_generics, where_clause) = input.generics.split_for_impl();
-            let ident = &input.ident;
-
-            Ok(quote! {
-                impl #impl_generics dust_dds::infrastructure::type_support::DdsSerialize for #ident #type_generics #where_clause {
-                    fn serialize_data(&self) -> dust_dds::infrastructure::error::DdsResult<Vec<u8>> {
-                        #serialize_function
-                    }
-                }
-            })
-        }
-        syn::Data::Union(data_union) => Err(syn::Error::new(
-            data_union.union_token.span,
-            "Union not supported",
-        )),
+    if let syn::Data::Union(data_union) = &input.data {
+        return Err(syn::Error::new(data_union.union_token.span, "Union not supported"));
     }
-}
 
-pub fn expand_dds_deserialize_data(input: &DeriveInput) -> Result<TokenStream> {
-    match &input.data {
-        syn::Data::Struct(_) | syn::Data::Enum(_) => {
-            let (_, type_generics, where_clause) = input.generics.split_for_impl();
+    let extensibility = get_input_extensibility(input)?;
 
-            // Create a '__de lifetime bound to all the lifetimes of the struct
-            let mut de_lifetime_param =
-                syn::LifetimeParam::new(syn::Lifetime::new("'__de", Span::call_site()));
-            for struct_lifetime in input.generics.lifetimes().cloned() {
-                de_lifetime_param.bounds.push(struct_lifetime.lifetime);
+    let (impl_generics, type_generics, where_clause) = input.generics.split_for_impl();
+    let ident = &input.ident;
+
+    let mut keys = Vec::new();
+    let mut ids = Vec::new();
+
+    if let syn::Data::Struct(data_struct) = &input.data {
+        if let syn::Fields::Named(fields_named) = &data_struct.fields {
+            for field in &fields_named.named {
+                let field_attrs = get_field_attributes(field)?;
+                if field_attrs.key {
+                    keys.push(field.ident.as_ref().unwrap().to_string());
+                }
+                if let Some(expr) = field_attrs.id {
+                    ids.push(format!("{}={:?}", field.ident.as_ref().unwrap(), expr));
+                }
+            }
+        }
+    }
+
+    let appendable_flag = matches!(extensibility, Extensibility::Appendable);
+    let final_flag = matches!(extensibility, Extensibility::Final);
+    let mutable_flag = matches!(extensibility, Extensibility::Mutable);
+
+    let key_fields = keys.join(", ");
+    let id_fields = ids.join(", ");
+
+    let serialize_function = quote! {
+        dust_dds::infrastructure::type_support::serialize_rtps_xtypes_xcdr1_le(self)
+    };
+
+    Ok(quote! {
+        impl #impl_generics dust_dds::infrastructure::type_support::DdsSerialize for #ident #type_generics #where_clause {
+            fn serialize_data(&self) -> dust_dds::infrastructure::error::DdsResult<Vec<u8>> {
+                #serialize_function
             }
 
-            // Append the '__de lifetime to the impl generics of the struct
-            let mut generics = input.generics.clone();
-            generics.params = Some(syn::GenericParam::Lifetime(de_lifetime_param))
-                .into_iter()
-                .chain(generics.params)
-                .collect();
-
-            let ident = &input.ident;
-
-            let deserialize_function = quote! {
-                dust_dds::infrastructure::type_support::deserialize_rtps_encapsulated_data(&mut serialized_data)
-            };
-
-            Ok(quote! {
-                impl #generics dust_dds::infrastructure::type_support::DdsDeserialize<'__de> for #ident #type_generics #where_clause {
-                    fn deserialize_data(mut serialized_data: &'__de [u8]) -> dust_dds::infrastructure::error::DdsResult<Self> {
-                        #deserialize_function
-                    }
-                }
-            })
+            #[allow(dead_code)]
+            pub fn _dds_flags_info() -> &'static str {
+                concat!(
+                    "appendable: ", if #appendable_flag { "true" } else { "false" },
+                    ", final: ", if #final_flag { "true" } else { "false" },
+                    ", mutable: ", if #mutable_flag { "true" } else { "false" },
+                    ", keys: ", #key_fields,
+                    ", ids: ", #id_fields,
+                )
+            }
         }
-        syn::Data::Union(data_union) => Err(syn::Error::new(
-            data_union.union_token.span,
-            "Union not supported",
-        )),
+    })
+}
+
+
+pub fn expand_dds_deserialize_data(input: &DeriveInput) -> Result<proc_macro2::TokenStream> {
+    if let syn::Data::Union(data_union) = &input.data {
+        return Err(syn::Error::new(data_union.union_token.span, "Union not supported"));
     }
+
+    let extensibility = get_input_extensibility(input)?;
+
+    let mut keys = Vec::new();
+    let mut ids = Vec::new();
+
+    if let syn::Data::Struct(data_struct) = &input.data {
+        if let syn::Fields::Named(fields_named) = &data_struct.fields {
+            for field in &fields_named.named {
+                let field_attrs = get_field_attributes(field)?;
+                if field_attrs.key {
+                    keys.push(field.ident.as_ref().unwrap().to_string());
+                }
+                if let Some(expr) = field_attrs.id {
+                    ids.push(format!("{}={:?}", field.ident.as_ref().unwrap(), expr));
+                }
+            }
+        }
+    }
+
+    let appendable_flag = matches!(extensibility, Extensibility::Appendable);
+    let final_flag = matches!(extensibility, Extensibility::Final);
+    let mutable_flag = matches!(extensibility, Extensibility::Mutable);
+
+    let key_fields = keys.join(", ");
+    let id_fields = ids.join(", ");
+
+    let (_, type_generics, where_clause) = input.generics.split_for_impl();
+
+    let mut de_lifetime_param =
+        syn::LifetimeParam::new(syn::Lifetime::new("'__de", Span::call_site()));
+
+    for lifetime_def in input.generics.lifetimes() {
+        de_lifetime_param.bounds.push(lifetime_def.lifetime.clone());
+    }
+
+    let mut generics = input.generics.clone();
+    generics.params = Some(syn::GenericParam::Lifetime(de_lifetime_param))
+        .into_iter()
+        .chain(generics.params)
+        .collect();
+
+    let ident = &input.ident;
+
+    // Your deserialization logic
+    let deserialize_function = quote! {
+        dust_dds::infrastructure::type_support::deserialize_rtps_encapsulated_data(&mut serialized_data)
+    };
+
+    Ok(quote! {
+        impl #generics dust_dds::infrastructure::type_support::DdsDeserialize<'__de> for #ident #type_generics #where_clause {
+            fn deserialize_data(mut serialized_data: &'__de [u8]) -> dust_dds::infrastructure::error::DdsResult<Self> {
+                #deserialize_function
+            }
+
+            #[allow(dead_code)]
+            pub fn _dds_flags_info() -> &'static str {
+                concat!(
+                    "appendable: ", if #appendable_flag { "true" } else { "false" },
+                    ", final: ", if #final_flag { "true" } else { "false" },
+                    ", mutable: ", if #mutable_flag { "true" } else { "false" },
+                    ", keys: ", #key_fields,
+                    ", ids: ", #id_fields,
+                )
+            }
+        }
+    })
 }
