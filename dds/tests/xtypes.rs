ce79f17e52a6546bc7488eea37e2c768581d60bc
Joao Rebelo <jrebelo@s2e-systems.com>
2025-09-18 14:06:33 +0200

    Add failing add member test

--- dds/tests/xtypes.rs
+++ dds/tests/xtypes.rs
@@ -1,4 +1,10 @@
-use dust_dds::xtypes::{dynamic_type::DynamicTypeBuilderFactory, type_object::TypeKind};
+use dust_dds::xtypes::{
+    dynamic_type::{
+        DynamicTypeBuilderFactory, ExtensibilityKind, MemberDescriptor, TryConstructKind,
+        TypeDescriptor,
+    },
+    type_object::{StringSTypeDefn, TypeIdentifier, TypeKind},
+};
 
 #[test]
 fn build_valid_primitive_types() {
@@ -15,4 +21,32 @@ fn build_invalid_primitive_types() {
     assert_eq!(dynamic_type.get_kind(), TypeKind::NONE);
     assert_eq!(dynamic_type.get_name(), "");
     assert_eq!(dynamic_type.get_member_count(), 0);
+}
+
+#[test]
+fn create_type_with_builder() {
+    let mut builder = DynamicTypeBuilderFactory::create_type(TypeDescriptor {
+        kind: TypeKind::STRUCTURE,
+        name: String::from("Shape"),
+        extensibility_kind: ExtensibilityKind::Appendable,
+        is_nested: false,
+    });
+
+    builder
+        .add_member(MemberDescriptor {
+            name: String::from("Color"),
+            id: 0,
+            r#type: TypeIdentifier::TiString8Small {
+                string_sdefn: StringSTypeDefn { bound: 128 },
+            },
+            default_value: "",
+            index: 0,
+            try_construct_kind: TryConstructKind::UseDefault,
+            is_key: true,
+            is_optional: false,
+            is_must_understand: true,
+            is_shared: false,
+            is_default_label: false,
+        })
+        .unwrap();
 }
