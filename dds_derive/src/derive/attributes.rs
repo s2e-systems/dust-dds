0f67ba40a2964711a0451e262f79917b5ae872a0
krisjyr <kristofer.jurgenstein@mil.ee>
2025-09-22 13:47:38 +0000

    Combined #400 and IDL gen

--- dds_derive/src/derive/attributes.rs
+++ dds_derive/src/derive/attributes.rs
@@ -8,34 +8,43 @@ pub enum Extensibility {
 
 pub fn get_input_extensibility(input: &DeriveInput) -> Result<Extensibility> {
     let mut extensibility = Extensibility::Final;
-    if let Some(xtypes_attribute) = input
-        .attrs
-        .iter()
-        .find(|attr| attr.path().is_ident("dust_dds"))
-    {
-        xtypes_attribute.parse_nested_meta(|meta| {
-            if meta.path.is_ident("extensibility") {
-                let format_str: syn::LitStr = meta.value()?.parse()?;
-                match format_str.value().as_ref() {
-                    "final" => {
-                        extensibility = Extensibility::Final;
-                        Ok(())
-                    }
-                    "appendable" => {
-                        extensibility = Extensibility::Appendable;
-                        Ok(())
-                    }
-                    "mutable" => {
-                        extensibility = Extensibility::Mutable;
-                        Ok(())
-                    }
-                    _ => Err(syn::Error::new(
-                        meta.path.span(),
-                        r#"Invalid format specified. Valid options are "final", "appendable", "mutable". "#,
-                    )),
+
+    for attr in input.attrs.iter().filter(|a| a.path().is_ident("dust_dds")) {
+        attr.parse_nested_meta(|meta| {
+            let name = meta.path.get_ident().map(|id| id.to_string());
+
+            match name.as_deref() {
+                Some("extensibility") => {
+                    let format_str: syn::LitStr = meta.value()?.parse()?;
+                    extensibility = match format_str.value().to_ascii_lowercase().as_str() {
+                        "final" => Extensibility::Final,
+                        "appendable" => Extensibility::Appendable,
+                        "mutable" => Extensibility::Mutable,
+                        other => {
+                            return Err(syn::Error::new(
+                                format_str.span(),
+                                format!(
+                                    "Invalid extensibility: `{}`. Use \"Final\", \"final\", \"Appendable\", \"appendable\", \"Mutable\", or \"mutable\"",
+                                    other
+                                ),
+                            ));
+                        }
+                    };
+                    Ok(())
                 }
-            } else {
-                Ok(())
+                Some("final") => {
+                    extensibility = Extensibility::Final;
+                    Ok(())
+                }
+                Some("appendable") => {
+                    extensibility = Extensibility::Appendable;
+                    Ok(())
+                }
+                Some("mutable") => {
+                    extensibility = Extensibility::Mutable;
+                    Ok(())
+                }
+                _ => Ok(()),
             }
         })?;
     }
--- dds_derive/src/derive/dds_serialize_data.rs
+++ dds_derive/src/derive/dds_serialize_data.rs
@@ -48,30 +48,34 @@ pub fn expand_dds_serialize_data(input: &DeriveInput) -> Result<TokenStream> {
         dust_dds::infrastructure::type_support::serialize_rtps_xtypes_xcdr1_le(self)
     };
 
-            Ok(quote! {
-                #[automatically_derived]
-                impl #impl_generics dust_dds::infrastructure::type_support::DdsSerialize for #ident #type_generics #where_clause {
-                    fn serialize_data(&self) -> dust_dds::infrastructure::error::DdsResult<Vec<u8>> {
-                        #serialize_function
-                    }
-                }
-            })
+    Ok(quote! {
+        #[automatically_derived]
+        impl #impl_generics dust_dds::infrastructure::type_support::DdsSerialize for #ident #type_generics #where_clause {
+            fn serialize_data(&self) -> dust_dds::infrastructure::error::DdsResult<Vec<u8>> {
+                #serialize_function
+            }
         }
-        syn::Data::Union(data_union) => Err(syn::Error::new(
-            data_union.union_token.span,
-            "Union not supported",
-        )),
-    }
+
+        impl #impl_generics #ident #type_generics #where_clause {
+            #[allow(dead_code)]
+            fn _dds_serialize_flags_info() -> &'static str {
+                #flags_info_literal
+            }
+        }
+    })
 }
 
 
 pub fn expand_dds_deserialize_data(input: &DeriveInput) -> Result<TokenStream> {
+    // Check for unsupported union type
     if let syn::Data::Union(data_union) = &input.data {
         return Err(syn::Error::new(data_union.union_token.span, "Union not supported"));
     }
 
+    // Get extensibility
     let extensibility = get_input_extensibility(input)?;
 
+    // Process struct fields for keys and ids
     let mut keys = Vec::new();
     let mut ids = Vec::new();
 
@@ -89,28 +93,30 @@ pub fn expand_dds_deserialize_data(input: &DeriveInput) -> Result<TokenStream> {
         }
     }
 
+    // Create flags for extensibility
     let appendable_flag = matches!(extensibility, Extensibility::Appendable);
     let final_flag = matches!(extensibility, Extensibility::Final);
     let mutable_flag = matches!(extensibility, Extensibility::Mutable);
 
+    // Format flags info string
     let key_fields = keys.join(", ");
     let id_fields = ids.join(", ");
-
     let flags_info_string = format!(
         "appendable: {}, final: {}, mutable: {}, keys: {}, ids: {}",
         appendable_flag, final_flag, mutable_flag, key_fields, id_fields
     );
     let flags_info_literal = LitStr::new(&flags_info_string, Span::call_site());
 
+    // Split generics for impl
     let (_, type_generics, where_clause) = input.generics.split_for_impl();
 
-    let mut de_lifetime_param =
-        syn::LifetimeParam::new(syn::Lifetime::new("'__de", Span::call_site()));
-
+    // Create '__de lifetime bound to all lifetimes of the struct
+    let mut de_lifetime_param = syn::LifetimeParam::new(syn::Lifetime::new("'__de", Span::call_site()));
     for lifetime_def in input.generics.lifetimes() {
         de_lifetime_param.bounds.push(lifetime_def.lifetime.clone());
     }
 
+    // Append '__de lifetime to the impl generics
     let mut generics = input.generics.clone();
     generics.params = Some(syn::GenericParam::Lifetime(de_lifetime_param))
         .into_iter()
@@ -119,22 +125,25 @@ pub fn expand_dds_deserialize_data(input: &DeriveInput) -> Result<TokenStream> {
 
     let ident = &input.ident;
 
+    // Define deserialization function
     let deserialize_function = quote! {
         dust_dds::infrastructure::type_support::deserialize_rtps_encapsulated_data(&mut serialized_data)
     };
 
-            Ok(quote! {
-                #[automatically_derived]
-                impl #generics dust_dds::infrastructure::type_support::DdsDeserialize<'__de> for #ident #type_generics #where_clause {
-                    fn deserialize_data(mut serialized_data: &'__de [u8]) -> dust_dds::infrastructure::error::DdsResult<Self> {
-                        #deserialize_function
-                    }
-                }
-            })
+    // Generate DdsDeserialize trait implementation and flags info method
+    Ok(quote! {
+        #[automatically_derived]
+        impl #generics dust_dds::infrastructure::type_support::DdsDeserialize<'__de> for #ident #type_generics #where_clause {
+            fn deserialize_data(mut serialized_data: &'__de [u8]) -> dust_dds::infrastructure::error::DdsResult<Self> {
+                #deserialize_function
+            }
         }
-        syn::Data::Union(data_union) => Err(syn::Error::new(
-            data_union.union_token.span,
-            "Union not supported",
-        )),
-    }
+
+        impl #generics #ident #type_generics #where_clause {
+            #[allow(dead_code)]
+            fn _dds_deserialize_flags_info() -> &'static str {
+                #flags_info_literal
+            }
+        }
+    })
 }
