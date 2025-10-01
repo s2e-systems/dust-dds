fc80d7c0f42478e18f888d9fff2a45b53718be22
Stefan Kimmer <skimmer@s2e-systems.com>
2025-10-01 09:34:44 +0200

    add nested tests

--- dds/src/xtypes/data_representation.rs
+++ dds/src/xtypes/data_representation.rs
@@ -4,7 +4,7 @@ use crate::xtypes::{
         TK_UINT32, TK_UINT8,
     },
     serialize::XTypesSerialize,
-    serializer::SerializeFinalStruct,
+    serializer::{SerializeFinalStruct, SerializeMutableStruct},
 };
 
 impl XTypesSerialize for DynamicData {
@@ -46,18 +46,65 @@ impl XTypesSerialize for DynamicData {
                                 let value = self.get_int64_value(member_id)?;
                                 final_serializer.serialize_field(value, member_name)?;
                             }
+                            TK_STRUCTURE => {
+                                let complex_value = self.get_complex_value(member_id)?;
+                                final_serializer.serialize_field(complex_value, member_name)?;
+                            }
+
                             _ => todo!("Not implemented for members of type {member_type_kind:x?}"),
                         }
                     }
                     Ok(())
                 }
                 ExtensibilityKind::Appendable => {
-                    let appendable_serializer = serializer.serialize_appendable_struct()?;
+                    let mutappendable_serializer = serializer.serialize_appendable_struct()?;
                     todo!()
                 }
                 ExtensibilityKind::Mutable => {
-                    let mutable_serializer = serializer.serialize_mutable_struct()?;
-                    todo!()
+                    let mut mutable_serializer = serializer.serialize_mutable_struct()?;
+                    for field_index in 0..self.get_item_count() {
+                        let member_id = self.get_member_id_at_index(field_index)?;
+                        let member_descriptor = self.get_descriptor(member_id)?;
+                        let member_type_kind = member_descriptor.r#type.get_kind();
+                        let member_name = &member_descriptor.name;
+                        let pid = member_id;
+                        match member_type_kind {
+                            TK_UINT8 => {
+                                let value = self.get_uint8_value(member_id)?;
+                                mutable_serializer.serialize_field(value, pid, member_name)?;
+                            }
+                            TK_UINT16 => {
+                                let value = self.get_uint16_value(member_id)?;
+                                mutable_serializer.serialize_field(value, pid, member_name)?;
+                            }
+                            TK_UINT32 => {
+                                let value = self.get_uint32_value(member_id)?;
+                                mutable_serializer.serialize_field(value, pid, member_name)?;
+                            }
+                            TK_INT16 => {
+                                let value = self.get_int16_value(member_id)?;
+                                mutable_serializer.serialize_field(value, pid, member_name)?;
+                            }
+                            TK_INT32 => {
+                                let value = self.get_int32_value(member_id)?;
+                                mutable_serializer.serialize_field(value, pid, member_name)?;
+                            }
+                            TK_INT64 => {
+                                let value = self.get_int64_value(member_id)?;
+                                mutable_serializer.serialize_field(value, pid, member_name)?;
+                            }
+                            TK_STRUCTURE => {
+                                let value = self.get_complex_value(member_id)?;
+                                mutable_serializer.serialize_field(value, pid, member_name)?;
+                            }
+
+                            _ => {
+                                todo!("Not implemented for members of type 0x{member_type_kind:x?}")
+                            }
+                        }
+                    }
+                    mutable_serializer.end()?;
+                    Ok(())
                 }
             },
             _ => todo!(),
@@ -67,11 +114,10 @@ impl XTypesSerialize for DynamicData {
 
 #[cfg(test)]
 mod tests {
+    use super::*;
     use crate::{
         infrastructure::type_support::TypeSupport, xtypes::xcdr_serializer::Xcdr1LeSerializer,
     };
-
-    use super::*;
 
     #[test]
     fn serialize_final_struct_cdr1() {
@@ -104,6 +150,112 @@ mod tests {
                 200, 0, 0, 0, //b
                 10, 0, //c
                 20, 0, //d
+            ]
+        )
+    }
+
+    #[test]
+    fn serialize_nested_final_struct_cdr1() {
+        #[derive(TypeSupport)]
+        struct Nested {
+            n: u16,
+            m: i16,
+        }
+        #[derive(TypeSupport)]
+        struct FinalStruct {
+            a: i32,
+            nested: Nested,
+        }
+        let dynamic_sample = FinalStruct {
+            a: 1,
+            nested: Nested { n: 2, m: 3 },
+        }
+        .create_dynamic_sample();
+        let mut buffer = vec![];
+        dynamic_sample
+            .serialize(&mut Xcdr1LeSerializer::new(&mut buffer))
+            .unwrap();
+        assert_eq!(
+            &buffer,
+            &[
+                1, 0, 0, 0, // a
+                2, 0, 3, 0, // nested
+            ]
+        )
+    }
+
+    #[test]
+    fn serialize_mutable_struct_cdr1() {
+        #[derive(TypeSupport)]
+        #[dust_dds(extensibility = "mutable")]
+        struct MutableStruct {
+            #[dust_dds(id = 0x07)]
+            byte: u8,
+            #[dust_dds(id = 0x08)]
+            long: i32,
+        }
+        let dynamic_sample = MutableStruct { byte: 1, long: 2 }.create_dynamic_sample();
+        let mut buffer = vec![];
+        dynamic_sample
+            .serialize(&mut Xcdr1LeSerializer::new(&mut buffer))
+            .unwrap();
+        assert_eq!(
+            &buffer,
+            &[
+                0x07, 0x00, 1, 0, // PID | length
+                1, 0, 0, 0, // byte | padding
+                0x08, 0x00, 4, 0, // PID | length
+                2, 0, 0, 0, // long
+                1, 0, 0, 0, // Sentinel
+            ]
+        )
+    }
+
+    #[test]
+    fn serialize_nested_mutable_struct_cdr1() {
+        #[derive(TypeSupport)]
+        #[dust_dds(extensibility = "mutable", nested)]
+        struct NestedStruct {
+            #[dust_dds(id = 0x09)]
+            nested_byte: u8,
+            #[dust_dds(id = 0x0A)]
+            nested_long: i32,
+        }
+        #[derive(TypeSupport)]
+        #[dust_dds(extensibility = "mutable")]
+        struct MutableStruct {
+            #[dust_dds(id = 0x07)]
+            byte: u8,
+            #[dust_dds(id = 0xFF)]
+            nested: NestedStruct,
+            #[dust_dds(id = 0x08)]
+            long: i32,
+        }
+        let dynamic_sample = MutableStruct {
+            byte: 1,
+            long: 2,
+            nested: NestedStruct {
+                nested_byte: 11,
+                nested_long: 22,
+            },
+        }
+        .create_dynamic_sample();
+        let mut buffer = vec![];
+        dynamic_sample
+            .serialize(&mut Xcdr1LeSerializer::new(&mut buffer))
+            .unwrap();
+        assert_eq!(
+            &buffer,
+            &[
+                0x07, 0x00, 1, 0, // PID | length
+                1, 0, 0, 0, // byte | padding
+                0x09, 0x00, 1, 0, // PID | length
+                11, 0, 0, 0, // nested_byte | padding
+                0x0A, 0x00, 1, 0, // PID | length
+                22, 0, 0, 0, // nested_long | padding
+                0x08, 0x00, 4, 0, // PID | length
+                2, 0, 0, 0, // long
+                1, 0, 0, 0, // Sentinel
             ]
         )
     }
