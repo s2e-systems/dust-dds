979a88b5a7f4243bb57f2239a717df279fded69b
Joao Rebelo <jrebelo@s2e-systems.com>
2025-10-29 14:19:51 +0100

    Only set the key flag if there is no data

--- dds/src/rtps/cache_change.rs
+++ dds/src/rtps/cache_change.rs
@@ -26,7 +26,7 @@ impl CacheChange {
             ChangeKind::Alive | ChangeKind::AliveFiltered => (true, false),
             ChangeKind::NotAliveDisposed
             | ChangeKind::NotAliveUnregistered
-            | ChangeKind::NotAliveDisposedUnregistered => (false, true),
+            | ChangeKind::NotAliveDisposedUnregistered => (false, !self.data_value.is_empty()),
         };
 
         let mut parameters = Vec::with_capacity(2);
