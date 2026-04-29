40d54223179f7cd80755b50e9a44af3a7dfb01f2
Stefan Kimmer <skimmer@s2e-systems.com>
2026-04-29 11:17:57 +0200

    merge tests

--- dds_gen/tests/id_attributes.rs
+++ dds_gen/tests/id_attributes.rs
@@ -3,11 +3,21 @@ use std::path::Path;
 use syn::File;
 
 #[test]
-fn key_and_id_attributes() {
-    let idl_file = Path::new("tests/key_and_id.idl");
+fn id_attributes() {
+    let idl_file = Path::new("tests/id_attributes.idl");
 
     let expected = syn::parse2::<File>(
         r#"
+            #[derive(Debug, dust_dds::infrastructure::type_support::DdsType)]
+            pub struct Person {
+                #[dust_dds(id = 1)]
+                pub name: String,
+                #[dust_dds(id = 2)]
+                pub age: i32,
+                #[dust_dds(id = 3)]
+                pub height: f64,
+            }
+
             #[derive(Debug, dust_dds::infrastructure::type_support::DdsType)]
             pub struct Message {
                 #[dust_dds(key)]
