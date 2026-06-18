ce535cce29f72df3196960ffba463e252f130724
Joao Rebelo <2730147+jrebelo@users.noreply.github.com>
2026-06-18 16:32:59 +0200

    Add type support documentation

--- dds/src/xtypes/mod.rs
+++ dds/src/xtypes/mod.rs
@@ -1,9 +1,19 @@
+#[doc(hidden)]
 pub mod bytes;
+#[doc(hidden)]
 pub mod data_storage;
+#[doc(hidden)]
 pub mod deserializer;
+#[doc(hidden)]
 pub mod dynamic_type;
+#[doc(hidden)]
 pub mod error;
+#[doc(hidden)]
 pub mod read_write;
+#[doc(hidden)]
 pub mod serializer;
+#[doc(hidden)]
 pub mod type_object;
+
+/// Traits related to the representation of XTypes required to be transmitted using DDS
 pub mod type_support;
