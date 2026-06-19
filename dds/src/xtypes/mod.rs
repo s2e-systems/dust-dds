e5f576bd011c9f4c400c8365f05c3d45cf8a5ea6
Joao Rebelo <2730147+jrebelo@users.noreply.github.com>
2026-06-19 09:12:02 +0200

    Add documentation to type_object module

--- dds/src/xtypes/mod.rs
+++ dds/src/xtypes/mod.rs
@@ -14,7 +14,10 @@ pub mod dynamic_type;
 /// Classes related to the XTypes error and return codes.
 pub mod error;
 
-#[doc(hidden)]
+/// Type Object representation as defined in the DDS-XTypes 1.3 standard.
+///
+/// A TypeObject describes a type structure completely or minimally, allowing DDS participants
+/// to exchange type information dynamically during discovery.
 pub mod type_object;
 
 /// Traits related to the representation of XTypes required to be transmitted using DDS
