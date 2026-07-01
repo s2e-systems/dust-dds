184afcd778f62da135430fd1e03a5e3b93b06f22
Joao Rebelo <jrebelo@s2e-systems.com>
2026-07-01 12:57:04 +0200

    Handle nested keys

--- dds/src/dcps/xtypes_glue/key_and_instance_handle.rs
+++ dds/src/dcps/xtypes_glue/key_and_instance_handle.rs
@@ -2,7 +2,8 @@ use crate::{
     infrastructure::instance::InstanceHandle,
     xtypes::{
         dynamic_type::{
-            DynamicData, DynamicDataFactory, DynamicType, DynamicTypeBuilderFactory, TypeKind,
+            DynamicData, DynamicDataFactory, DynamicType, DynamicTypeBuilder,
+            DynamicTypeBuilderFactory, TypeKind,
         },
         error::{XTypesError, XTypesResult},
         serializer::serialize_final_without_header,
@@ -14,15 +15,26 @@ pub struct KeyHolderType<'a>(DynamicType<'a>);
 
 impl<'a> KeyHolderType<'a> {
     pub fn from_dynamic_type(value: &DynamicType<'a>) -> XTypesResult<Self> {
+        fn fill_struct_key_holder_type<'a>(
+            value: &DynamicType<'a>,
+            builder: &mut DynamicTypeBuilder,
+        ) -> XTypesResult<()> {
+            for member_index in 0..value.get_member_count() {
+                let dynamic_type_member = value.get_member_by_index(member_index)?;
+                if dynamic_type_member.get_descriptor()?.is_key {
+                    builder.add_member(dynamic_type_member.get_descriptor()?.clone())?;
+                } else if dynamic_type_member.descriptor.r#type.descriptor.kind
+                    == TypeKind::STRUCTURE
+                {
+                    fill_struct_key_holder_type(&dynamic_type_member.descriptor.r#type, builder)?;
+                }
+            }
+            Ok(())
+        }
+
         let mut key_holder_type_builder =
             DynamicTypeBuilderFactory::create_type(value.descriptor.clone());
-        for member_index in 0..value.get_member_count() {
-            let dynamic_type_member = value.get_member_by_index(member_index)?;
-            if dynamic_type_member.get_descriptor()?.is_key {
-                key_holder_type_builder
-                    .add_member(dynamic_type_member.get_descriptor()?.clone())?;
-            }
-        }
+        fill_struct_key_holder_type(value, &mut key_holder_type_builder)?;
         Ok(Self(key_holder_type_builder.build()))
     }
 }
@@ -31,14 +43,29 @@ pub struct KeyHolderData<'a>(DynamicData<'a>);
 
 impl<'a> KeyHolderData<'a> {
     pub fn from_dynamic_data(value: &DynamicData<'a>) -> XTypesResult<KeyHolderData<'a>> {
+        fn fill_struct_key_holder_data<'a>(
+            value: &DynamicData<'a>,
+            key_holder_data: &mut DynamicData,
+        ) -> XTypesResult<()> {
+            let dynamic_type = value.r#type();
+            for member_index in 0..dynamic_type.get_member_count() {
+                let dynamic_type_member = dynamic_type.get_member_by_index(member_index)?;
+                let key_member_id = dynamic_type_member.get_id();
+                if dynamic_type_member.descriptor.is_key {
+                    key_holder_data
+                        .set_value(key_member_id, value.get_value(key_member_id)?.clone());
+                } else if dynamic_type_member.descriptor.r#type.get_kind() == TypeKind::STRUCTURE {
+                    fill_struct_key_holder_data(
+                        value.get_complex_value(key_member_id)?,
+                        key_holder_data,
+                    )?;
+                }
+            }
+            Ok(())
+        }
         let key_holder_type = KeyHolderType::from_dynamic_type(&value.r#type())?.0;
         let mut key_holder_data = DynamicDataFactory::create_data(key_holder_type);
-        for key_member_index in 0..key_holder_type.get_member_count() {
-            let key_member_id = key_holder_type
-                .get_member_by_index(key_member_index)?
-                .get_id();
-            key_holder_data.set_value(key_member_id, value.get_value(key_member_id)?.clone());
-        }
+        fill_struct_key_holder_data(value, &mut key_holder_data)?;
         Ok(Self(key_holder_data))
     }
 
@@ -50,18 +77,15 @@ impl<'a> KeyHolderData<'a> {
 pub fn get_instance_handle_from_key_holder_data<'a>(
     key_holder_data: &KeyHolderData<'a>,
 ) -> Result<InstanceHandle, XTypesError> {
-    let key = if key_holder_data.0.r#type().get_kind() == TypeKind::STRUCTURE {
-        let data = serialize_final_without_header(Vec::new(), &key_holder_data.0)?;
-        if data.len() <= 16 {
-            let mut key = [0; 16];
-            key[0..data.len()].copy_from_slice(&data);
-            key
-        } else {
-            md5::compute(data).into()
-        }
+    let data = serialize_final_without_header(Vec::new(), &key_holder_data.0)?;
+    let key = if data.len() <= 16 {
+        let mut key = [0; 16];
+        key[0..data.len()].copy_from_slice(&data);
+        key
     } else {
-        [0; 16]
+        md5::compute(data).into()
     };
+
     Ok(InstanceHandle::new(key))
 }
 
@@ -70,4 +94,80 @@ pub fn get_instance_handle_from_dynamic_data<'a>(
 ) -> Result<InstanceHandle, XTypesError> {
     let key_holder_data = KeyHolderData::from_dynamic_data(value)?;
     get_instance_handle_from_key_holder_data(&key_holder_data)
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use crate::{
+        infrastructure::instance::InstanceHandle,
+        xtypes::{dynamic_type::DynamicDataFactory, type_support::TypeSupport},
+    };
+
+    #[test]
+    fn test_multiple_nested_keys() {
+        #[derive(TypeSupport)]
+        struct Inner {
+            #[dust_dds(key)]
+            id: u8,
+        }
+
+        #[derive(TypeSupport)]
+        struct Outer {
+            a: Inner,
+            #[dust_dds(key)]
+            b: u16,
+        }
+
+        #[derive(TypeSupport)]
+        struct OuterOuter {
+            a: Outer,
+        }
+
+        let outer_outer = OuterOuter {
+            a: Outer {
+                a: Inner { id: 1 },
+                b: 3,
+            },
+        };
+
+        let mut data = DynamicDataFactory::create_data(OuterOuter::get_type());
+        outer_outer.create_dynamic_sample(&mut data);
+
+        let instance_handle = get_instance_handle_from_dynamic_data(&data).unwrap();
+
+        assert_eq!(
+            instance_handle,
+            InstanceHandle::new([1, 0, 0, 3, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0])
+        )
+    }
+
+    #[test]
+    fn test_full_struct_key() {
+        #[derive(TypeSupport)]
+        struct Inner {
+            id: u8,
+            b: u16,
+        }
+
+        #[derive(TypeSupport)]
+        struct Outer {
+            #[dust_dds(key)]
+            a: Inner,
+        }
+
+        let outer = Outer {
+            a: Inner { id: 1, b: 3 },
+        };
+
+        let mut data = DynamicDataFactory::create_data(Outer::get_type());
+        outer.create_dynamic_sample(&mut data);
+
+        let instance_handle = get_instance_handle_from_dynamic_data(&data).unwrap();
+
+        assert_eq!(
+            instance_handle,
+            InstanceHandle::new([1, 0, 0, 3, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0])
+        )
+    }
 }
