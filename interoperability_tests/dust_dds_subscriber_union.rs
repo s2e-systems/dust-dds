7fea7b441aef422d5f3c0d3250ca8c1351809a02
Carlo Corradini <carlo.ita98@gmail.com>
2026-07-02 23:00:23 +0200

    test: union interoperability test (#564)
    * test: union interoperability test
    
    * chore(interoperability_tests): add check return code when calling dds_write
    
    * Change TypeSupport import path
    
    * Add missing type identifier conversions
    
    * Fix typo
    
    * Add generated union
    
    * Add pub use to each variant of enum
    
    * Revert "Add pub use to each variant of enum"
    
    This reverts commit 9e280897996d489a2c37bc7f76d0b1ec9d802428.
    
    * Simplify interoperability test
    
    * Fix circle ci yml error
    
    * Add missing inheritance files
    
    * Add name to union type definition
    
    * Revert line ending of interoperability cargo file
    
    ---------
    
    Co-authored-by: Joao Rebelo <jrebelo@s2e-systems.com>
    Co-authored-by: Joao Rebelo <2730147+jrebelo@users.noreply.github.com>

---
+++ interoperability_tests/dust_dds_subscriber_union.rs
@@ -0,0 +1,80 @@
+include!("target/idl/union.rs");
+
+use crate::interoperability::test::VariantUnion;
+use dust_dds::{
+    domain::domain_participant_factory::DomainParticipantFactory,
+    infrastructure::{
+        listener::NO_LISTENER,
+        qos::{DataReaderQos, QosKind},
+        qos_policy::{
+            DurabilityQosPolicy, DurabilityQosPolicyKind, ReliabilityQosPolicy,
+            ReliabilityQosPolicyKind,
+        },
+        sample_info::{ANY_INSTANCE_STATE, ANY_SAMPLE_STATE, ANY_VIEW_STATE},
+        status::{NO_STATUS, StatusKind},
+        time::{Duration, DurationKind},
+    },
+    wait_set::{Condition, WaitSet},
+};
+
+fn main() {
+    let domain_id = 0;
+    let participant_factory = DomainParticipantFactory::get_instance();
+
+    let participant = participant_factory
+        .create_participant(domain_id, QosKind::Default, NO_LISTENER, NO_STATUS)
+        .unwrap();
+
+    let topic = participant
+        .find_topic::<VariantUnion>("Union", Duration::new(120, 0))
+        .unwrap();
+
+    let subscriber = participant
+        .create_subscriber(QosKind::Default, NO_LISTENER, NO_STATUS)
+        .unwrap();
+
+    let reader_qos = DataReaderQos {
+        reliability: ReliabilityQosPolicy {
+            kind: ReliabilityQosPolicyKind::Reliable,
+            max_blocking_time: DurationKind::Finite(Duration::new(1, 0)),
+        },
+        durability: DurabilityQosPolicy {
+            kind: DurabilityQosPolicyKind::TransientLocal,
+        },
+        ..Default::default()
+    };
+    let reader = subscriber
+        .create_datareader::<VariantUnion>(
+            &topic,
+            QosKind::Specific(reader_qos),
+            NO_LISTENER,
+            NO_STATUS,
+        )
+        .unwrap();
+
+    let reader_cond = reader.get_statuscondition();
+    reader_cond
+        .set_enabled_statuses(&[StatusKind::SubscriptionMatched])
+        .unwrap();
+    let mut wait_set = WaitSet::new();
+    wait_set
+        .attach_condition(Condition::StatusCondition(reader_cond.clone()))
+        .unwrap();
+
+    wait_set.wait(Duration::new(60, 0)).unwrap();
+
+    reader_cond
+        .set_enabled_statuses(&[StatusKind::DataAvailable])
+        .unwrap();
+    wait_set.wait(Duration::new(30, 0)).unwrap();
+
+    let samples = reader
+        .read(1, ANY_SAMPLE_STATE, ANY_VIEW_STATE, ANY_INSTANCE_STATE)
+        .unwrap();
+
+    let data = samples[0].data.as_ref().unwrap();
+    println!("Received: {data:?}");
+
+    // Sleep to allow sending acknowledgements
+    std::thread::sleep(std::time::Duration::from_secs(2));
+}
