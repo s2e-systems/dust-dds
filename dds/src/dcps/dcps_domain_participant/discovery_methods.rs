e50358cc922637331973404d7cbaec5690ab6ccb
Joao Rebelo <2730147+jrebelo@users.noreply.github.com>
2026-07-15 16:54:09 +0200

    Replace if by match

--- dds/src/dcps/dcps_domain_participant/discovery_methods.rs
+++ dds/src/dcps/dcps_domain_participant/discovery_methods.rs
@@ -783,37 +783,41 @@ impl DcpsDomainParticipant {
                             .find(|x| x.topic_name == data_writer.topic_name)
                             .expect("A matched topic to the writer must exist");
 
-                        let is_matched_type = if let Some(discovered_type_information) =
-                            &discovered_reader_data
-                                .dds_subscription_data
-                                .type_information
-                            // This additional check is done for interoperability with implementations that do not communicate the correct type information. In that case we fallback to matching on type name 
-                            && discovered_type_information
-                                .complete
-                                .typeid_with_size
-                                .typeobject_serialized_size
-                                > 0
+                        let is_matched_type = match &discovered_reader_data
+                            .dds_subscription_data
+                            .type_information
                         {
-                            // If the minimal hash match it is guaranteed compatible
-                            if writer_associated_topic
-                                .type_information
-                                .minimal
-                                .typeid_with_size
-                                == discovered_type_information.minimal.typeid_with_size
+                            Some(discovered_type_information)
+                            // This additional check is done for interoperability with implementations that 
+                            // do not communicate the correct type information. 
+                            // In that case we fallback to matching on type name 
+                                if discovered_type_information
+                                    .complete
+                                    .typeid_with_size
+                                    .typeobject_serialized_size
+                                    > 0 =>
                             {
-                                true
-                            } else if let Some(discovered_type_information) =
-                                writer_associated_topic
-                                    .discovered_type_representation
-                                    .iter()
-                                    .find(|(x, _)| x == discovered_type_information)
-                            {
-                                match &discovered_type_information.1 {
-                                    DiscoveredTypeRepresentationState::Requested => {
-                                        return;
-                                    }
-                                    DiscoveredTypeRepresentationState::Discovered(type_object) => {
-                                        match &type_object {
+                                // If the minimal hash match it is guaranteed compatible
+                                if writer_associated_topic
+                                    .type_information
+                                    .minimal
+                                    .typeid_with_size
+                                    == discovered_type_information.minimal.typeid_with_size
+                                {
+                                    true
+                                } else if let Some(discovered_type_information) =
+                                    writer_associated_topic
+                                        .discovered_type_representation
+                                        .iter()
+                                        .find(|(x, _)| x == discovered_type_information)
+                                {
+                                    match &discovered_type_information.1 {
+                                        DiscoveredTypeRepresentationState::Requested => {
+                                            return;
+                                        }
+                                        DiscoveredTypeRepresentationState::Discovered(
+                                            type_object,
+                                        ) => match &type_object {
                                             TypeObject::EkComplete { complete } => {
                                                 CompleteTypeObject::from(
                                                     writer_associated_topic.type_support,
@@ -825,15 +829,16 @@ impl DcpsDomainParticipant {
                                                     writer_associated_topic.type_support,
                                                 ) == minimal
                                             }
-                                        }
+                                        },
                                     }
+                                } else {
+                                    todo!("Must send a request for this type")
                                 }
-                            } else {
-                                todo!("Must send a request for this type")
                             }
-                        } else {
-                            discovered_reader_data.dds_subscription_data.get_type_name()
-                                == writer_associated_topic.type_name
+                            _ => {
+                                discovered_reader_data.dds_subscription_data.get_type_name()
+                                    == writer_associated_topic.type_name
+                            }
                         };
 
                         let the_participant = DomainParticipantAsync::new(
