56b9493b773df19d74ce14562b41f0d822603887
Joao Rebelo <2730147+jrebelo@users.noreply.github.com>
2026-07-21 17:39:57 +0200

    Add executable for dust_dds_idl_gen

--- dds_gen/src/lib.rs
+++ dds_gen/src/lib.rs
@@ -5,6 +5,7 @@ mod preprocessor;
 use pest::Parser;
 use std::path::Path;
 
+use crate::generator::c::CGenerator;
 use crate::generator::rust::RustGenerator;
 
 pub fn compile_idl(idl_filepath: &Path) -> Result<String, String> {
@@ -20,3 +21,18 @@ pub fn compile_idl(idl_filepath: &Path) -> Result<String, String> {
     rust_generator.generate(parsed_idl);
     Ok(output)
 }
+
+pub fn compile_idl_c(idl_filepath: &Path) -> Result<String, String> {
+    let processed_idl =
+        preprocessor::Preprocessor::parse(idl_filepath).map_err(|e| e.to_string())?;
+    let parsed_idl = parser::IdlParser::parse(parser::Rule::specification, processed_idl.as_ref())
+        .map_err(|e| format!("Error parsing IDL string: {e}"))?
+        .next()
+        .expect("Must contain a specification");
+
+    let mut output = String::new();
+    let mut c_generator = CGenerator::new(&mut output);
+    c_generator.generate(parsed_idl);
+    Ok(output)
+}
+
