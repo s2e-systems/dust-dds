0c3387df6a32757a044662a0c848cc5c98ee23a1
Joao Rebelo <2730147+jrebelo@users.noreply.github.com>
2026-07-21 17:51:52 +0200

    Add test for c basic types

--- dds_gen/tests/basic_types_c.rs
+++ dds_gen/tests/basic_types_c.rs
@@ -1,42 +1,32 @@
 use std::path::Path;
 
-use syn::File;
-
 #[test]
 fn basic_types() {
     let idl_file = Path::new("tests/basic_types.idl");
-    let expected = syn::parse2::<File>(
-        r#"
-            #[derive(Debug, Clone, dust_dds::infrastructure::type_support::DdsType)]
-            pub struct BasicTypes {
-                pub a: bool,
-                pub b: char,
-                pub c: char,
-                pub d: u8,
-                pub e: String,
-                pub f: String,
-                pub g: i16,
-                pub h: u16,
-                pub i: i32,
-                pub j: u32,
-                pub k: i64,
-                pub l: u64,
-                pub m: f32,
-                pub n: f64,
-            }
-    "#
-        .parse()
-        .unwrap(),
-    )
-    .unwrap();
+    let expected = r#"
+    #include <stdbool.h>
+    #include <stdint.h>
+    #include <stddef.h>
 
-    let result = syn::parse2::<File>(
-        dust_dds_gen::compile_idl_c(idl_file)
-            .unwrap()
-            .parse()
-            .unwrap(),
-    )
-    .unwrap();
+    struct BasicTypes {
+        bool a;
+        char b;
+        wchar_t c;
+        uint8_t d;
+        char* e;
+        wchar_t* f;
+        int16_t g;
+        uint16_t h;
+        int32_t i;
+        uint32_t j;
+        int64_t k;
+        uint64_t l;
+        float m;
+        double n;
+    };
+"#;
+
+    let result = dust_dds_gen::compile_idl_c(idl_file).unwrap();
 
     assert_eq!(result, expected);
 }
