2820889d17047b3bffc3f4fa2d7fa1998e273eb7
Joao Rebelo <2730147+jrebelo@users.noreply.github.com>
2026-08-06 15:20:24 +0200

    Fix clippy

--- dds/src/dcps/dcps_domain_participant/writer_methods.rs
+++ dds/src/dcps/dcps_domain_participant/writer_methods.rs
@@ -380,7 +380,7 @@ impl DcpsDomainParticipant {
                         return;
                     }
                     let expiration_time = match data_writer.qos.reliability.max_blocking_time {
-                        DurationKind::Finite(t) => Some(runtime.clock().now() + t.into()),
+                        DurationKind::Finite(t) => Some(runtime.clock().now() + t),
                         DurationKind::Infinite => None,
                     };
                     data_writer.pending_write_sample = Some(PendingWriteSample {
