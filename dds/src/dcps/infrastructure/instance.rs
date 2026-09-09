ddee62ba896328e2615631eca650aa15b7ceee30
Joao Rebelo <2730147+jrebelo@users.noreply.github.com>
2026-09-09 09:57:52 +0200

    Fix format

--- dds/src/dcps/infrastructure/instance.rs
+++ dds/src/dcps/infrastructure/instance.rs
@@ -96,9 +96,6 @@ mod tests {
     #[test]
     fn lower_hex_instance_handle() {
         let h = InstanceHandle::new([0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15]);
-        assert_eq!(
-            format!("{:x}", h),
-            "000102030405060708090a0b0c0d0e0f"
-        );
+        assert_eq!(format!("{:x}", h), "000102030405060708090a0b0c0d0e0f");
     }
 }
